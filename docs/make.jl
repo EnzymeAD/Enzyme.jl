3a420edc90d21bc9cc63592e21918710bfcf9b2e
Oliver Schulz <oschulz@mpp.mpg.de>
2021-06-29 00:00:33 +0200

    Enable Documenter strict mode, forcepush, etc.

--- docs/make.jl
+++ docs/make.jl
@@ -13,15 +13,20 @@ makedocs(;
         canonical="https://wsmoses.github.io/Enzyme.jl",
         assets=String[],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
         "API" => "api.md",
         "Implementing pullbacks" => "pullbacks.md",
         "For developers" => "dev_docs.md",
         "Internal API" => "internal_api.md",
     ],
+    doctest = true,
+    linkcheck = true,
+    strict = true,
 )
 
 deploydocs(;
     repo="github.com/wsmoses/Enzyme.jl",
+    forcepush = true,
+    push_preview = true,
 )
