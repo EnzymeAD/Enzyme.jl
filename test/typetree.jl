626becbe89d911a7046a65adfbe5693bec2b7913
Valentin Churavy <v.churavy@gmail.com>
2022-02-03 12:52:53 -0500

    Correct recursive type detection

--- test/typetree.jl
+++ test/typetree.jl
@@ -9,6 +9,18 @@ const dl = string(LLVM.DataLayout(LLVM.JITTargetMachine()))
 
 tt(T) = string(typetree(T, ctx, dl))
 
+struct Atom
+  x::Float32
+  y::Float32
+  z::Float32
+  type::Int32
+end
+
+struct Composite
+    k::Atom
+    y::Atom
+end
+
 @testset "TypeTree" begin
     @test tt(Float16) == "{[-1]:Float@half}"
     @test tt(Float32) == "{[-1]:Float@float}"
@@ -17,4 +29,6 @@ tt(T) = string(typetree(T, ctx, dl))
     @test tt(String) ==  "{}"
     @test tt(AbstractChannel) == "{}"
     @test tt(Base.ImmutableDict{Symbol, Any}) == "{[0]:Pointer, [8]:Pointer, [16]:Pointer}"
+    @test tt(Atom) == "{[0]:Float@float, [4]:Float@float, [8]:Float@float, [12]:Integer, [13]:Integer, [14]:Integer, [15]:Integer"
+    @test tt(Composite) == "{[0]:Float@float, [4]:Float@float, [8]:Float@float, [12]:Integer, [13]:Integer, [14]:Integer, [15]:Integer, [16]:Float@float, [20]:Float@float, [24]:Float@float, [28]:Integer, [29]:Integer, [30]:Integer, [31]:Integer}"
 end
