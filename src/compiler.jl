389ad0f0043b26966f444d5997228bb73b968230
William S. Moses <gh@wsmoses.com>
2022-05-25 20:49:45 -0400

    Fix debug info2

--- src/compiler.jl
+++ src/compiler.jl
@@ -3340,7 +3340,10 @@ function create_abi_wrapper(enzymefn::LLVM.Function, F, argtypes, rettype, actua
 
     FT = LLVM.FunctionType(T_void, T_wrapperargs)
     llvm_f = LLVM.Function(mod, safe_name(LLVM.name(enzymefn)*"wrap"), FT)
-    LLVM.set_subprogram!(llvm_f, LLVM.get_subprogram(enzymefn)) 
+    sfn = LLVM.get_subprogram(enzymefn)
+    if sfn !== nothing
+        LLVM.set_subprogram!(llvm_f, sfn)
+    end
     dl = datalayout(mod)
 
     params = [parameters(llvm_f)...]
@@ -3561,10 +3564,13 @@ function lower_convention(functy::Type, mod::LLVM.Module, entry_f::LLVM.Function
         push!(wrapper_types, typ)
     end
     wrapper_fn = LLVM.name(entry_f)
-    LLVM.name!(entry_f, safe_name(wrapper_fn * ".inner"))
+    LLVM.name!(entry_f, safe_name(wrapper_fn * "."))
     wrapper_ft = LLVM.FunctionType(RT, wrapper_types)
     wrapper_f = LLVM.Function(mod, LLVM.name(entry_f), wrapper_ft)
-    LLVM.set_subprogram!(wrapper_f, LLVM.get_subprogram(entry_f)) 
+    sfn = LLVM.get_subprogram(entry_f)
+    if sfn !== nothing
+        LLVM.set_subprogram!(wrapper_f, sfn)
+    end
 
     hasReturnsTwice = any(map(k->kind(k)==kind(EnumAttribute("returns_twice"; ctx)), collect(function_attributes(entry_f))))
     push!(function_attributes(wrapper_f), EnumAttribute("returns_twice"; ctx))
@@ -3610,6 +3616,9 @@ function lower_convention(functy::Type, mod::LLVM.Module, entry_f::LLVM.Function
 
         entry = BasicBlock(wrapper_f, "entry"; ctx)
         position!(builder, entry)
+        if LLVM.get_subprogram(entry_f) !== nothing
+            debuglocation!(builder, DILocation(ctx, 0, 0, LLVM.get_subprogram(entry_f)))
+        end
 
         wrapper_args = Vector{LLVM.Value}()
 
@@ -3667,7 +3676,7 @@ function lower_convention(functy::Type, mod::LLVM.Module, entry_f::LLVM.Function
 
     fixup_metadata!(entry_f)
 	
-	ModulePassManager() do pm
+    ModulePassManager() do pm
         always_inliner!(pm)
         run!(pm, mod)
     end
@@ -3688,7 +3697,14 @@ function lower_convention(functy::Type, mod::LLVM.Module, entry_f::LLVM.Function
             end
         end
     end
-    verify(wrapper_f)
+    
+    if LLVM.API.LLVMVerifyFunction(wrapper_f, LLVM.API.LLVMReturnStatusAction) != 0
+        @show mod
+        @show LLVM.API.LLVMVerifyFunction(wrapper_f, LLVM.API.LLVMPrintMessageAction)
+        @show wrapper_f
+        flush(stdout)
+        throw(LLVM.LLVMException("broken function"))
+    end
     return wrapper_f
 end
 
@@ -4433,7 +4449,7 @@ end
     target = Compiler.EnzymeTarget()
     params = Compiler.EnzymeCompilerParams(adjoint, Mode, width, A, true, DF != Nothing, #=abiwrap=#true, ModifiedBetween)
     job    = Compiler.CompilerJob(target, primal, params)
-
+    
     sig = Tuple{F, map(eltype, TT.parameters)...}
 
     # world = ...
@@ -4515,13 +4531,12 @@ import GPUCompiler: deferred_codegen_jobs
     target = EnzymeTarget()
     params = EnzymeCompilerParams(adjoint, Mode, width, rt, true, DupClosure, #=abiwrap=#true, ModifiedBetween)
     job    = CompilerJob(target, primal, params)
-
+    
     addr = get_trampoline(job)
     id = Base.reinterpret(Int, pointer(addr))
 
     deferred_codegen_jobs[id] = job
     trampoline = reinterpret(Ptr{Cvoid}, id)
-
     quote
         ccall("extern deferred_codegen", llvmcall, Ptr{Cvoid}, (Ptr{Cvoid},), $trampoline)
     end
