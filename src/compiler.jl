2ede12d02647222896878be60a2d0a29bb48f954
Valentin Churavy <v.churavy@gmail.com>
2022-09-27 18:43:14 -0400

    fixup! implement julia_call for 1.9

--- src/compiler.jl
+++ src/compiler.jl
@@ -3362,10 +3362,10 @@ function julia_allocator(B, LLVMType, Count, AlignedSize)
             else
                 # %5 = call nonnull {}* ({}* ({}*, {}**, i32)*, {}*, ...) @julia.call({}* ({}*, {}**, i32)* @jl_f_apply_type, {}* null, {}* inttoptr (i64 139640605802128 to {}*), {}* %4, {}* inttoptr (i64 139640590432896 to {}*))
                 generic_FT = LLVM.FunctionType(T_prjlvalue, 
-                    [T_prjlvalue, T_pprjlvalue, T_int32]))
+                    [T_prjlvalue, T_pprjlvalue, T_int32])
                 julia_call = get_function!(mod, "julia_call",
                     LLVM.FunctionType(T_prjlvalue, 
-                        [generic_FT, T_prjlvalue]); vararg=true)
+                        [generic_FT, T_prjlvalue]; vararg=true))
                 tag = call!(B, julia_call, [f_apply_type, LLVM.PointerNull(T_prjlvalue), EnzymeTapeT, boxed_count, TapeT])
             end
         end
