c8a6f2869a5a5faf0dbe82d581f0761b0f9a33a6
Valentin Churavy <v.churavy@gmail.com>
2022-10-08 17:42:35 -0400

    cleanup fwd rules test

--- test/rules.jl
+++ test/rules.jl
@@ -2,6 +2,8 @@ using Enzyme
 using Enzyme: EnzymeRules
 using Test
 
+import EnzymeRules: forward
+
 @testset "Custom Forward Rules" begin
 
     rule_f(x) = x^2
@@ -11,35 +13,37 @@ using Test
        return nothing
     end
 
-    function Enzyme.EnzymeRules.forward(::Type{Tuple{typeof(rule_f), Float64}}, RT, Args)
-        @assert Args[1] <: Const
-        if RT <: DuplicatedNoNeed && Args[2] <: Duplicated
+    function forward(::Type{Tuple{typeof(rule_f), Float64}}, rt, args)
+        @assert args[1] <: Const
+
+        if rt <: DuplicatedNoNeed && args[2] <: Duplicated
             tmp1(func, x) = 10+2*x.val*x.dval
             return tmp1
         end
-        if RT <: Enzyme.BatchDuplicatedNoNeed && Args[2] <: Enzyme.BatchDuplicated
-            function tmp1b(func, x::Enzyme.BatchDuplicated{T, N}) where {T, N}
+
+        if rt <: BatchDuplicatedNoNeed && args[2] <: BatchDuplicated
+            function tmp1b(func, x::BatchDuplicated{T, N}) where {T, N}
                 return NTuple{N, T}(1000+2*x.val*dv for dv in x.dval)
             end
             return tmp1b
         end
-        if RT <: Duplicated && Args[2] <: Duplicated
-            tmp2(func, x) = Enzyme.Duplicated(func.val(x.val), 100+2*x.val*x.dval)
+        if rt <: Duplicated && args[2] <: Duplicated
+            tmp2(func, x) = Duplicated(func.val(x.val), 100+2*x.val*x.dval)
             return tmp2
         end
-        if RT <: Enzyme.BatchDuplicated && Args[2] <: Enzyme.BatchDuplicated
-            function tmp2b(func, x::Enzyme.BatchDuplicated{T, N}) where {T, N}
-                return Enzyme.BatchDuplicated(func.val(x.val), NTuple{N, T}(10000+2*x.val*dv for dv in x.dval))
+        if rt <: BatchDuplicated && Args[2] <: BatchDuplicated
+            function tmp2b(func, x::BatchDuplicated{T, N}) where {T, N}
+                return BatchDuplicated(func.val(x.val), NTuple{N, T}(10000+2*x.val*dv for dv in x.dval))
             end
             return tmp2b
         end
         return nothing
     end
 
-    function Enzyme.EnzymeRules.forward(::Type{Tuple{typeof(rule_fip), T}}, RT, Args) where {T}
-        @assert Args[1] <: Const
-        @assert RT <: Const
-        if Args[2] <: Duplicated
+    function forward(::Type{Tuple{typeof(rule_fip), T}}, rt, args) where {T}
+        @assert args[1] <: Const
+        @assert rt <: Const
+        if args[2] <: Duplicated
             function tmp1(func, x)
                 ld = x.val[1]
                 x.val[1] *= ld
@@ -51,14 +55,14 @@ using Test
         return nothing
     end
 
-    @test Enzyme.autodiff(Enzyme.Forward, rule_f, Duplicated(2.0, 1.0))[1] ≈ 14.0
-    @test Enzyme.autodiff(Enzyme.Forward, x->rule_f(x)^2, Duplicated(2.0, 1.0))[1] ≈ 832.0
+    @test autodiff(Forward, rule_f, Duplicated(2.0, 1.0))[1] ≈ 14.0
+    @test autodiff(Forward, x->rule_f(x)^2, Duplicated(2.0, 1.0))[1] ≈ 832.0
 
-    res = Enzyme.autodiff(Enzyme.Forward, rule_f, BatchDuplicatedNoNeed, BatchDuplicated(2.0, (1.0, 3.0)))[1] 
+    res = autodiff(Forward, rule_f, BatchDuplicatedNoNeed, BatchDuplicated(2.0, (1.0, 3.0)))[1] 
     @test res[1] ≈ 1004.0
     @test res[2] ≈ 1012.0
 
-    res = Enzyme.autodiff(Enzyme.Forward, x->rule_f(x)^2, BatchDuplicatedNoNeed, BatchDuplicated(2.0, (1.0, 3.0)))[1]
+    res = Enzyme.autodiff(Forward, x->rule_f(x)^2, BatchDuplicatedNoNeed, BatchDuplicated(2.0, (1.0, 3.0)))[1]
 
     @test res[1] ≈ 80032.0
     @test res[2] ≈ 80096.0
@@ -70,5 +74,4 @@ using Test
 
     @test vec ≈ [4.0]
     @test dvec ≈ [14.0]
-
 end
