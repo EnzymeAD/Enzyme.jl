806e0ed2136d28bdd13ec9b901e470ef8cc2ae0f
William Moses <gh@wsmoses.com>
2022-12-25 01:33:46 -0500

    Add cbrt test (#568)

--- test/runtests.jl
+++ test/runtests.jl
@@ -137,6 +137,8 @@ end
     @test autodiff(Reverse, Base.atan, Active, Active(0.9), Active(3.4))[1] ≈ ForwardDiff.derivative(x->Base.atan(x, 3.4), 0.9)
     @test autodiff(Reverse, Base.atan, Active, Active(0.9), Active(3.4))[2] ≈ ForwardDiff.derivative(x->Base.atan(0.9, x), 3.4)
 
+    test_scalar(cbrt, 1.0)
+    test_scalar(cbrt, 1.0f0; rtol = 1.0e-5, atol = 1.0e-5)
     test_scalar(Base.sinh, 1.0)
     test_scalar(Base.cosh, 1.0)
     test_scalar(Base.sinc, 2.2)
