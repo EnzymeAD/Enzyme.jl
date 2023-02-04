79c744c0723339dbb75630cbd5e318480bde83ee
Valentin Churavy <v.churavy@gmail.com>
2023-02-04 17:37:44 -0500

    fixup! Update test/rules.jl

--- test/rules.jl
+++ test/rules.jl
@@ -37,6 +37,7 @@ function forward(::Const{Core.typeof(f_ip)}, ::Type{<:Const}, x::Duplicated)
 end
 
 function has_frule(f, @nospecialize(RT), @nospecialize(TT::Type{<:Tuple}); world=Base.get_world_counter())
+    TT = Base.unwrap_unionall(TT)
     TT = Tuple{<:Annotation{Core.typeof(f)}, Type{<:RT}, TT.parameters...}
     isapplicable(forward, TT; world)
 end
