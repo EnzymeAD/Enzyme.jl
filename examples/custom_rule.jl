5a484c5dad04a722411c0d5eab6cbe320f3591dd
Gaurav Arya <gauravarya272@gmail.com>
2023-04-07 19:32:10 -0400

    Tweaks to custom rule tutorial

--- examples/custom_rule.jl
+++ examples/custom_rule.jl
@@ -1,7 +1,7 @@
 # # Enzyme custom rules tutorial
 
 # The goal of this tutorial is to give a simple example of defining a custom rule with Enzyme.
-# Specifically, our goal will be to write a custom rule for the following function `f`:
+# Specifically, our goal will be to write custom rules for the following function `f`:
 
 function f(y, x)
     y .= x.^2
@@ -24,19 +24,20 @@ g(y, x) = f(y, x)^2 # function to differentiate
 @show autodiff(Forward, g, Duplicated(y, dy), Duplicated(x, dx)) # derivative of g w.r.t. x[1]
 @show dy; # derivative of y w.r.t. x[1] when g is run
 
-# (See the [AutoDiff API tutorial](..autodiff.md) for more information on using `autodiff`.)
+# (See the [AutoDiff API tutorial](autodiff.md) for more information on using `autodiff`.)
 
+# But there may be special cases where we need to write a custom rule to help Enzyme out.
 # Let's see how to write a custom rule for `f`!
 
 # !!! warning "Don't use custom rules unnecessarily!"
-#   Enzyme can efficiently handle a wide range of constructs, and so a custom rule should only be required
-#   in certain special cases. For example, a function may make a foreign call that Enzyme cannot differentiate,
-#   or we may have higher-level mathematical knowledge that enables us to write a more efficient rule. 
-#   Even in these cases, try to make your custom rule encapsulate the minimum possible construct that Enzyme
-#   cannot differentiate, rather than expanding the scope of the rule unnecessarily. 
-#   For pedagogical purposes, we will disregard this principle here and go ahead and write a rule for `f` :)
+#     Enzyme can efficiently handle a wide range of constructs, and so a custom rule should only be required
+#     in certain special cases. For example, a function may make a foreign call that Enzyme cannot differentiate,
+#     or we may have higher-level mathematical knowledge that enables us to write a more efficient rule. 
+#     Even in these cases, try to make your custom rule encapsulate the minimum possible construct that Enzyme
+#     cannot differentiate, rather than expanding the scope of the rule unnecessarily.
+#     For pedagogical purposes, we will disregard this principle here and go ahead and write a custom rule for `f` :)
 
-# ## Defining our first rule 
+# ## Defining our first (forward-mode) rule 
 
 # First, we import the functions [`EnzymeRules.forward`](@ref), [`EnzymeRules.augmented_primal`](@ref),
 # and [`EnzymeRules.reverse`](@ref).
@@ -104,15 +105,15 @@ dy = [0.0, 0.0]
 @show autodiff(Forward, f, Duplicated(y, dy), Duplicated(x, dx)) # derivative of f w.r.t. x[1]
 @show dy; # derivative of y w.r.t. x[1] when f is run
 
-# !!! note "Custom rule selection"
-#   When multiple custom rules for a function are defined, the correct rule is chosen chosen using 
-#   [Julia's multiple dispatch](https://docs.julialang.org/en/v1/manual/methods/#Methods).
-#   In particular, it is important to understand that the custom rule does not *determine* the
-#   activities of the inputs and the outputs: rather, `Enzyme` annotates the activities independently,
-#   and then *dispatches* to the custom rule handling the activities, if one exists.
+# !!! note "Custom rule dispatch"
+#     When multiple custom rules for a function are defined, the correct rule is chosen chosen using 
+#     [Julia's multiple dispatch](https://docs.julialang.org/en/v1/manual/methods/#Methods).
+#     In particular, it is important to understand that the custom rule does not *determine* the
+#     activities of the inputs and the outputs: rather, `Enzyme` decides the activity annotations independently,
+#     and then *dispatches* to the custom rule handling the activities, if one exists.
 
-# Finally, it may be that either `x` or `y`  are marked as [`Const`](@ref). We can in fact handle this case, 
-# along with the previous two cases, together in a single rule:
+# Finally, it may be that either `x` or `y` are marked as [`Const`](@ref). We can in fact handle this case, 
+# along with the previous two cases, all together in a single rule:
 
 function forward(func::Const{typeof(f)}, RT::Type{<:Union{DuplicatedNoNeed, Duplicated}}, 
                  y::Union{Const, Duplicated}, x::Union{Const, Duplicated})
@@ -133,11 +134,13 @@ end
 # Note that there are also exist batched duplicated annotations for forward mode, namely [`BatchDuplicated`](@ref)
 # and [`BatchDuplicatedNoNeed`](@ref), which are not covered in this tutorial.
 
-# ## Reverse-mode
+# ## Defining a reverse-mode rule
 
-# Finally, let's look at how to write a simple reverse-mode rule! First, we write a method for [`EnzymeRules.augmented_primal`](@ref):
+# Finally, let's look at how to write a simple reverse-mode rule! 
+# First, we write a method for [`EnzymeRules.augmented_primal`](@ref):
 
-function augmented_primal(config::ConfigWidth{1}, func::Const{typeof(f)}, ::Type{<:Active}, y::Duplicated, x::Duplicated)
+function augmented_primal(config::ConfigWidth{1}, func::Const{typeof(f)}, ::Type{<:Active},
+                          y::Duplicated, x::Duplicated)
     println("In custom augmented primal rule.")
     if needs_primal(config)
         return AugmentedReturn(func.val(y.val, x.val), nothing, nothing)
@@ -147,27 +150,30 @@ function augmented_primal(config::ConfigWidth{1}, func::Const{typeof(f)}, ::Type
 end
 
 # Let's unpack our signature for `augmented_primal` :
-# * We accepted a [`Config`](@ref) object with a specified width of 1, which means that our rule does not support batched reverse mode.
+# * We accepted a [`EnzymeRules.Config`](@ref) object with a specified width of 1, which means that our rule does not support batched reverse mode.
 # * Our function `f` was annotated [`Const`](@ref) as usual.
 # * We dispatched on an [`Active`](@ref) annotation for the return value. This is a special annotation for scalar values, such as our return value,
 #   that indicates that that we care about the value's derivative but we need not explicitly allocate a mutable shadow since it is a scalar value.
-# * We annotated `x` and `y` with `Duplicated` as in the forward mode case.
+# * We annotated `x` and `y` with [`Duplicated`](@ref) as in the forward mode case.
 
 # The body of our function simply checks if the `config` requires the primal, running it if so.
 # Our function returns an `AugmentedReturn` object specifying the return value, its shadow, and any extra tape information (none in this csae).
 # Note that we return a shadow of `nothing` since the return value is marked [`Active`](@ref).
 
-# Now, we write a method for [`EnzymeRules.reverse`](@ref).
+# Now, we write a method for [`EnzymeRules.reverse`](@ref):
 
-function reverse(config::ConfigWidth{1}, func::Const{typeof(f)}, out::Active, tape, y::Duplicated, x::Duplicated)
+function reverse(config::ConfigWidth{1}, func::Const{typeof(f)}, out::Active, tape,
+                 y::Duplicated, x::Duplicated)
     println("In custom reverse rule.")
     y.dval .+= out.val 
     x.dval .+= 2 .* x.val .* y.dval # accumulate into shadow, don't assign!
     return ()
 end
 
-# The activities match what we used for `augmented_primal`. One difference is that we know receive an *instance* of [`Active`](@ref)
-# for the return type. Here, `out.val` stores the accumulated derivative value for `out` (not the original return value!).
+# The activities used in the signature match what we used for `augmented_primal`. 
+# One key difference is that we know receive an *instance* `out` of [`Active`](@ref) for the return type, not just a type annotation.
+# Here, `out.val` stores the derivative value for `out` (not the original return value!).
+# Using this derivative value, we accumulate the backpropagated derivatives for `x` and `y` into their shadows. 
 
 # Finally, let's see our reverse rule in action!
 
@@ -180,4 +186,4 @@ g(y, x) = f(y, x)^2
 
 autodiff(Reverse, g, Duplicated(y, dy), Duplicated(x, dx)) # derivative of f w.r.t. y[1]
 @show dx # derivative of f w.r.t. x
-@show dy # derivative of f w.r.t. y
+@show dy; # derivative of f w.r.t. y
