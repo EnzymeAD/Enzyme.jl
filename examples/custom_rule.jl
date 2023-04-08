5892c7286edab61d4bf471fcd3c8b865d68ed7c3
Gaurav Arya <gauravarya272@gmail.com>
2023-04-08 15:36:25 -0400

    More progress on custom rule example

--- examples/custom_rule.jl
+++ examples/custom_rule.jl
@@ -9,7 +9,7 @@ function f(y, x)
 end
 
 # Our function `f` populates its first input `y` with the element-wise square of `x`.
-# In addition to doing this mutation, it returns `sum(y)` as output. What a sneaky function!
+# In addition, it returns `sum(y)` as output. What a sneaky function!
 
 # In this case, Enzyme can differentiate through `f` automatically. For example, using forward mode:
 
@@ -51,9 +51,9 @@ using .EnzymeRules
 
 function forward(func::Const{typeof(f)}, ::Type{<:Duplicated}, y::Duplicated, x::Duplicated)
     println("Using custom rule!")
-    out = func.val(y.val, x.val)
+    ret = func.val(y.val, x.val)
     y.dval .= 2 .* x.val .* x.dval
-    return Duplicated(out, sum(y.dval)) 
+    return Duplicated(ret, sum(y.dval)) 
 end
 
 # In the signature of our rule, we have made use of `Enzyme`'s activity annotations. Let's break down each one:
@@ -85,7 +85,7 @@ g(y, x) = f(y, x)^2 # function to differentiate
 # Our custom rule applies for the specific set of activities that are annotated for `f` in the above `autodiff` call. 
 # However, Enzyme has a number of other annotations. Let us consider a particular case as an example, where the output
 # has a [`DuplicatedNoNeed`](@ref) annotation. This means we are only interested in its derivative, not its value.
-# To squeeze the last drop of performance, the below rule avoids computing the output of the original function and 
+# To squeeze out the last drop of performance, the below rule avoids computing the output of the original function and 
 # just computes its derivative.
 
 function forward(func::Const{typeof(f)}, ::Type{<:DuplicatedNoNeed}, y::Duplicated, x::Duplicated)
@@ -95,7 +95,7 @@ function forward(func::Const{typeof(f)}, ::Type{<:DuplicatedNoNeed}, y::Duplicat
     return sum(y.dval)
 end
 
-# Our rule is triggered, for example, when we call `autodiff` directly on `f`, as the output's derivative isn't needed:
+# Our rule is triggered, for example, when we call `autodiff` directly on `f`, as the return value's derivative isn't needed:
 
 x  = [3.0, 1.0]
 dx = [1.0, 0.0]
@@ -109,13 +109,15 @@ dy = [0.0, 0.0]
 #     When multiple custom rules for a function are defined, the correct rule is chosen using 
 #     [Julia's multiple dispatch](https://docs.julialang.org/en/v1/manual/methods/#Methods).
 #     In particular, it is important to understand that the custom rule does not *determine* the
-#     activities of the inputs and the outputs: rather, `Enzyme` decides the activity annotations independently,
+#     activities of the inputs and the return value: rather, `Enzyme` decides the activity annotations independently,
 #     and then *dispatches* to the custom rule handling the activities, if one exists.
 #     If a custom rule is specified for the correct function/argument types, but not the correct activity annotation, 
 #     a runtime error will be thrown alerting the user to the missing activity rule rather than silently ignoring the rule."
 
-# Finally, it may be that either `x`, `y`, or the output are marked as [`Const`](@ref). We can in fact handle this case, 
+# Finally, it may be that either `x`, `y`, or the return value are marked as [`Const`](@ref). We can in fact handle this case, 
 # along with the previous two cases, all together in a single rule:
+
+Base.delete_method.(methods(forward, (Const{typeof(f)}, Vararg))) # delete our old rules
 
 function forward(func::Const{typeof(f)}, RT::Type{<:Union{Const, DuplicatedNoNeed, Duplicated}}, 
                  y::Union{Const, Duplicated}, x::Union{Const, Duplicated})
@@ -126,15 +128,17 @@ function forward(func::Const{typeof(f)}, RT::Type{<:Union{Const, DuplicatedNoNee
     elseif !(y isa Const) 
         y.dval .= 0
     end
-    dout = !(y isa Const) ? sum(y.dval) : zero(eltype(y.val))
+    dret = !(y isa Const) ? sum(y.dval) : zero(eltype(y.val))
     if RT <: Const
         return nothing
     elseif RT <: DuplicatedNoNeed
-        return dout 
+        return dret 
     else
-        return Duplicated(sum(y.val), dout)
+        return Duplicated(sum(y.val), dret)
     end
 end
+
+# Let's try out our rule:
 
 x  = [3.0, 1.0]
 dx = [1.0, 0.0]
@@ -145,7 +149,7 @@ g(y, x) = f(y, x)^2 # function to differentiate
 
 @show autodiff(Forward, g, Duplicated(y, dy), Duplicated(x, dx)) # derivative of g w.r.t. x[1]
 @show autodiff(Forward, g, Const(y), Duplicated(x, dx)) # derivative of g w.r.t. x[1], with y annotated Const
-@show autodiff(Forward, g, Const(y), Const(x)) # derivative of g w.r.t. x[1], with x and y annotated Const
+@show autodiff(Forward, g, Const(y), Const(x)); # derivative of g w.r.t. x[1], with x and y annotated Const
 
 # Note that there are also exist batched duplicated annotations for forward mode, namely [`BatchDuplicated`](@ref)
 # and [`BatchDuplicatedNoNeed`](@ref), which are not covered in this tutorial.
@@ -158,20 +162,20 @@ g(y, x) = f(y, x)^2 # function to differentiate
 function augmented_primal(config::ConfigWidth{1}, func::Const{typeof(f)}, ::Type{<:Active},
                           y::Duplicated, x::Duplicated)
     println("In custom augmented primal rule.")
-    # Compute primal
+    ## Compute primal
     if needs_primal(config)
         primal = func.val(y.val, x.val)
     else
         y.val .= x.val.^2 # y still needs to be mutated even if primal not needed!
         primal = nothing
     end
-    # Save x in tape if x will be overwritten
+    ## Save x in tape if x will be overwritten
     if overwritten(config)[3]
         tape = copy(x.val) 
     else
         tape = nothing
     end
-    # Return an AugmentedReturn object with shadow = nothing
+    ## Return an AugmentedReturn object with shadow = nothing
     return AugmentedReturn(primal, nothing, tape)
 end
 
@@ -190,21 +194,19 @@ end
 
 # Now, we write a method for [`EnzymeRules.reverse`](@ref):
 
-function reverse(config::ConfigWidth{1}, func::Const{typeof(f)}, out::Active, tape,
+function reverse(config::ConfigWidth{1}, func::Const{typeof(f)}, dret::Active, tape,
                  y::Duplicated, x::Duplicated)
     println("In custom reverse rule.")
-    # retrieve x value, either from original x or from tape if x may have been overwritten.
+    ## retrieve x value, either from original x or from tape if x may have been overwritten.
     xval = overwritten(config)[3] ? tape : x.val 
-    # accumulate into x's shadow, don't assign!
-    @show out.val
-    @show xval
-    x.dval .+= 2 .* xval .* out.val 
+    ## accumulate into x's shadow, don't assign!
+    x.dval .+= 2 .* xval .* dret.val 
     return ()
 end
 
 # The activities used in the signature match what we used for `augmented_primal`. 
-# One key difference is that we know receive an *instance* `out` of [`Active`](@ref) for the return type, not just a type annotation.
-# Here, `out.val` stores the derivative value for `out` (not the original return value!).
+# One key difference is that we know receive an *instance* `dret` of [`Active`](@ref) for the return type, not just a type annotation.
+# Here, `dret.val` stores the derivative value for `dret` (not the original return value!).
 # Using this derivative value, we accumulate the backpropagated derivatives for `x` into its shadow. 
 # Note that we do not accumulate anything into `y`'s shadow! This is because `y` is overwritten within `f`, so there is no derivative
 # w.r.t. to the `y` that was originally inputted.
@@ -220,13 +222,14 @@ g(y, x) = f(y, x)^2
 
 autodiff(Reverse, g, Duplicated(y, dy), Duplicated(x, dx))
 @show dx # derivative of g w.r.t. x
-@show dy # derivative of g w.r.t. y
+@show dy; # derivative of g w.r.t. y
 
-# Also try a function which mutates x afterwards
+# Let's also try a function which mutates `x` after running `f`:
+
 function h(y, x)
-    out = f(y, x)
+    ret = f(y, x)
     x .= x.^2
-    return out# + sum(x)
+    return ret
 end
 
 x  = [3.0, 1.0]
@@ -234,23 +237,6 @@ y  = [0.0, 0.0]
 dx .= 0
 dy .= 0
 
-autodiff(Reverse, h, Duplicated(y, dy), Duplicated(x, dx))
+## autodiff(Reverse, h, Duplicated(y, dy), Duplicated(x, dx))
 @show dx # derivative of h w.r.t. x
 @show dy; # derivative of h w.r.t. y
-
-function a(y, x)
-    y .= x.^2
-    out = sum(y) 
-    x .= x.^2
-    return out^2
-end
-
-x  = [3.0, 1.0]
-y  = [0.0, 0.0]
-dx .= 0
-dy .= 0
-
-
-autodiff(Reverse, a, Duplicated(y, dy), Duplicated(x, dx))
-@show dx
-@show dy
