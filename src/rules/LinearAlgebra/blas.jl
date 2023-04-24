4c21032a316f676fc14c884d2f5d120f8a1a917d
Seth Axen <seth@sethaxen.com>
2023-04-24 19:45:43 +0200

    Return primal if return type is Const

--- src/rules/LinearAlgebra/blas.jl
+++ src/rules/LinearAlgebra/blas.jl
@@ -38,7 +38,8 @@ for (fname, Ttype) in ((:dot, :BlasReal), (:dotu, :BlasComplex), (:dotc, :BlasCo
             Y::ConstOrDuplicated{<:Union{Ptr{T},AbstractArray{T}}},
             incy::Const{<:Integer},
         ) where {T<:BLAS.$Ttype}
-            RT <: Const && return nothing
+            RT <: Const && return func.val(n.val, X.val, incx.val, Y.val, incy.val)
+
             dval = if !(X isa Const) && !(Y isa Const)
                 func.val(n.val, X.dval, incx.val, Y.val, incy.val) +
                 func.val(n.val, X.val, incx.val, Y.dval, incy.val)
