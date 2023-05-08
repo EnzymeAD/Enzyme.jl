e96c0c5fc41088d611baa65f3ccbd7379552b9d7
Manuel Drehwald <git@manuel.drehwald.info>
2023-05-08 11:53:38 -0400

    add blas attrs on the julia side (#794)
    * add blas attrs on the julia side
    
    * disable dot fallback in enzyme propper
    
    * disable dot fallback
    
    * disable dot fallback2
    
    * Bump jll
    
    ---------
    
    Co-authored-by: William S. Moses <gh@wsmoses.com>

--- src/compiler.jl
+++ src/compiler.jl
@@ -6885,6 +6885,26 @@ function annotate!(mod, mode)
         end
     end
 
+    blas_types = ("s", "d")
+    blas_readonly = ("dot", "asum", "nrm2", "amax")
+    blas_argmemonly = ("scal", "axpy", "copy", "swap", "gemv", "ger", "spmv", "spr", "gbmv", "sbmv", "trmv", "trsv", "tbmv", "tbsv", "gemm", "symm", "trmm", "trsm", "syrk", "syr2k")
+    blas_endings = ("_", "_64_")
+    blas_fncs_ro = [(x*y*z) for x in blas_types for y in blas_readonly for z in blas_endings]
+    blas_fncs_mo = [(x*y*z) for x in blas_types for y in blas_argmemonly for z in blas_endings]
+    for fname in blas_fncs_ro
+        if haskey(fns, fname)
+            fn = fns[fname]
+            push!(function_attributes(fn), LLVM.EnumAttribute("readonly", 0; ctx))
+            push!(function_attributes(fn), LLVM.EnumAttribute("argmemonly", 0; ctx))
+        end
+    end
+    for fname in blas_fncs_mo
+        if haskey(fns, fname)
+            fn = fns[fname]
+            push!(function_attributes(fn), LLVM.EnumAttribute("argmemonly", 0; ctx))
+        end
+    end
+
     for fname in ("jl_f_getfield","ijl_f_getfield","jl_get_nth_field_checked","ijl_get_nth_field_checked")
         if haskey(fns, fname)
             fn = fns[fname]
@@ -8398,7 +8418,8 @@ end
 
     primalf = meta.entry
     check_ir(job, mod)
-    if API.EnzymeBitcodeReplacement(mod) != 0
+    disableFallback = ["sdot_64_", "ddot_64_"]
+    if API.EnzymeBitcodeReplacement(mod, disableFallback) != 0
         ModulePassManager() do pm
             instruction_combining!(pm)
             run!(pm, mod)
