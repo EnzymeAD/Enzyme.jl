e999b45d7258333f6c4d613f93a3d249d8027ac8
Seth Axen <seth@sethaxen.com>
2023-05-12 11:07:52 +0200

    Add all_or_no_batch utility

--- test/testers.jl
+++ test/testers.jl
@@ -227,3 +227,22 @@ function test_forward(
         end
     end
 end
+
+"""
+    all_or_no_batch(activities...) -> Bool
+
+Returns `true` if `activities` are compatible in terms of batched activities.
+
+When a test set loops over many activities, some of which may be `BatchedDuplicated` or
+`BatchedDuplicatedNoNeed`, this is useful for skipping those combinations that are
+incompatible and will raise errors.
+"""
+function all_or_no_batch(activities...)
+    no_batch = !any(activities) do T
+        T <: Union{BatchDuplicated,BatchDuplicatedNoNeed}
+    end
+    all_batch_or_const = all(activities) do T
+        T <: Union{BatchDuplicated,BatchDuplicatedNoNeed,Const}
+    end
+    return all_batch_or_const || no_batch
+end
