765651fb1b3f454e5fd156b34222c04df085bd4d
William S. Moses <gh@wsmoses.com>
2023-05-28 01:11:59 -0400

    Add cuda fixes

--- src/compiler.jl
+++ src/compiler.jl
@@ -129,6 +129,10 @@ end
 const nofreefns = Set{String}((
     "ijl_module_parent", "jl_module_parent",
     "julia.safepoint",
+    "ijl_set_task_tid", "jl_set_task_tid",
+    "ijl_get_task_tid", "jl_get_task_tid",
+    "julia.get_pgcstack_or_new",
+    "ijl_global_event_loop", "jl_global_event_loop",
     "ijl_gf_invoke_lookup", "jl_gf_invoke_lookup",
     "ijl_f_typeassert", "jl_f_typeassert",
     "ijl_type_unionall", "jl_type_unionall",
@@ -188,6 +192,10 @@ const nofreefns = Set{String}((
 const inactivefns = Set{String}((
     "ijl_module_parent", "jl_module_parent",
     "julia.safepoint",
+    "ijl_set_task_tid", "jl_set_task_tid",
+    "ijl_get_task_tid", "jl_get_task_tid",
+    "julia.get_pgcstack_or_new",
+    "ijl_global_event_loop", "jl_global_event_loop",
     "ijl_gf_invoke_lookup", "jl_gf_invoke_lookup",
     "ijl_f_typeassert", "jl_f_typeassert",
     "ijl_type_unionall", "jl_type_unionall",
@@ -2626,7 +2634,7 @@ function jlcall_fwd(B::LLVM.API.LLVMBuilderRef, OrigCI::LLVM.API.LLVMValueRef, g
         end
     end
 
-    @assert false "jl_call calling convention not implemented yet", orig
+    emit_error(LLVM.IRBuilder(B), orig, "Enzyme: jl_call calling converntion not implemented in forward for "*string(orig))
 
     return 0
 end
@@ -2667,7 +2675,7 @@ function jlcall_augfwd(B::LLVM.API.LLVMBuilderRef, OrigCI::LLVM.API.LLVMValueRef
         end
     end
 
-    @assert false "jl_call calling convention not implemented yet", orig
+    emit_error(LLVM.IRBuilder(B), orig, "Enzyme: jl_call calling converntion not implemented in aug_forward for "*string(orig))
 
     return 0
 end
@@ -2716,7 +2724,7 @@ function jlcall_rev(B::LLVM.API.LLVMBuilderRef, OrigCI::LLVM.API.LLVMValueRef, g
         end
     end
 
-    @assert false "jl_call calling convention not implemented yet", orig
+    emit_error(LLVM.IRBuilder(B), orig, "Enzyme: jl_call calling converntion not implemented in reverse for "*string(orig))
 
     return nothing
 end
@@ -6940,7 +6948,7 @@ function __init__()
         @cfunction(jl_array_ptr_copy_fwd, UInt8, (LLVM.API.LLVMBuilderRef, LLVM.API.LLVMValueRef, API.EnzymeGradientUtilsRef, Ptr{LLVM.API.LLVMValueRef}, Ptr{LLVM.API.LLVMValueRef})),
     )
     register_handler!(
-        (),
+        ("jl_uv_associate_julia_struct","uv_async_init"),
         @cfunction(jl_unhandled_augfwd, UInt8, (LLVM.API.LLVMBuilderRef, LLVM.API.LLVMValueRef, API.EnzymeGradientUtilsRef, Ptr{LLVM.API.LLVMValueRef}, Ptr{LLVM.API.LLVMValueRef}, Ptr{LLVM.API.LLVMValueRef})),
         @cfunction(jl_unhandled_rev, Cvoid, (LLVM.API.LLVMBuilderRef, LLVM.API.LLVMValueRef, API.EnzymeGradientUtilsRef, LLVM.API.LLVMValueRef)),
         @cfunction(jl_unhandled_fwd, UInt8, (LLVM.API.LLVMBuilderRef, LLVM.API.LLVMValueRef, API.EnzymeGradientUtilsRef, Ptr{LLVM.API.LLVMValueRef}, Ptr{LLVM.API.LLVMValueRef})),
