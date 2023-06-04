974604ab81c0af089a4b86e584d00e156c0ed723
Seth Axen <seth@sethaxen.com>
2023-06-04 10:37:23 +0200

    Handle nonnumeric types in test_approx

--- lib/EnzymeTestUtils/src/test_approx.jl
+++ lib/EnzymeTestUtils/src/test_approx.jl
@@ -1,11 +1,23 @@
 function test_approx(x::Number, y::Number; kwargs...)
     @test isapprox(x, y; kwargs...)
+    return nothing
 end
 function test_approx(x::AbstractArray{<:Number}, y::AbstractArray{<:Number}; kwargs...)
     @test isapprox(x, y; kwargs...)
+    return nothing
+end
+function test_approx(x::AbstractArray, y::AbstractArray; kwargs...)
+    test_approx.(x, y; kwargs...)
+    return nothing
 end
 function test_approx(x, y; kwargs...)
-    for k in fieldnames(typeof(x))
-        test_approx(getfield(x, k), getfield(y, k); kwargs...)
+    names = fieldnames(typeof(x))
+    if isempty(names)
+        @test x === y || x == y
+    else
+        for k in names
+            test_approx(getfield(x, k), getfield(y, k); kwargs...)
+        end
     end
+    return nothing
 end
