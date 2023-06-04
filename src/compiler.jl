8c19412dc9f48a7a394f3b845c47829e65cca5c5
Valentin Churavy <v.churavy@gmail.com>
2023-06-04 15:52:47 -0400

    Make bitcode replacement a preferences for easier profiling

--- src/compiler.jl
+++ src/compiler.jl
@@ -18,6 +18,11 @@ import GPUCompiler: CompilerJob, codegen, safe_name
 using LLVM.Interop
 import LLVM: Target, TargetMachine
 
+using Preferences
+
+bitcode_replacement() = parse(Bool, @load_preference("bitcode_replacement", "true"))
+bitcode_replacement!(val) = @set_preferences!("bitcode_replacement" => string(val))
+
 using Printf
 
 function cpu_name()
@@ -8685,7 +8690,7 @@ function GPUCompiler.codegen(output::Symbol, job::CompilerJob{<:EnzymeTarget};
             end
         end
     end
-    if API.EnzymeBitcodeReplacement(mod, disableFallback) != 0
+    if bitcode_replacement() && API.EnzymeBitcodeReplacement(mod, disableFallback) != 0
         ModulePassManager() do pm
             instruction_combining!(pm)
             run!(pm, mod)
