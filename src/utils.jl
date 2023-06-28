41bf93362705e2a0a5e38be94d5b3dc69c33e372
motabbara <m.tabbara@icloud.com>
2023-06-28 16:41:26 +1000

    insert right part of GPUCompiler

--- src/utils.jl
+++ src/utils.jl
@@ -13,7 +13,25 @@ else
     import Base: allocatedinline
 end
 
-####From GPUCompiler 0.19.4 https://github.com/JuliaGPU/GPUCompiler.jl/blob/v0.19.4/src/jlgen.jl
+#Excerpt from https://github.com/JuliaGPU/GPUCompiler.jl/blob/v0.19.4/src/jlgen.jl
+
+using Core: MethodInstance
+# Julia compiler integration
+
+
+## world age lookups
+
+# `tls_world_age` should be used to look up the current world age. in most cases, this is
+# what you should use to invoke the compiler with.
+#
+# `codegen_world_age` is a special function that returns the world age in which the passed
+# method instance (identified by its function and argument types) is to be compiled. the
+# returned constant is automatically invalidated when the method is redefined, and as such
+# can be used to drive cached compilation. it is unlikely that you should use this function
+# directly, instead use `cached_compilation` which handles invalidation for you.
+
+tls_world_age() = ccall(:jl_get_tls_world_age, UInt, ())
+
 if VERSION >= v"1.10.0-DEV.873"
 
 # on 1.10 (JuliaLang/julia#48611) the generated function knows which world it was invoked in
@@ -182,4 +200,68 @@ end
 end
 
 end
+
+## looking up method instances
+
+export methodinstance
+
+using Core.Compiler: retrieve_code_info, CodeInfo, MethodInstance, SSAValue, SlotNumber, ReturnNode
+using Base: _methods_by_ftype
+
+@inline function typed_signature(ft::Type, tt::Type)
+    u = Base.unwrap_unionall(tt)
+    return Base.rewrap_unionall(Tuple{ft, u.parameters...}, tt)
+end
+
+# create a MethodError from a function type
+# TODO: fix upstream
+function unsafe_function_from_type(ft::Type)
+    if isdefined(ft, :instance)
+        ft.instance
+    else
+        # HACK: dealing with a closure or something... let's do somthing really invalid,
+        #       which works because MethodError doesn't actually use the function
+        Ref{ft}()[]
+    end
+end
+function MethodError(ft::Type, tt::Type, world::Integer=typemax(UInt))
+    Base.MethodError(unsafe_function_from_type(ft), tt, world)
+end
+
+"""
+    methodinstance(ft::Type, tt::Type, [world::UInt])
+
+Look up the method instance that corresponds to invoking the function with type `ft` with
+argument typed `tt`. If the `world` argument is specified, the look-up is static and will
+always return the same result. If the `world` argument is not specified, the look-up is
+dynamic and the returned method instance will automatically be invalidated when a relevant
+function is redefined.
+"""
+function methodinstance(ft::Type, tt::Type, world::Integer=tls_world_age())
+    sig = typed_signature(ft, tt)
+
+    # look-up the method
+    if VERSION >= v"1.10.0-DEV.65"
+        meth = Base._which(sig; world).method
+    elseif VERSION >= v"1.7.0-DEV.435"
+        meth = Base._which(sig, world).method
+    else
+        meth = ccall(:jl_gf_invoke_lookup, Any, (Any, UInt), sig, world)
+        if meth == nothing
+            error("no unique matching method found for the specified argument types")
+        end
+    end
+
+    (ti, env) = ccall(:jl_type_intersection_with_env, Any,
+                      (Any, Any), sig, meth.sig)::Core.SimpleVector
+
+    meth = Base.func_for_method_checked(meth, ti, env)
+
+    method_instance = ccall(:jl_specializations_get_linfo, Ref{Core.MethodInstance},
+                            (Any, Any, Any, UInt), meth, ti, env, world)
+
+    return method_instance
+end
+
+Base.@deprecate_binding FunctionSpec methodinstance
 
