53b756691df19e40431c154fba5791204faf3ff2
William S. Moses <gh@wsmoses.com>
2023-07-13 14:23:21 -0400

    Fix

--- src/compiler/orcv2.jl
+++ src/compiler/orcv2.jl
@@ -10,10 +10,18 @@ import ..Compiler: API, cpu_name, cpu_features
 
 export get_trampoline
 
-struct CompilerInstance
-    jit::LLVM.JuliaOJIT
-    lctm::Union{LLVM.LazyCallThroughManager, Nothing}
-    ism::Union{LLVM.IndirectStubsManager, Nothing}
+@static if LLVM.has_julia_ojit()
+    struct CompilerInstance
+        jit::LLVM.JuliaOJIT
+        lctm::Union{LLVM.LazyCallThroughManager, Nothing}
+        ism::Union{LLVM.IndirectStubsManager, Nothing}
+    end
+else
+    struct CompilerInstance
+        jit::LLVM.LLJIT
+        lctm::Union{LLVM.LazyCallThroughManager, Nothing}
+        ism::Union{LLVM.IndirectStubsManager, Nothing}
+    end
 end
 
 function LLVM.dispose(ci::CompilerInstance)
@@ -63,63 +71,67 @@ function __init__()
     tempTM = LLVM.JITTargetMachine(LLVM.triple(), cpu_name(), cpu_features(); optlevel)
     LLVM.asm_verbosity!(tempTM, true)
     tm[] = tempTM
+    
+    lljit = if !LLVM.has_julia_ojit()
+        tempTM = LLVM.JITTargetMachine(LLVM.triple(), cpu_name(), cpu_features(); optlevel)
+        LLVM.asm_verbosity!(tempTM, true)
 
-    tempTM = LLVM.JITTargetMachine(LLVM.triple(), cpu_name(), cpu_features(); optlevel)
-    LLVM.asm_verbosity!(tempTM, true)
+        gdb = haskey(ENV, "ENABLE_GDBLISTENER")
+        perf = haskey(ENV, "ENABLE_JITPROFILING")
+        if gdb || perf
+            ollc = LLVM.ObjectLinkingLayerCreator() do es, triple
+                oll = ObjectLinkingLayer(es)
+                if gdb
+                    register!(oll, GDBRegistrationListener())
+                end
+                if perf
+                    register!(oll, IntelJITEventListener())
+                    register!(oll, PerfJITEventListener())
+                end
+                return oll
+            end
+            GC.@preserve ollc begin
+                builder = LLJITBuilder()
+                LLVM.linkinglayercreator!(builder, ollc)
+                tmb = TargetMachineBuilder(tempTM)
+                LLVM.targetmachinebuilder!(builder, tmb)
+                LLJIT(builder)
+            end
+        else
+            LLJIT(;tm=tempTM)
+        end
+    else
+        JuliaOJIT()
+    end
 
-    # gdb = haskey(ENV, "ENABLE_GDBLISTENER")
-    # perf = haskey(ENV, "ENABLE_JITPROFILING")
-    # if gdb || perf
-    #     ollc = LLVM.ObjectLinkingLayerCreator() do es, triple
-    #         oll = ObjectLinkingLayer(es)
-    #         if gdb
-    #             register!(oll, GDBRegistrationListener())
-    #         end
-    #         if perf
-    #             register!(oll, IntelJITEventListener())
-    #             register!(oll, PerfJITEventListener())
-    #         end
-    #         return oll
-    #     end
-    #     GC.@preserve ollc begin
-    #         builder = LLJITBuilder()
-    #         LLVM.linkinglayercreator!(builder, ollc)
-    #         tmb = TargetMachineBuilder(tempTM)
-    #         LLVM.targetmachinebuilder!(builder, tmb)
-    #         lljit = LLJIT(builder)
-    #     end
-    # else
-    #     lljit = LLJIT(;tm=tempTM)
+    jd_main = JITDylib(lljit)
 
-    # end
-    jljit = JuliaOJIT()
-
-    jd_main = JITDylib(jljit)
-
-    prefix = LLVM.get_prefix(jljit)
+    prefix = LLVM.get_prefix(lljit)
     dg = LLVM.CreateDynamicLibrarySearchGeneratorForProcess(prefix)
     LLVM.add!(jd_main, dg)
 
 	if Sys.iswindows() && Int === Int64
 		# TODO can we check isGNU?
-		define_absolute_symbol(jd_main, mangle(jljit, "___chkstk_ms"))
+		define_absolute_symbol(jd_main, mangle(lljit, "___chkstk_ms"))
 	end
 
-    es = ExecutionSession(jljit)
+    es = ExecutionSession(lljit)
     try
-        lctm = LLVM.LocalLazyCallThroughManager(triple(jljit), es)
-        ism = LLVM.LocalIndirectStubsManager(triple(jljit))
-        jit[] = CompilerInstance(jljit, lctm, ism)
+        lctm = LLVM.LocalLazyCallThroughManager(triple(lljit), es)
+        ism = LLVM.LocalIndirectStubsManager(triple(lljit))
+        jit[] = CompilerInstance(lljit, lctm, ism)
     catch err
         @warn "OrcV2 initialization failed with" err
-        jit[] = CompilerInstance(jljit, nothing, nothing)
+        jit[] = CompilerInstance(lljit, nothing, nothing)
     end
 
-    # atexit() do
-    #     ci = jit[]
-    #     dispose(ci)
-    #     dispose(tm[])
-    # end
+    atexit() do
+        if !LLVM.has_julia_ojit()
+           ci = jit[]
+           dispose(ci)
+        end
+        dispose(tm[])
+    end
 end
 
 function move_to_threadsafe(ir)
@@ -135,25 +147,25 @@ function move_to_threadsafe(ir)
     end
 end
 
-function add_trampoline!(jd, (jljit, lctm, ism), entry, target)
+function add_trampoline!(jd, (lljit, lctm, ism), entry, target)
     flags = LLVM.API.LLVMJITSymbolFlags(
                 LLVM.API.LLVMJITSymbolGenericFlagsCallable |
                 LLVM.API.LLVMJITSymbolGenericFlagsExported, 0)
 
     alias = LLVM.API.LLVMOrcCSymbolAliasMapPair(
-                mangle(jljit, entry),
+                mangle(lljit, entry),
                 LLVM.API.LLVMOrcCSymbolAliasMapEntry(
-                    mangle(jljit, target), flags))
+                    mangle(lljit, target), flags))
 
     mu = LLVM.reexports(lctm, ism, jd, [alias])
     LLVM.define(jd, mu)
 
-    LLVM.lookup(jljit, entry)
+    LLVM.lookup(lljit, entry)
 end
 
 function get_trampoline(job)
     compiler = jit[]
-    jljit = compiler.jit
+    lljit = compiler.jit
     lctm  = compiler.lctm
     ism   = compiler.ism
 
@@ -165,17 +177,17 @@ function get_trampoline(job)
     needs_augmented_primal = mode == API.DEM_ReverseModePrimal || mode == API.DEM_ReverseModeGradient
 
     # We could also use one dylib per job
-    jd = JITDylib(jljit)
+    jd = JITDylib(lljit)
 
     adjoint_sym = String(gensym(:adjoint))
     _adjoint_sym = String(gensym(:adjoint))
-    adjoint_addr = add_trampoline!(jd, (jljit, lctm, ism),
+    adjoint_addr = add_trampoline!(jd, (lljit, lctm, ism),
                                    _adjoint_sym, adjoint_sym)
 
     if needs_augmented_primal
         primal_sym = String(gensym(:augmented_primal))
         _primal_sym = String(gensym(:augmented_primal))
-        primal_addr = add_trampoline!(jd, (jljit, lctm, ism),
+        primal_addr = add_trampoline!(jd, (lljit, lctm, ism),
                                       _primal_sym, primal_sym)
     else
         primal_sym = nothing
@@ -203,13 +215,14 @@ function get_trampoline(job)
             end
 
             tsm = move_to_threadsafe(mod)
-            il = LLVM.IRTransformLayer(lljit)
+
+            il = if LLVM.has_julia_ojit()
+                LLVM.IRCompileLayer(lljit)
+            else
+                LLVM.IRTransformLayer(lljit)
+            end
             LLVM.emit(il, mr, tsm)
         end
-
-        tsm = move_to_threadsafe(mod)
-        il = LLVM.IRCompileLayer(jljit)
-        LLVM.emit(il, mr, tsm)
         return nothing
     end
 
@@ -221,11 +234,11 @@ function get_trampoline(job)
 
     symbols = [
         LLVM.API.LLVMOrcCSymbolFlagsMapPair(
-            mangle(jljit, adjoint_sym), flags),
+            mangle(lljit, adjoint_sym), flags),
     ]
     if needs_augmented_primal
         push!(symbols, LLVM.API.LLVMOrcCSymbolFlagsMapPair(
-            mangle(jljit, primal_sym), flags),)
+            mangle(lljit, primal_sym), flags),)
     end
 
     mu = LLVM.CustomMaterializationUnit(adjoint_sym, symbols,
@@ -235,10 +248,10 @@ function get_trampoline(job)
 end
 
 function add!(mod)
-    jljit = jit[].jit
-    jd = LLVM.JITDylib(jljit)
+    lljit = jit[].jit
+    jd = LLVM.JITDylib(lljit)
     tsm = move_to_threadsafe(mod)
-    LLVM.add!(jljit, jd, tsm)
+    LLVM.add!(lljit, jd, tsm)
     return nothing
 end
 
