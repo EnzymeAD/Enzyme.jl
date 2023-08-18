769f68613ac4c69b7733030f4aaf1b9b99c5fd95
William S. Moses <gh@wsmoses.com>
2023-08-18 18:24:15 +0900

    Add wip rrule importer

--- test/ext/chainrulescore.jl
+++ test/ext/chainrulescore.jl
@@ -64,6 +64,67 @@ fdiff(f, x::Number) = autodiff(Forward, f, Duplicated, Duplicated(x, one(x)))[2]
     end
 end
 
+rdiff(f, x::Number) = autodiff(Reverse, f, Active, Active(x))[1][1]
+
+@testset "import_rrule" begin
+    f1(x) = 2*x
+    ChainRulesCore.@scalar_rule f1(x)  (5*one(x),)
+    Enzyme.@import_frule typeof(f1) Any
+    @test rdiff(f1, 1f0) === 5f0
+    @test rdiff(f1, 1.0) === 5.0
+
+    # specific signature    
+    f2(x) = 2*x
+    ChainRulesCore.@scalar_rule f2(x)  (5*one(x),)
+    Enzyme.@import_frule typeof(f2) Float32
+    @test rdiff(f2, 1f0) === 5f0
+    @test rdiff(f2, 1.0) === 2.0
+
+    # two arguments
+    f3(x, y) = 2*x + y
+    ChainRulesCore.@scalar_rule f3(x, y)  (5*one(x), y)
+    Enzyme.@import_frule typeof(f3) Any Any    
+    @test rdiff(x -> f3(x, 1.0), 2.) === 5.0
+    @test rdiff(y -> f3(1.0, y), 2.) === 2.0
+
+    @testset "batch duplicated" begin 
+        x = [1.0, 2.0, 0.0]        
+        Enzyme.@import_rrule typeof(Base.sort) Any
+
+        test_reverse(Base.sort, Duplicated, (x, Duplicated))
+        # Unsupported by EnzymeTestUtils
+        # test_reverse(Base.sort, Duplicated, (x, DuplicatedNoNeed))
+        test_reverse(Base.sort, DuplicatedNoNeed, (x, Duplicated))
+        # Unsupported by EnzymeTestUtils
+        # test_reverse(Base.sort, DuplicatedNoNeed, (x, DuplicatedNoNeed))
+        test_reverse(Base.sort, Const, (x, Duplicated))
+        # Unsupported by EnzymeTestUtils
+        # test_reverse(Base.sort, Const, (x, DuplicatedNoNeed))
+
+        test_reverse(Base.sort, Const, (x, Const))
+
+        # ChainRules does not support this case (returning notangent)
+        # test_reverse(Base.sort, Duplicated, (x, Const))
+        # test_reverse(Base.sort, DuplicatedNoNeed, (x, Const))
+
+        test_reverse(Base.sort, BatchDuplicated, (x, BatchDuplicated))
+        # Unsupported by EnzymeTestUtils
+        # test_reverse(Base.sort, BatchDuplicated, (x, BatchDuplicatedNoNeed))
+        test_reverse(Base.sort, BatchDuplicatedNoNeed, (x, BatchDuplicated))
+        # Unsupported by EnzymeTestUtils
+        # test_reverse(Base.sort, BatchDuplicatedNoNeed, (x, BatchDuplicatedNoNeed))
+        test_reverse(Base.sort, Const, (x, BatchDuplicated))
+        # Unsupported by EnzymeTestUtils
+        # test_reverse(Base.sort, Const, (x, BatchDuplicatedNoNeed))        
+        
+        # ChainRules does not support this case (returning notangent)
+        # test_reverse(Base.sort, BatchDuplicated, (x, Const))
+        # test_reverse(Base.sort, BatchDuplicatedNoNeed, (x, Const))
+    end
+end
+
+
+
 
 
 
