8c32f4252a53eb1cb9a880b38d5ebbb23e668e1b
Seth Axen <seth@sethaxen.com>
2023-09-12 16:24:22 +0200

    Workaround broken tests

--- test/blas.jl
+++ test/blas.jl
@@ -1,6 +1,7 @@
 using Enzyme
 using EnzymeTestUtils
 using LinearAlgebra
+using MetaTesting
 using Test
 
 @testset "BLAS rules" begin
@@ -29,14 +30,62 @@ using Test
                     @testset for (sz, inc) in ((10, 1), ((2, 20), -2))
                         a = randn(T)
                         x = randn(T, sz)
-                        test_forward(BLAS.scal!, Tret, n, (a, Ta), (x, Tx), inc; atol, rtol)
+                        @test !fails() do
+                            test_forward(
+                                BLAS.scal!, Tret, n, (a, Ta), (x, Tx), inc; atol, rtol
+                            )
+                        end broken = (T <: ComplexF32 && !(Ta <: Const) && !(Tx <: Const))
+                    end
+                end
+                @testset "BLAS.scal!(a, x)" begin
+                    a = randn(T)
+                    x = randn(T, n)
+                    @test !fails() do
+                        test_forward(BLAS.scal!, Tret, (a, Ta), (x, Tx); atol, rtol)
+                    end broken = (T <: ComplexF32 && !(Ta <: Const) && !(Tx <: Const))
+                end
+            end
+        end
+
+        @testset "reverse" begin
+            @testset for Tret in (Const,),
+                Ta in (Const, Active),
+                Tx in (Duplicated, BatchDuplicated),
+                T in BLASFloats
+
+                are_activities_compatible(Tret, Ta, Tx) || continue
+                atol = rtol = sqrt(eps(real(T)))
+
+                if T <: Complex && Ta <: Active && Tx <: BatchDuplicated
+                    # avoid failure that crashes Julia
+                    @test false skip = true
+                    continue
+                end
+
+                @testset "BLAS.scal!(n, a, x, incx)" begin
+                    @testset for (sz, inc) in ((10, 1), ((2, 20), -2))
+                        a = randn(T)
+                        x = randn(T, sz)
+                        @test !fails() do
+                            test_reverse(
+                                Tret, n, (a, Ta), (x, Tx), inc; atol, rtol
+                            ) do n, a, x, inc
+                                BLAS.scal!(n, a, x, inc)
+                                return nothing
+                            end
+                        end broken = (Tx <: BatchDuplicated && sz isa Int)
                     end
                 end
 
                 @testset "BLAS.scal!(a, x)" begin
                     a = randn(T)
                     x = randn(T, n)
-                    test_forward(BLAS.scal!, Tret, (a, Ta), (x, Tx); atol, rtol)
+                    @test !fails() do
+                        test_reverse(Tret, (a, Ta), (x, Tx); atol, rtol) do a, x
+                            BLAS.scal!(a, x)
+                            return nothing
+                        end
+                    end broken = (Tx <: BatchDuplicated)
                 end
             end
         end
