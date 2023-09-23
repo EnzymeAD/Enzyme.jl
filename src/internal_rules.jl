95948cb59b5fa1d33b3b2c77ba506c4b4db8a354
William S. Moses <gh@wsmoses.com>
2023-09-23 16:48:07 -0500

    Primitive deepcopy support

--- src/internal_rules.jl
+++ src/internal_rules.jl
@@ -99,3 +99,110 @@ end
 function EnzymeRules.inactive_noinl(::typeof(Base.size), args...)
     return nothing
 end
+
+
+# Note all of these forward mode definitions do not support runtime activity as
+# the do not keep the primal if shadow(x.y) == primal(x.y)
+function EnzymeRules.forward(::Const{typeof(Base.deepcopy)}, ::Type{<:DuplicatedNoNeed}, x::Duplicated)
+    return deepcopy(x.dval)
+end
+
+function EnzymeRules.forward(::Const{typeof(Base.deepcopy)}, ::Type{<:BatchDuplicatedNoNeed}, x::BatchDuplicated{T, N}) where {T, N}
+    ntuple(Val(N)) do _
+        deepcopy(x.dval)
+    end
+end
+
+# Deepcopy preserving the primal if runtime inactive
+@inline function deepcopy_rtact(copied::RT, primal::RT, seen::IdDict, shadow::RT) where {RT <: Integer}
+    return Base.deepcopy_internal(shadow, seen)
+end
+@inline function deepcopy_rtact(copied::RT, primal::RT, seen::IdDict, shadow::RT) where {RT <: AbstractFloat}
+    return Base.deepcopy_internal(shadow, seen)
+end
+@inline function deepcopy_rtact(copied::RT, primal::RT, seen::IdDict, shadow::RT) where {RT <: Array}
+    if !haskey(seen, shadow)
+        if primal === shadow
+            return seen[shadow] = copied
+        end
+        newa = RT(undef, size(shadow))
+        seen[shadow] = newa
+        for i in eachindex(shadow)
+            @inbounds newa[i] = deepcopy_rtact(copied[i], primal[i], seen, shadow[i])
+        end
+    end
+    return seen[shadow]
+end
+
+function EnzymeRules.forward(func::Const{typeof(Base.deepcopy)}, ::Type{<:Duplicated}, x::Duplicated)
+    primal = func.val(x.val)
+    return Duplicated(primal, deepcopy_rtact(primal, x.val, IdDict(), x.dval))
+end
+
+function EnzymeRules.forward(func::Const{typeof(Base.deepcopy)}, ::Type{<:BatchDuplicated}, x::BatchDuplicated{T, N}) where {T,N}
+    primal = func.val(x.val)
+    return BatchDuplicated(primal, ntuple(Val(N)) do i
+        deepcopy_rtact(primal, x.val, IdDict(), x.dval[i])
+    end)
+end
+
+function EnzymeRules.augmented_primal(config, func::Const{typeof(Base.deepcopy)}, ::Type{RT}, x::Annotation{Ty}) where {RT, Ty}
+    primal = if EnzymeRules.needs_primal(config)
+        func.val(x.val)
+    else
+        nothing
+    end
+
+    @assert !(typeof(x) <: Active)
+
+    source = if EnzymeRules.needs_primal(config)
+        primal
+    else
+        x.val
+    end
+
+    shadow = ntuple(Val(EnzymeRules.width(config))) do _
+        Base.@_inline_meta
+        Enzyme.Compiler.make_zero(Core.Typeof(source), IdDict(), source,
+            #=copy_if_inactive=#Val(!EnzymeRules.needs_primal(config))
+        )
+    end
+
+    if EnzymeRules.width(config) == 1
+        shadow = shadow[1]
+    end
+
+    return EnzymeRules.AugmentedReturn(primal, shadow, shadow)
+end
+
+
+@inline function accumulate_into(into::RT, seen::IdDict, from::RT)::Tuple{RT,RT} where {RT<:Array}
+    if !haskey(seen, into)
+        seen[into] = (into, from)
+        for i in eachindex(from)
+            tup = accumulate_into(into[i], seen, from[i])
+            @inbounds into[i] = tup[1]
+            @inbounds from[i] = tup[2]
+        end
+    end
+    return seen[into]
+end
+
+@inline function accumulate_into(into::RT, seen::IdDict, from::RT)::Tuple{RT,RT} where {RT<:AbstractFloat}
+    if !haskey(seen, into)
+        seen[into] = (into+from, RT(0))
+    end
+    return seen[into]
+end
+
+function EnzymeRules.reverse(config, func::Const{typeof(Base.deepcopy)}, ::Type{RT}, shadow, x::Annotation{Ty}) where {RT, Ty}
+    if EnzymeRules.width(config) == 1
+        accumulate_into(x.dval, IdDict(), shadow)
+    else
+        for i in 1:EnzymeRules.width(config)
+            accumulate_into(x.dval[i], IdDict(), shadow[i])
+        end
+    end
+
+    return (nothing,)
+end
