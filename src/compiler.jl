100294e2517572f42508f4c8af7d3ab26c0e8a0a
William S. Moses <gh@wsmoses.com>
2023-10-13 16:16:40 -0700

    Nofree array del

--- src/compiler.jl
+++ src/compiler.jl
@@ -144,6 +144,7 @@ const known_ops = Dict(
 end
 
 const nofreefns = Set{String}((
+    "jl_array_del_end","ijl_array_del_end",
     "jl_get_world_counter", "ijl_get_world_counter",
     "memhash32_seed", "memhash_seed",
     "ijl_module_parent", "jl_module_parent",
