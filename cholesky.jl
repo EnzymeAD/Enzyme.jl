44b8b5899f8d6f715f789e514f4d7f9910628d24
Michel Schanen <mschanen@anl.gov>
2023-12-20 11:04:15 -0600

    Fix

--- cholesky.jl
+++ cholesky.jl
@@ -5,9 +5,6 @@ using SparseArrays
 using Test
 using FiniteDifferences
 
-include("utils.jl")
-include("utils_check.jl")
-
 function symmetric_definite(n :: Int=10; FC=Float64)
   α = FC <: Complex ? FC(im) : one(FC)
   A = spdiagm(-1 => α * ones(FC, n-1), 0 => 4 * ones(FC, n), 1 => conj(α) * ones(FC, n-1))
@@ -101,11 +98,11 @@ A = Matrix(A)
 x = driver(A, b)
 fdm = forward_fdm(2, 1);
 fdJ = FiniteDifferences.jacobian(fdm, b_one, copy(b))[1]
-adJ = adJdxdb(A, b)
+fwdJ = fwdJdxdb(A, b)
 J = Jdxdb(A, b)
 
-@test isapprox(adJ, J)
-@test isapprox(adJ, fdJ)
+@test isapprox(fwdJ, J)
+@test isapprox(fwdJ, fdJ)
 
 fact = cholesky(A)
 x = fact\b
@@ -115,7 +112,7 @@ c = Matrix(fact)*x
 isapprox(c, b)
 
 cc = zeros(length(c))
-@show @which mul!(cc, fact.U, x)
+mul!(cc, fact.U, x)
 mul!(cc, fact.L, cc)
 
 isapprox(cc, c)
