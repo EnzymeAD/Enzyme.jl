f8be0f2d77a37f2965a8d4975a53922adddd0524
William S. Moses <gh@wsmoses.com>
2024-01-01 14:20:31 -0500

    Fix 1.10 for x86

--- src/compiler.jl
+++ src/compiler.jl
@@ -2458,9 +2458,17 @@ function julia_allocator(B, LLVMType, Count, AlignedSize, IsDefault, ZI)
             needs_dynamic_size_workaround = !isa(Size, LLVM.ConstantInt) || convert(Int, Size) != 1
         end
 
+        T_size_t = convert(LLVM.LLVMType, Int)
+        if value_type(Size) != T_size_t
+            Size = trunc!(B, Size, T_size_t)
+        end
+
         obj = emit_allocobj!(B, tag, Size, needs_dynamic_size_workaround)
 
         if ZI != C_NULL
+            if value_type(AlignedSize) != T_size_t
+                AlignedSize = trunc!(B, AlignedSize, T_size_t)
+            end
             unsafe_store!(ZI, zero_allocation(B, TT, LLVMType, obj, AlignedSize, Size, #=ZeroAll=#false))
         end
         AS = Tracked
