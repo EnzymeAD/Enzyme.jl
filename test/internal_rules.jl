7ec83164f50c93090c87da8d365490b95707a721
Simone Carlo Surace <simsurace@gmx.net>
2024-02-27 10:45:39 +0100

    Fix tests

--- test/internal_rules.jl
+++ test/internal_rules.jl
@@ -352,27 +352,40 @@ end
                     end
                 end
                 if TE == Float64
-                    @testset "$wrapper wrapper" for wrapper in (Symmetric, Hermitian,)
-                        function fw(A)
-                            C = cholesky(wrapper(A * adjoint(A)))
-                            return sum(abs2, C.L * C.U)
-                        end
-                        gw(A) = sum(abs2, wrapper(A * adjoint(A)))
+                    function f_sym(A)
+                        C = cholesky(Symmetric(A * adjoint(A)))
+                        return sum(abs2, C.L * C.U)
+                    end
+                    g_sym(A) = sum(abs2, Symmetric(A * adjoint(A)))
+                    function f_her(A)
+                        C = cholesky(Hermitian(A * adjoint(A)))
+                        return sum(abs2, C.L * C.U)
+                    end
+                    g_her(A) = sum(abs2, Hermitian(A * adjoint(A)))
 
-                        @testset "Forward mode" begin
-                            dA = rand(TE, size(A)...)
-                            d1 = autodiff(Forward, fw, Duplicated, Duplicated(A, dA))
-                            d2 = autodiff(Forward, gw, Duplicated, Duplicated(A, dA))
-                            @test all(d1 .≈ d2)
-                        end
+                    @testset "Forward mode" begin
+                        dA = rand(TE, size(A)...)
+                        d1 = autodiff(Forward, f_sym, Duplicated, Duplicated(A, dA))
+                        d2 = autodiff(Forward, g_sym, Duplicated, Duplicated(A, dA))
+                        @test all(d1 .≈ d2)
 
-                        @testset "Reverse mode" begin
-                            dA1 = zero(A)
-                            dA2 = zero(A)
-                            autodiff(Reverse, fw, Active, Duplicated(A, dA1))
-                            autodiff(Reverse, gw, Active, Duplicated(A, dA2))
-                            @test dA1 ≈ dA2
-                        end
+                        d1 = autodiff(Forward, f_her, Duplicated, Duplicated(A, dA))
+                        d2 = autodiff(Forward, g_her, Duplicated, Duplicated(A, dA))
+                        @test all(d1 .≈ d2)
+                    end
+
+                    @testset "Reverse mode" begin
+                        dA1 = zero(A)
+                        dA2 = zero(A)
+                        autodiff(Reverse, f_sym, Active, Duplicated(A, dA1))
+                        autodiff(Reverse, g_sym, Active, Duplicated(A, dA2))
+                        @test dA1 ≈ dA2
+
+                        dA1 = zero(A)
+                        dA2 = zero(A)
+                        autodiff(Reverse, f_her, Active, Duplicated(A, dA1))
+                        autodiff(Reverse, g_her, Active, Duplicated(A, dA2))
+                        @test dA1 ≈ dA2
                     end
                 end
             end
