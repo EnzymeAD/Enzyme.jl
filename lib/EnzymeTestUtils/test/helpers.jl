641befd12f45ce54ef001dc45f565eb48c32ffbd
Seth Axen <seth@sethaxen.com>
2024-03-06 13:17:39 +0100

    Add function for structured array testing

--- lib/EnzymeTestUtils/test/helpers.jl
+++ lib/EnzymeTestUtils/test/helpers.jl
@@ -1,3 +1,5 @@
+using LinearAlgebra
+
 struct TestStruct{X,A}
     x::X
     a::A
@@ -14,3 +16,14 @@ end
 
 f_array(x) = sum(abs2, x)
 f_multiarg(x::AbstractArray, a) = abs2.(a .* x)
+
+function f_structured_array(x::Hermitian)
+    y = x * 3
+    # mutate the unused triangle, which ensures that our Jacobian differs from FiniteDifferences
+    if y.uplo == 'U'
+        LowerTriangular(y.data) .*= 2
+    else
+        UpperTriangular(y.data) .*= 2
+    end
+    return y
+end
