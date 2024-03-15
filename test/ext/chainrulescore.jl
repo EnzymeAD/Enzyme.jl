9be1bb67752db7d051265b74a9193fe7b806d5fc
Carlo Lucibello <carlo.lucibello@gmail.com>
2024-03-15 07:33:48 +0100

    address review comments

--- test/ext/chainrulescore.jl
+++ test/ext/chainrulescore.jl
@@ -5,15 +5,13 @@ using ChainRulesCore
 using LinearAlgebra
 using EnzymeTestUtils
 
-# utility functions
 fdiff(f, x::Number) = autodiff(Forward, f, Duplicated, Duplicated(x, one(x)))[2]
-rdiff(f, x::Number) = autodiff(Reverse, f, Active, Active(x))[1][1]
-rdiff(f, x::AbstractArray) = autodiff(Reverse, f, Active, Duplicated(x, zero(x)))[1][1]
 
 @testset "import_frule" begin
     f1(x) = 2*x
-    ChainRulesCore.@scalar_rule f1(x)  (5.0,)
+    ChainRulesCore.@scalar_rule f1(x)  (5*one(x),)
     Enzyme.@import_frule typeof(f1) Any
+    @test fdiff(f1, 1f0) === 5f0
     @test fdiff(f1, 1.0) === 5.0
 
     # specific signature    
@@ -26,11 +24,15 @@ rdiff(f, x::AbstractArray) = autodiff(Reverse, f, Active, Duplicated(x, zero(x))
     @testset "batch duplicated" begin 
         x = [1.0, 2.0, 0.0]        
         Enzyme.@import_frule typeof(Base.sort)  Any
-        for Tret in (Duplicated, DuplicatedNoNeed)
-            for Tx in (Duplicated, BatchDuplicated)
-                test_forward(sort, Tret, (x, Tx))
-            end
-        end
+        test_forward(Base.sort, Duplicated, (x, Duplicated))
+        # test_forward(Base.sort, Duplicated, (x, BatchDuplicated))
+        # test_forward(Base.sort, DuplicatedNoNeed, (x, Duplicated))
+        # test_forward(Base.sort, DuplicatedNoNeed, (x, BatchDuplicated))
+        # for Tret in (Duplicated, DuplicatedNoNeed)
+        #     for Tx in (Duplicated, BatchDuplicated)
+        #         test_forward(sort, Tret, (x, Tx))
+        #     end
+        # end
     end
 end
 
