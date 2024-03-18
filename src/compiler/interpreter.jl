4b2849a445dfe2367eda13e52bb371225a2cba54
Shuhei Kadowaki <aviatesk@gmail.com>
2024-03-18 19:23:49 +0900

    adjustments to the latest inlining interface changes

--- src/compiler/interpreter.jl
+++ src/compiler/interpreter.jl
@@ -68,20 +68,17 @@ else
 end
 
 # No need to do any locking since we're not putting our results into the runtime cache
-Core.Compiler.lock_mi_inference(interp::EnzymeInterpreter, mi::MethodInstance) = nothing
-Core.Compiler.unlock_mi_inference(interp::EnzymeInterpreter, mi::MethodInstance) = nothing
+Core.Compiler.lock_mi_inference(::EnzymeInterpreter, ::MethodInstance) = nothing
+Core.Compiler.unlock_mi_inference(::EnzymeInterpreter, ::MethodInstance) = nothing
 
-function Core.Compiler.add_remark!(interp::EnzymeInterpreter, sv::InferenceState, msg)
-end
-
-Core.Compiler.may_optimize(interp::EnzymeInterpreter) = true
-Core.Compiler.may_compress(interp::EnzymeInterpreter) = true
+Core.Compiler.may_optimize(::EnzymeInterpreter) = true
+Core.Compiler.may_compress(::EnzymeInterpreter) = true
 # From @aviatesk:
 #     `may_discard_trees = true`` means a complicated (in terms of inlineability) source will be discarded,
 #      but as far as I understand Enzyme wants "always inlining, except special cased functions",
 #      so I guess we really don't want to discard sources?
-Core.Compiler.may_discard_trees(interp::EnzymeInterpreter) = false
-Core.Compiler.verbose_stmt_info(interp::EnzymeInterpreter) = false
+Core.Compiler.may_discard_trees(::EnzymeInterpreter) = false
+Core.Compiler.verbose_stmt_info(::EnzymeInterpreter) = false
 
 if isdefined(Base.Experimental, Symbol("@overlay"))
 Core.Compiler.method_table(interp::EnzymeInterpreter, sv::InferenceState) =
@@ -123,7 +120,7 @@ function isKWCallSignature(@nospecialize(TT))
     return TT <: Tuple{typeof(Core.kwcall), Any, Any, Vararg}
 end
 
-function simplify_kw(specTypes)
+function simplify_kw(@nospecialize specTypes)
     if isKWCallSignature(specTypes)
         return Base.tuple_type_tail(Base.tuple_type_tail(specTypes))
     else
@@ -133,42 +130,111 @@ end
 
 # https://github.com/JuliaLang/julia/pull/46965
 import Core.Compiler: CallInfo
-function Core.Compiler.inlining_policy(interp::EnzymeInterpreter,
-    @nospecialize(src), @nospecialize(info::CallInfo), stmt_flag::UInt8, mi::MethodInstance, argtypes::Vector{Any})
+struct NoInlineCallInfo <: CallInfo
+    info::CallInfo # wrapped call
+    tt # ::Type
+    kind::Symbol
+    NoInlineCallInfo(@nospecialize(info::CallInfo), @nospecialize(tt), kind::Symbol) = new(info, tt, kind)
+end
+Core.Compiler.nsplit_impl(info::NoInlineCallInfo) = Core.Compiler.nsplit(info.info)
+Core.Compiler.getsplit_impl(info::NoInlineCallInfo, idx::Int) = Core.Compiler.getsplit(info.info, idx)
+Core.Compiler.getresult_impl(info::NoInlineCallInfo, idx::Int) = Core.Compiler.getresult(info.info, idx)
+struct AlwaysInlineCallInfo <: CallInfo
+    info::CallInfo # wrapped call
+    tt # ::Type
+    AlwaysInlineCallInfo(@nospecialize(info::CallInfo), @nospecialize(tt)) = new(info, tt)
+end
+Core.Compiler.nsplit_impl(info::AlwaysInlineCallInfo) = Core.Compiler.nsplit(info.info)
+Core.Compiler.getsplit_impl(info::AlwaysInlineCallInfo, idx::Int) = Core.Compiler.getsplit(info.info, idx)
+Core.Compiler.getresult_impl(info::AlwaysInlineCallInfo, idx::Int) = Core.Compiler.getresult(info.info, idx)
 
+using Core.Compiler: ArgInfo, StmtInfo, AbsIntState
+function Core.Compiler.abstract_call_gf_by_type(interp::EnzymeInterpreter, @nospecialize(f),
+    arginfo::ArgInfo, si::StmtInfo, @nospecialize(atype), sv::AbsIntState, max_methods::Int)
+    ret = @invoke Core.Compiler.abstract_call_gf_by_type(interp::AbstractInterpreter, f::Any,
+        arginfo::ArgInfo, si::StmtInfo, atype::Any, sv::AbsIntState, max_methods::Int)
+    callinfo = ret.info
     method_table = Core.Compiler.method_table(interp)
-    specTypes = simplify_kw(mi.specTypes)
-
+    specTypes = simplify_kw(atype)
     if is_primitive_func(specTypes)
-        @safe_debug "Blocking inlining for primitive func" mi.specTypes
-        return nothing
+        callinfo = NoInlineCallInfo(callinfo, atype, :primitive)
+    elseif is_alwaysinline_func(specTypes)
+        callinfo = AlwaysInlineCallInfo(callinfo, atype)
+    elseif EnzymeRules.is_inactive_from_sig(specTypes; world = interp.world, method_table)
+        callinfo = NoInlineCallInfo(callinfo, atype, :inactive)
+    elseif interp.mode == API.DEM_ForwardMode
+        if EnzymeRules.has_frule_from_sig(specTypes; world = interp.world, method_table)
+            callinfo = NoInlineCallInfo(callinfo, atype, :frule)
+        end
+    elseif EnzymeRules.has_rrule_from_sig(specTypes; world = interp.world, method_table)
+        callinfo = NoInlineCallInfo(callinfo, atype, :rrule)
     end
+    return Core.Compiler.CallMeta(ret.rt, ret.exct, ret.effects, callinfo)
+end
 
-    if is_alwaysinline_func(specTypes)
-        @safe_debug "Forcing inlining for primitive func" mi.specTypes
-        @assert src !== nothing
+function Core.Compiler.inlining_policy(interp::EnzymeInterpreter,
+    @nospecialize(src), @nospecialize(info::CallInfo), stmt_flag::UInt32)
+    if info isa NoInlineCallInfo
+        if info.kind === :primitive
+            @safe_debug "Blocking inlining for primitive func" info.tt
+        elseif info.kind === :inactive
+            @safe_debug "Blocking inlining due to inactive rule" info.tt
+        elseif info.kind === :frule
+            @safe_debug "Blocking inlining due to frule" info.tt
+        else
+            @assert info.kind === :rrule
+            @safe_debug "Blocking inlining due to rrule" info.tt
+        end
+        return nothing
+    elseif info isa AlwaysInlineCallInfo
+        @safe_debug "Forcing inlining for primitive func" info.tt
         return src
     end
+    return @invoke Core.Compiler.inlining_policy(interp::AbstractInterpreter,
+        src::Any, info::CallInfo, stmt_flag::UInt32)
+end
 
-    if EnzymeRules.is_inactive_from_sig(specTypes; world = interp.world, method_table)
+else # @static if VERSION ≥ v"1.10.0-DEV.879"
+
+function enzyme_inlining_policy(interp::EnzymeInterpreter, mi::MethodInstance)
+    method_table = Core.Compiler.method_table(interp)
+    specTypes = simplify_kw(mi.specTypes)
+    if is_primitive_func(specTypes)
+        @safe_debug "Blocking inlining for primitive func" mi.specTypes
+        return false
+    elseif is_alwaysinline_func(specTypes)
+        @safe_debug "Forcing inlining for primitive func" mi.specTypes
+        return true
+    elseif EnzymeRules.is_inactive_from_sig(specTypes; world = interp.world, method_table)
         @safe_debug "Blocking inlining due to inactive rule" mi.specTypes
-        return nothing
-    end
-
-    if interp.mode == API.DEM_ForwardMode
+        return false
+    elseif interp.mode == API.DEM_ForwardMode
         if EnzymeRules.has_frule_from_sig(specTypes; world = interp.world, method_table)
             @safe_debug "Blocking inlining due to frule" mi.specTypes
-            return nothing
+            return false
         end
-    else
-        if EnzymeRules.has_rrule_from_sig(specTypes; world = interp.world, method_table)
-            @safe_debug "Blocking inling due to rrule" mi.specTypes
+    elseif EnzymeRules.has_rrule_from_sig(specTypes; world = interp.world, method_table)
+        @safe_debug "Blocking inling due to rrule" mi.specTypes
+        return false
+    end
+    return nothing
+end
+
+@static if VERSION ≥ v"1.9.0-DEV.1535"
+
+using Core.Compiler: CallInfo
+function Core.Compiler.inlining_policy(interp::EnzymeInterpreter,
+    @nospecialize(src), @nospecialize(info::CallInfo), stmt_flag::UInt8, mi::MethodInstance, argtypes::Vector{Any})
+    ret = enzyme_inlining_policy(interp, mi)
+    if ret isa Bool
+        if ret
+            @assert src !== nothing
+            return src
+        else
             return nothing
         end
     end
-
     return Base.@invoke Core.Compiler.inlining_policy(interp::AbstractInterpreter,
         src::Any, info::CallInfo, stmt_flag::UInt8, mi::MethodInstance, argtypes::Vector{Any})
 end
 
-end
