18e4f6e0191caf1a5779b012aa1c25e61e6b1293
Valentin Churavy <vchuravy@users.noreply.github.com>
2024-03-31 16:12:49 -0400

    Fix formatting of extension

--- lib/EnzymeCore/ext/AdaptExt.jl
+++ lib/EnzymeCore/ext/AdaptExt.jl
@@ -1,11 +1,13 @@
 module AdaptExt
+
 using Adapt
 using EnzymeCore
 
-	Adapt.adapt_structure(to, x::Const) = Const(adapt(to, x.val))
-	Adapt.adapt_structure(to, x::Active) = Active(adapt(to, x.val))
-	Adapt.adapt_structure(to, x::Duplicated) = Duplicated(adapt(to, x.val), adapt(to, x.dval))
-	Adapt.adapt_structure(to, x::DuplicatedNoNeed) = DuplicatedNoNeed(adapt(to, x.val), adapt(to, x.dval))
-	Adapt.adapt_structure(to, x::BatchDuplicated) = BatchDuplicated(adapt(to, x.val), adapt(to, x.dval))
-	Adapt.adapt_structure(to, x::BatchDuplicatedNoNeed) = BatchDuplicatedNoNeed(adapt(to, x.val), adapt(to, x.dval))
-end
+Adapt.adapt_structure(to, x::Const) = Const(adapt(to, x.val))
+Adapt.adapt_structure(to, x::Active) = Active(adapt(to, x.val))
+Adapt.adapt_structure(to, x::Duplicated) = Duplicated(adapt(to, x.val), adapt(to, x.dval))
+Adapt.adapt_structure(to, x::DuplicatedNoNeed) = DuplicatedNoNeed(adapt(to, x.val), adapt(to, x.dval))
+Adapt.adapt_structure(to, x::BatchDuplicated) = BatchDuplicated(adapt(to, x.val), adapt(to, x.dval))
+Adapt.adapt_structure(to, x::BatchDuplicatedNoNeed) = BatchDuplicatedNoNeed(adapt(to, x.val), adapt(to, x.dval))
+
+end #module
