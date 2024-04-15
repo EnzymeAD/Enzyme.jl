9d6b969b2c36f47d8acf31254498aa4a81edae31
Gaurav Arya <gauravarya272@gmail.com>
2024-04-15 15:36:03 -0400

    Try narrowing inactive rules for `rand` (#1388)
    * Restrict scope of inactive markers on Randon.rand
    
    * Simplify
    
    * Keep the randn rules
    
    * Add test
    
    * Rm newline
    
    * Fix typo
    
    * Add Random import for test

--- test/internal_rules.jl
+++ test/internal_rules.jl
@@ -7,6 +7,7 @@ using FiniteDifferences
 using LinearAlgebra
 using SparseArrays
 using Test
+import Random
 
 struct TPair
     a::Float64
@@ -432,4 +433,18 @@ end
     end
 end
 end
+
+@testset "rand and randn rules" begin
+    # Distributed as x + unit normal + uniform
+    struct MyDistribution
+        x::Float64
+    end
+
+    Random.rand(rng::Random.AbstractRNG, d::MyDistribution) = d.x + randn() + rand()
+    Random.rand(d::MyDistribution) = rand(Random.default_rng(), d)
+
+    # Outer rand should be differentiated through, and inner rand and randn should be ignored.
+    @test autodiff(Enzyme.Reverse, x -> rand(MyDistribution(x)), Active, Active(1.0)) == ((1.0,),)
+end
+
 end # InternalRules
