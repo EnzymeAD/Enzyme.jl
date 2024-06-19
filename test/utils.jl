61a8f1a29fe07ade16a884ed4fd6a65d6ce46b5b
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-06-19 08:51:54 +0200

    Add arguments

--- test/utils.jl
+++ test/utils.jl
@@ -1,4 +1,11 @@
+using Enzyme: pick_chunksize
+using EnzymeCore: Forward
 using Test
 
-@test Enzyme.pick_chunksize(zeros(1)) == Val(16)
-@test Enzyme.pick_chunksize(zeros(100)) == Val(16)
+mode = Forward
+ftype = typeof(sum)
+argtypes = typeof.((Duplicated(ones(1), zeros(1)),))
+@test pick_chunksize(Val(1), mode, ftype, argtypes...) == 1
+
+argtypes = typeof.((Duplicated(ones(100), zeros(100)),))
+@test pick_chunksize(Val(100), mode, ftype, argtypes...) == 16
