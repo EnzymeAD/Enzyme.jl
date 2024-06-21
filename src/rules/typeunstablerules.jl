600deb02ad0b3ba19e676d3da61cb50b9f2de451
William S. Moses <gh@wsmoses.com>
2024-06-21 18:55:06 -0400

    fixup

--- src/rules/typeunstablerules.jl
+++ src/rules/typeunstablerules.jl
@@ -300,7 +300,8 @@ function common_newstructv_fwd(offset, B, orig, gutils, normalR, shadowR)
     if !newstruct_common(#=fwd=#true, #=run=#true, offset, B, orig, gutils, normalR, shadowR)
         origops = collect(operands(orig))
         abs_partial = [abs_typeof(v, true) for v in origops[offset+1:end-1]]
-        emit_error(B, orig, "Enzyme: Not yet implemented, mixed activity for jl_new_struct constants="*string(icvs)*" "*string(orig)*" "*string(abs)*" "*string([v for v in origops[offset+1:end-1]]))
+        icvs = [is_constant_value(gutils, v) for v in origops[offset+1:end-1]]
+        emit_error(B, orig, "Enzyme: Not yet implemented, mixed activity for jl_new_struct constants="*string(icvs)*" "*string(orig)*" "*string(abs_partial)*" "*string([v for v in origops[offset+1:end-1]]))
     end
 
     return false
