edf6904ecaf2bf806c306a1e5ae9de124c57ecef
William S. Moses <gh@wsmoses.com>
2024-06-22 22:16:37 -0400

    mark lookup worlds as inactive

--- src/compiler.jl
+++ src/compiler.jl
@@ -103,6 +103,7 @@ Dict{DataType, Tuple{Symbol, Int, Union{Nothing, Tuple{Symbol, DataType}}}}(
 end
 
 const nofreefns = Set{String}((
+    "ijl_gf_invoke_lookup_worlds", "jl_gf_invoke_lookup_worlds",
     "ijl_array_grow_at", "jl_array_grow_at",
     "ijl_try_substrtod", "jl_try_substrtod",
     "jl_f__apply_iterate",
@@ -180,6 +181,7 @@ const nofreefns = Set{String}((
 ))
 
 const inactivefns = Set{String}((
+    "ijl_gf_invoke_lookup_worlds", "jl_gf_invoke_lookup_worlds",
     "ijl_try_substrtod", "jl_try_substrtod",
     "ijl_tagged_gensym", "jl_tagged_gensym",
     "jl_get_world_counter", "ijl_get_world_counter",
