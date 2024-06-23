c2940efb7e8b4754488c48a51fd7bad19d559094
William Moses <gh@wsmoses.com>
2024-06-23 14:27:14 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,23 +9,17 @@ Enzyme.API.printall!(true)
 
 
 @noinline function mygetindex(R, len)
-  # out = Vector{Float64}(undef, 3)
-  out = Base.llvmcall(("
-declare i8* @malloc(i64)
-
-define i64 @f() {
-  %r = call i8* @malloc(i64 24)
-  %p = ptrtoint i8* %r to i64
-  ret i64 %p
-}", "f"), Ptr{Float64}, Tuple{})
+  out = Vector{Float64}(undef, 3)
   oi = 1
   i = 1
-  while i < unsafe_load(len)
-    v = Core.arrayref(false, R, i)
-    # Core.arrayset(false, out, v, oi)
-    unsafe_store!(out, v, oi)
-    oi+=1
-    i += 4
+  v = @inbounds Core.arrayref(false, R, 1)
+  while true
+    Core.arrayset(false, out, v, i)
+    # unsafe_store!(out, v, oi)
+    i += 1
+    if i >= unsafe_load(len)
+	break
+    end
   end
   out
 end
@@ -37,8 +31,8 @@ end
 end
 
 function whocallsmorethan30args(R)
-   return unsafe_load(mydiag(R))
-   # return @inbounds mydiag(R)[1]    
+   # return unsafe_load(mydiag(R))
+   return @inbounds mydiag(R)[1]    
 end
 
 @testset "generic" begin
