fee487654516806dc9ba0f77b903ea33896c0009
William Moses <gh@wsmoses.com>
2024-06-23 12:14:55 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,72 +8,11 @@ if isfile(preferences_file) && !isfile(test_preferences_file)
 end
 end
 
-# # work around https://github.com/JuliaLang/Pkg.jl/issues/1585
-# using Pkg
-# Pkg.develop(PackageSpec(; path=joinpath(dirname(@__DIR__), "lib", "EnzymeTestUtils")))
-
-using GPUCompiler
 using Enzyme
 using Test
-using FiniteDifferences
-using Aqua
-using SparseArrays
-using StaticArrays
-using Statistics
-using LinearAlgebra
-using InlineStrings
 
 using Enzyme_jll
-@info "Testing against" Enzyme_jll.libEnzyme
 
-function isapproxfn(fn, args...; kwargs...)
-    isapprox(args...; kwargs...)
-end
-# Test against FiniteDifferences
-function test_scalar(f, x; rtol=1e-9, atol=1e-9, fdm=central_fdm(5, 1), kwargs...)
-    ∂x, = autodiff(ReverseHolomorphic, f, Active, Active(x))[1]
-
-    finite_diff = if typeof(x) <: Complex
-      RT = typeof(x).parameters[1]
-      (fdm(dx -> f(x+dx), RT(0)) - im * fdm(dy -> f(x+im*dy), RT(0)))/2
-    else
-      fdm(f, x)
-    end
-
-    @test isapproxfn((Enzyme.Reverse, f), ∂x, finite_diff; rtol=rtol, atol=atol, kwargs...)
-
-    if typeof(x) <: Integer
-        x = Float64(x)
-    end
-
-    if typeof(x) <: Complex
-        ∂re, = autodiff(Forward, f, Duplicated(x, one(typeof(x))))
-        ∂im, = autodiff(Forward, f, Duplicated(x, im*one(typeof(x))))
-        ∂x = (∂re - im*∂im)/2
-    else
-        ∂x, = autodiff(Forward, f, Duplicated(x, one(typeof(x))))
-    end
-
-    @test isapproxfn((Enzyme.Reverse, f), ∂x, finite_diff; rtol=rtol, atol=atol, kwargs...)
-
-end
-
-genlatestsin(x)::Float64 = Base.invokelatest(sin, x)
-function genlatestsinx(xp)
-    x = @inbounds xp[1]
-    @inbounds xp[1] = 0.0
-    Base.invokelatest(sin, x)::Float64 + 1
-end
-
-function loadsin(xp)
-    x = @inbounds xp[1]
-    @inbounds xp[1] = 0.0
-    sin(x)
-end
-function invsin(xp)
-    xp = Base.invokelatest(convert, Vector{Float64}, xp)
-    loadsin(xp)
-end
 
 Enzyme.API.printall!(true)
 
@@ -81,9 +20,6 @@ Enzyme.API.printall!(true)
         temp = diag(R)     
          R_inv = [temp[1] 0. 0. 0. 0. 0.; 
              0. temp[2] 0. 0. 0. 0.; 
-             0. 0. temp[3] 0. 0. 0.; 
-             0. 0. 0. temp[4] 0. 0.; 
-             0. 0. 0. 0. temp[5] 0.; 
          ]
     
         return sum(R_inv)
@@ -102,9 +38,9 @@ Enzyme.API.printall!(true)
 	@show dR
     	@test 1.0 ≈ dR[1, 1]
     	@test 1.0 ≈ dR[2, 2]
-    	@test 1.0 ≈ dR[3, 3]
-    	@test 1.0 ≈ dR[4, 4]
-    	@test 1.0 ≈ dR[5, 5]
-    	@test 0.0 ≈ dR[6, 6]
+    	#@test 1.0 ≈ dR[3, 3]
+    	#@test 1.0 ≈ dR[4, 4]
+    	#@test 1.0 ≈ dR[5, 5]
+    	#@test 0.0 ≈ dR[6, 6]
     end
 end
