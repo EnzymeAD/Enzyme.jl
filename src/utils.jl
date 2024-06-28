cfe3b446fe7df3879a89d30a512ad2f53ffa16f2
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-06-28 08:55:28 +0200

    Rename and fix tests

--- src/utils.jl
+++ src/utils.jl
@@ -1,18 +1,16 @@
 """
-    pick_chunksize(totalsize, mode, ftype, return_activity, argtypes...)
+    pick_batchsize(totalsize, mode, ftype, return_activity, argtypes...)
 
-Return a reasonable chunk size for batched differentiation.
+Return a reasonable batch size for batched differentiation.
 
 !!! warning
     This function is experimental, and not part of the public API.
 """
-function pick_chunksize(
-    totalsize::Integer,
-    mode::Mode,
-    ftype::Type,
-    return_activity,::Type{<:Annotation},
-    argtypes::Vararg{Type{<:Annotation}, Nargs}
-) where {Nargs}
+function pick_batchsize(totalsize::Integer,
+                        mode::Mode,
+                        ftype::Type,
+                        return_activity, ::Type{<:Annotation},
+                        argtypes::Vararg{Type{<:Annotation},Nargs}) where {Nargs}
     return min(totalsize, 16)
 end
 
