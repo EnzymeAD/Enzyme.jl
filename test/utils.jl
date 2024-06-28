a35fb46fa9d2ecc219f425a0959c383b867e18f3
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-06-28 08:55:28 +0200

    Rename and fix tests

--- test/utils.jl
+++ test/utils.jl
@@ -1,11 +1,11 @@
-using Enzyme: pick_chunksize
+using Enzyme: Active, Duplicated, pick_batchsize
 using EnzymeCore: Forward
 using Test
 
 mode = Forward
 ftype = typeof(sum)
 argtypes = typeof.((Duplicated(ones(1), zeros(1)),))
-@test pick_chunksize(1, mode, ftype, Active, argtypes...) == 1
+@test pick_batchsize(1, mode, ftype, Active, argtypes...) == 1
 
 argtypes = typeof.((Duplicated(ones(100), zeros(100)),))
-@test pick_chunksize(100, mode, ftype, Active, argtypes...) == 16
+@test pick_batchsize(100, mode, ftype, Active, argtypes...) == 16
