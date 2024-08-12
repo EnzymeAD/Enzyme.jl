7f99e89a630e9057174b0ed376b3591cebd30c77
William Moses <gh@wsmoses.com>
2024-08-12 00:52:47 -0400

    Update bfloat16s.jl

--- test/ext/bfloat16s.jl
+++ test/ext/bfloat16s.jl
@@ -2,12 +2,6 @@ using Enzyme
 using Test
 using BFloat16s
 
-msum(x) = sum(x)
-
-@test Enzyme.gradient(Reverse, msum, ones(BFloat16, 10)) ≈ ones(BFloat16, 10)
-
-@test Enzyme.gradient(Forward, msum, ones(BFloat16, 10)) ≈ ones(BFloat16, 10)
-
 @test_broken Enzyme.gradient(Reverse, sum, ones(BFloat16, 10)) ≈ ones(BFloat16, 10)
 
 @test_broken Enzyme.gradient(Forward, sum, ones(BFloat16, 10)) ≈ ones(BFloat16, 10)
