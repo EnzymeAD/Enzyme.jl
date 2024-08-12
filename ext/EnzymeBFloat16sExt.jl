0cf47c18f17ad9b47cb848252a965a0a255bba04
William Moses <gh@wsmoses.com>
2024-08-11 21:53:02 -0700

    More bfloat fix (#1728)
    * More bfloat fix
    
    * Update bfloat16s.jl
    
    * Update bfloat16s.jl

--- ext/EnzymeBFloat16sExt.jl
+++ ext/EnzymeBFloat16sExt.jl
@@ -4,7 +4,7 @@ using BFloat16s
 using Enzyme
 
 function Enzyme.typetree_inner(::Type{BFloat16}, ctx, dl, seen::Enzyme.Compiler.TypeTreeTable)
-    return TypeTree(Enzyme.API.DT_BFloat16, -1, ctx)
+    return Enzyme.TypeTree(Enzyme.API.DT_BFloat16, -1, ctx)
 end
 
 end
