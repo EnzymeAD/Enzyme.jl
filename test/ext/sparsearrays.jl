ee96ec11c84803d595db512091cf7bac9222faeb
Paul Tiede <ptiede91@gmail.com>
2024-09-05 12:07:30 -0400

    Add more testing

--- test/ext/sparsearrays.jl
+++ test/ext/sparsearrays.jl
@@ -2,10 +2,31 @@ using Enzyme
 using SparseArrays
 using LinearAlgebra
 
-@testset "SparseArrays reverse rule" begin
-    C = zeros(128)
-    M = sprand(128, 96, 0.1)
-    v = randn(96)
+@testset "SparseArrays spmatvec reverse rule" begin
+    C = zeros(18)
+    M = sprand(18, 9, 0.1)
+    v = randn(9)
+    α = 2.0
+    β = 1.0
+
+    for Tret in (Duplicated,), Tv in (Const, Duplicated), 
+        Tα in (Const, Active), Tβ in (Const, Active)
+        test_reverse(mul!, Tret, (C, Tret), (M, Const), (v, Tv), (α, Tα), (β, Tβ))
+    end
+
+    for Tret in (Duplicated,), Tv in (Const, Duplicated)
+        test_reverse(mul!, Tret, (C, Tret), (M, Const), (v, Tv), (true, Const), (true, Const))
+    end
+
+    for Tret in (Duplicated,), Tv in (Const, Duplicated), bα in (true, false), bβ in (true, false)
+        test_reverse(mul!, Tret, (C, Tret), (M, Const), (v, Tv), (bα, Const), (bβ, Const))
+    end
+end
+
+@testset "SparseArrays spmatmat reverse rule" begin
+    C = zeros(18, 11)
+    M = sprand(18, 9, 0.1)
+    v = randn(9, 11)
     α = 2.0
     β = 1.0
 
