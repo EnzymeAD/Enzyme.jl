319c956ec8dc8a881d801d2a19e3829979167bc7
William Moses <gh@wsmoses.com>
2024-09-16 09:13:07 -0500

    Update internal_rules.jl

--- src/internal_rules.jl
+++ src/internal_rules.jl
@@ -1005,25 +1005,27 @@ function EnzymeRules.forward(config::EnzymeRules.FwdConfig,
         smpl::Annotation{<:Random.SamplerTrivial{Random.CloseOpen01{FT}}},
     ) where {rngty <: Union{TaskLocalRNG, Xoshiro}, FT <: Union{Float32, Float64}}
     Ty.val(rng.val, dst.val, smpl.val)
-    if RT <: Duplicated
-        fill!(dst.dval, 0)
-        Duplicated(dst.val, dst.dval)
-    elseif RT <: Const
-        dst.val
-    elseif RT <: DuplicatedNoNeed
-        fill!(dst.dval, 0)
-        dst.dval
-    else
-        ntuple(Val(EnzymeRules.width(config))) do i
-            Base.@_inline_meta
-            fill!(dst.dval[i], 0)
-            nothing
-        end
-        if RT <: BatchDuplicated
-            BatchDuplicated(dst.val, dst.dval)
+
+    if !(dst isa Const)
+        if EnzymeRules.width(config) == 1
+            fill!(dst.dval, 0)
         else
-            dst.dval
+            ntuple(Val(EnzymeRules.width(config))) do i
+                Base.@_inline_meta
+                fill!(dst.dval[i], 0)
+                nothing
+            end
         end
+    end
+    
+    if EnzymeRules.needs_primal(config) && EnzymeRules.needs_shadow(config)
+        dst
+    elseif EnzymeRules.needs_shadow(config)
+        dst.dval
+    elseif EnzymeRules.needs_primal(config)
+        dst.val
+    else
+        nothing
     end
 end
 
