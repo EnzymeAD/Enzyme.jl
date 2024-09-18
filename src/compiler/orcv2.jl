9ad8d9ee8d8bf1adbadae10fcc01ba091133f5ee
William S. Moses <gh@wsmoses.com>
2024-09-18 17:22:51 -0400

    fix

--- src/compiler/orcv2.jl
+++ src/compiler/orcv2.jl
@@ -224,7 +224,7 @@ function get_trampoline(job)
                 # but it would be nicer if _thunk just codegen'd the half
                 # we need.
                 other_func = functions(mod)[other_name]
-                Enzyme.Compiler.eraseInst(mod, other_func)
+                Compiler.eraseInst(mod, other_func)
             end
 
             tsm = move_to_threadsafe(mod)
