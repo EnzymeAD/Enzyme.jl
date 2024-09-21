8efb08f3a75e2a609a432a4ca1428a8505619daf
William Moses <gh@wsmoses.com>
2024-09-21 15:57:42 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,13 @@ using Test
 using InteractiveUtils
 using Enzyme
 Enzyme.API.printall!(true)
+Enzyme.Compiler.DumpPostOpt[] = true
 
 @testset "Simple tests" begin
     f1(x) = 1.0 + x
     f2(x) = x*x
     for T in (Float16,)
-        @show @code_typed autodiff(Reverse, tanh, Active, Active(T(1)))
+        @show @code_llvm autodiff(Reverse, tanh, Active, Active(T(1)))
         res = autodiff(Reverse, tanh, Active, Active(T(1)))[1][1]
         @test res isa T
         cmp = if T == Float64
@@ -17,7 +18,7 @@ Enzyme.API.printall!(true)
             T(0.41997434161402606939f0)
         end
         @test res ≈ cmp
-        @show @code_typed autodiff(Forward, tanh, Duplicated(T(1), T(1)))
+        @show @code_llvm autodiff(Forward, tanh, Duplicated(T(1), T(1)))
         res = autodiff(Forward, tanh, Duplicated(T(1), T(1)))[1]
         @test res isa T
         @test res ≈ cmp
