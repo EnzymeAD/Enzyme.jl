45aed60dfbd8fcab45830f58dddc6763c98ec112
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-09-28 00:34:21 +0200

    More cases for set_runtime_activity

--- lib/EnzymeCore/src/EnzymeCore.jl
+++ lib/EnzymeCore/src/EnzymeCore.jl
@@ -264,8 +264,10 @@ function clear_err_if_func_written end
 
 """
     set_runtime_activity(::Mode)
+    set_runtime_activity(::Mode, activitiy::Bool)
+    set_runtime_activity(::Mode, config::Union{FwdConfig,RevConfig})
 
-Return a new mode where runtime activity analysis is activated.
+Return a new mode where runtime activity analysis is activated / set to the desired value.
 """
 function set_runtime_activity end
 
