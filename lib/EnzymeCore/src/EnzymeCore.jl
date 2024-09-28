edd00954a7e274064bcd9097485bcdf19f3624cc
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-09-28 18:26:41 +0200

    Improve documentation of modes (#1895)
    * Improve documentation of modes
    
    * Alignment
    
    * Add comment on setter functions
    
    * List helper functions
    
    * More cases for set_runtime_activity
    
    * Merge remote-tracking branch 'upstream/main' into gd/modes_doc
    
    * Cleaner diff
    
    * Smaller diff

--- lib/EnzymeCore/src/EnzymeCore.jl
+++ lib/EnzymeCore/src/EnzymeCore.jl
@@ -209,50 +209,123 @@ end
     abstract type ABI
 
 Abstract type for what ABI  will be used.
+
+# Subtypes
+
+- [`FFIABI`](@ref) (the default)
+- [`InlineABI`](@ref)
+- [`NonGenABI`](@ref)
 """
 abstract type ABI end
 
 """
     struct FFIABI <: ABI
 
-Foreign function call ABI. JIT the differentiated function, then inttoptr call the address.
+Foreign function call [`ABI`](@ref). JIT the differentiated function, then inttoptr call the address.
 """
 struct FFIABI <: ABI end
+
 """
     struct InlineABI <: ABI
 
-Inlining function call ABI. 
+Inlining function call [`ABI`](@ref). 
 """
 struct InlineABI <: ABI end
+
 """
     struct NonGenABI <: ABI
 
-Non-generated function ABI. 
+Non-generated function [`ABI`](@ref). 
 """
 struct NonGenABI <: ABI end
+
 const DefaultABI = FFIABI
 
 """
-    abstract type Mode
+    abstract type Mode{ABI,ErrIfFuncWritten,RuntimeActivity}
 
-Abstract type for what differentiation mode will be used.
+Abstract type for which differentiation mode will be used.
+
+# Subtypes
+
+- [`ForwardMode`](@ref)
+- [`ReverseMode`](@ref)
+- [`ReverseModeSplit`](@ref)
+
+# Type parameters
+
+- `ABI`: what runtime [`ABI`](@ref) to use
+- `ErrIfFuncWritten`: whether to error when the function differentiated is a closure and written to.
+- `RuntimeActivity`: whether to enable runtime activity (default off)
+
+!!! warning
+    The type parameters of `Mode` are not part of the public API and can change without notice.
+    You can modify them with the following helper functions:
+    - [`WithPrimal`](@ref) / [`NoPrimal`](@ref)
+    - [`set_err_if_func_written`](@ref) / [`clear_err_if_func_written`](@ref)
+    - [`set_runtime_activity`](@ref) / [`clear_runtime_activity`](@ref)
+    - [`set_abi`](@ref)
 """
 abstract type Mode{ABI, ErrIfFuncWritten, RuntimeActivity} end
 
 """
-    struct ReverseMode{ReturnPrimal,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten} <: Mode{ABI, ErrIfFuncWritten, RuntimeActivity}
+    struct ReverseMode{
+        ReturnPrimal,
+        RuntimeActivity,
+        ABI,
+        Holomorphic,
+        ErrIfFuncWritten
+    } <: Mode{ABI,ErrIfFuncWritten,RuntimeActivity}
 
-Reverse mode differentiation.
-- `ReturnPrimal`: Should Enzyme return the primal return value from the augmented-forward.
-- `RuntimeActivity`: Should Enzyme enable runtime activity (default off)
-- `ABI`: What runtime ABI to use
-- `Holomorphic`: Whether the complex result function is holomorphic and we should compute d/dz
-- `ErrIfFuncWritten`: Should Enzyme err if the function differentiated is a closure and written to.
+Subtype of [`Mode`](@ref) for reverse mode differentiation.
+
+# Type parameters
+
+- `ReturnPrimal`: whether to return the primal return value from the augmented-forward pass.
+- `Holomorphic`: Whether the complex result function is holomorphic and we should compute `d/dz`
+- other parameters: see [`Mode`](@ref)
+
+!!! warning
+    The type parameters of `ReverseMode` are not part of the public API and can change without notice.
+    Please use one of the following concrete instantiations instead:
+    - [`Reverse`](@ref)
+    - [`ReverseWithPrimal`](@ref)
+    - [`ReverseHolomorphic`](@ref)
+    - [`ReverseHolomorphicWithPrimal`](@ref)
+    You can modify them with the following helper functions:
+    - [`WithPrimal`](@ref) / [`NoPrimal`](@ref)
+    - [`set_err_if_func_written`](@ref) / [`clear_err_if_func_written`](@ref)
+    - [`set_runtime_activity`](@ref) / [`clear_runtime_activity`](@ref)
+    - [`set_abi`](@ref)
 """
 struct ReverseMode{ReturnPrimal,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten} <: Mode{ABI, ErrIfFuncWritten,RuntimeActivity} end
+
+"""
+    const Reverse
+
+Default instance of [`ReverseMode`](@ref) that doesn't return the primal
+"""
 const Reverse = ReverseMode{false,false,DefaultABI, false, false}()
+
+"""
+    const ReverseWithPrimal
+
+Default instance of [`ReverseMode`](@ref) that also returns the primal.
+"""
 const ReverseWithPrimal = ReverseMode{true,false,DefaultABI, false, false}()
+
+"""
+    const ReverseHolomorphic
+
+Holomorphic instance of [`ReverseMode`](@ref) that doesn't return the primal
+"""
 const ReverseHolomorphic = ReverseMode{false,false,DefaultABI, true, false}()
+
+"""
+    const ReverseHolomorphicWithPrimal
+
+Holomorphic instance of [`ReverseMode`](@ref) that also returns the primal
+"""
 const ReverseHolomorphicWithPrimal = ReverseMode{true,false,DefaultABI, true, false}()
 
 @inline set_err_if_func_written(::ReverseMode{ReturnPrimal,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten}) where {ReturnPrimal,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten} = ReverseMode{ReturnPrimal,RuntimeActivity,ABI,Holomorphic,true}()
@@ -265,34 +338,80 @@ const ReverseHolomorphicWithPrimal = ReverseMode{true,false,DefaultABI, true, fa
 @inline clear_runtime_activity(::ReverseMode{ReturnPrimal,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten}) where {ReturnPrimal,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten} = ReverseMode{ReturnPrimal,false,ABI,Holomorphic,ErrIfFuncWritten}()
 
 """
-        WithPrimal(::Enzyme.Mode)
+    WithPrimal(::Mode)
 
-Modifies the mode to include the primal value.
+Return a new mode which includes the primal value.
 """
 @inline WithPrimal(::ReverseMode{ReturnPrimal,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten}) where {ReturnPrimal,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten} = ReverseMode{true,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten}()
 
 """
-        NoPrimal(::Enzyme.Mode)
+    NoPrimal(::Mode)
 
-Modifies the mode to exclude the primal value.
+Return a new mode which excludes the primal value.
 """
 @inline NoPrimal(::ReverseMode{ReturnPrimal,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten}) where {ReturnPrimal,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten} = ReverseMode{false,RuntimeActivity,ABI,Holomorphic,ErrIfFuncWritten}()
 
-
 """
-    struct ReverseModeSplit{ReturnPrimal,ReturnShadow,RuntimeActivity,Width,ModifiedBetween,ABI} <: Mode{ABI,ErrIfFuncWritten,RuntimeActivity}
+    struct ReverseModeSplit{
+        ReturnPrimal,
+        ReturnShadow,
+        Width,
+        RuntimeActivity,
+        ModifiedBetween,
+        ABI,
+        ErrFuncIfWritten
+    } <: Mode{ABI,ErrIfFuncWritten,RuntimeActivity}
+        WithPrimal(::Enzyme.Mode)
 
-Reverse mode differentiation.
-- `ReturnPrimal`: Should Enzyme return the primal return value from the augmented-forward.
-- `ReturnShadow`: Should Enzyme return the shadow return value from the augmented-forward.
-- `RuntimeActivity`: Should Enzyme differentiate with runtime activity on (default off).
-- `Width`: Batch Size (0 if to be automatically derived)
-- `ModifiedBetween`: Tuple of each argument's modified between state (true if to be automatically derived).
+Subtype of [`Mode`](@ref) for split reverse mode differentiation, to use in [`autodiff_thunk`](@ref) and variants.
+
+# Type parameters
+
+- `ReturnShadow`: whether to return the shadow return value from the augmented-forward.
+- `Width`: batch size (pick `0` to derive it automatically)
+- `ModifiedBetween`: `Tuple` of each argument's "modified between" state (pick `true` to derive it automatically).
+- other parameters: see [`ReverseMode`](@ref)
+
+!!! warning
+    The type parameters of `ReverseModeSplit` are not part of the public API and can change without notice.
+    Please use one of the following concrete instantiations instead: 
+    - [`ReverseSplitNoPrimal`](@ref)
+    - [`ReverseSplitWithPrimal`](@ref)
+    You can modify them with the following helper functions:
+    - [`WithPrimal`](@ref) / [`NoPrimal`](@ref)
+    - [`set_err_if_func_written`](@ref) / [`clear_err_if_func_written`](@ref)
+    - [`set_runtime_activity`](@ref) / [`clear_runtime_activity`](@ref)
+    - [`set_abi`](@ref)
+    - [`ReverseSplitModified`](@ref), [`ReverseSplitWidth`](@ref)
 """
 struct ReverseModeSplit{ReturnPrimal,ReturnShadow,Width,RuntimeActivity,ModifiedBetween,ABI,Holomorphic,ErrIfFuncWritten,ShadowInit} <: Mode{ABI, ErrIfFuncWritten,RuntimeActivity} end
+
+"""
+    const ReverseSplitNoPrimal
+
+Default instance of [`ReverseModeSplit`](@ref) that doesn't return the primal
+"""
 const ReverseSplitNoPrimal = ReverseModeSplit{false, true, false, 0, true,DefaultABI, false, false, false}()
+
+"""
+    const ReverseSplitWithPrimal
+
+Default instance of [`ReverseModeSplit`](@ref) that also returns the primal
+"""
 const ReverseSplitWithPrimal = ReverseModeSplit{true, true, false, 0, true,DefaultABI, false, false, false}()
+
+"""
+    ReverseSplitModified(::ReverseModeSplit, ::Val{MB})
+
+Return a new instance of [`ReverseModeSplit`](@ref) mode where `ModifiedBetween` is set to `MB`. 
+"""
 @inline ReverseSplitModified(::ReverseModeSplit{ReturnPrimal, ReturnShadow, RuntimeActivity, Width, MBO, ABI, Holomorphic, ErrIfFuncWritten, ShadowInit}, ::Val{MB}) where {ReturnPrimal,ReturnShadow,RuntimeActivity,Width,MB,MBO,ABI, Holomorphic, ErrIfFuncWritten, ShadowInit} = ReverseModeSplit{ReturnPrimal,ReturnShadow,RuntimeActivity, Width,MB,ABI, Holomorphic, ErrIfFuncWritten, ShadowInit}()
+
+"""
+    ReverseSplitWidth(::ReverseModeSplit, ::Val{W})
+
+Return a new instance of [`ReverseModeSplit`](@ref) mode where `Width` is set to `W`. 
+"""
 @inline ReverseSplitWidth(::ReverseModeSplit{ReturnPrimal, ReturnShadow, RuntimeActivity, WidthO, MB, ABI, Holomorphic, ErrIfFuncWritten, ShadowInit}, ::Val{Width}) where {ReturnPrimal,ReturnShadow,RuntimeActivity,Width,MB,WidthO,ABI, Holomorphic, ErrIfFuncWritten, ShadowInit} = ReverseModeSplit{ReturnPrimal,ReturnShadow,RuntimeActivity,Width,MB,ABI, Holomorphic, ErrIfFuncWritten, ShadowInit}()
 
 @inline set_err_if_func_written(::ReverseModeSplit{ReturnPrimal,ReturnShadow,RuntimeActivity,Width,ModifiedBetween,ABI, Holomorphic, ErrIfFuncWritten, ShadowInit}) where {ReturnPrimal,ReturnShadow,RuntimeActivity,Width,ModifiedBetween,ABI, Holomorphic, ErrIfFuncWritten, ShadowInit} = ReverseModeSplit{ReturnPrimal,ReturnShadow,RuntimeActivity,Width,ModifiedBetween,ABI, Holomorphic, true, ShadowInit}()
@@ -307,13 +426,46 @@ const ReverseSplitWithPrimal = ReverseModeSplit{true, true, false, 0, true,Defau
 
 
 """
-    struct Forward{ReturnPrimal, ABI, ErrIfFuncWritten,RuntimeActivity} <: Mode{ABI, ErrIfFuncWritten, RuntimeActivity}
+    struct ForwardMode{
+        ReturnPrimal,
+        ABI,
+        ErrIfFuncWritten,
+        RuntimeActivity
+    } <: Mode{ABI,ErrIfFuncWritten,RuntimeActivity}
 
-Forward mode differentiation
+Subtype of [`Mode`](@ref) for forward mode differentiation.
+
+# Type parameters
+
+- `ReturnPrimal`: whether to return the primal return value from the augmented-forward.
+- other parameters: see [`Mode`](@ref)
+
+!!! warning
+    The type parameters of `ForwardMode` are not part of the public API and can change without notice.
+    Please use one of the following concrete instantiations instead:
+    - [`Forward`](@ref)
+    - [`ForwardWithPrimal`](@ref)
+    You can modify them with the following helper functions:
+    - [`WithPrimal`](@ref) / [`NoPrimal`](@ref)
+    - [`set_err_if_func_written`](@ref) / [`clear_err_if_func_written`](@ref)
+    - [`set_runtime_activity`](@ref) / [`clear_runtime_activity`](@ref)
+    - [`set_abi`](@ref)
 """
 struct ForwardMode{ReturnPrimal, ABI, ErrIfFuncWritten,RuntimeActivity} <: Mode{ABI, ErrIfFuncWritten, RuntimeActivity}
 end
+
+"""
+    const Forward
+
+Default instance of [`ForwardMode`](@ref) that doesn't return the primal
+"""
 const Forward = ForwardMode{false, DefaultABI, false, false}()
+
+"""
+    const ForwardWithPrimal
+
+Default instance of [`ForwardMode`](@ref) that also returns the primal
+"""
 const ForwardWithPrimal = ForwardMode{true, DefaultABI, false, false}()
 
 @inline set_err_if_func_written(::ForwardMode{ReturnPrimal,ABI,ErrIfFuncWritten,RuntimeActivity}) where {ReturnPrimal,ABI,ErrIfFuncWritten,RuntimeActivity} = ForwardMode{ReturnPrimal,ABI,true,RuntimeActivity}()
@@ -337,22 +489,22 @@ function autodiff_deferred_thunk end
 """
     make_zero(::Type{T}, seen::IdDict, prev::T, ::Val{copy_if_inactive}=Val(false))::T
 
-    Recursively make a zero'd copy of the value `prev` of type `T`. The argument `copy_if_inactive` specifies
-    what to do if the type `T` is guaranteed to be inactive, use the primal (the default) or still copy the value. 
+Recursively make a zero'd copy of the value `prev` of type `T`. The argument `copy_if_inactive` specifies
+what to do if the type `T` is guaranteed to be inactive, use the primal (the default) or still copy the value. 
 """
 function make_zero end
 
 """
     make_zero!(val::T, seen::IdSet{Any}=IdSet())::Nothing
 
-    Recursively set a variables differentiable fields to zero. Only applicable for mutable types `T`.
+Recursively set a variables differentiable fields to zero. Only applicable for mutable types `T`.
 """
 function make_zero! end
 
 """
     make_zero(prev::T)
 
-    Helper function to recursively make zero.
+Helper function to recursively make zero.
 """
 @inline function make_zero(prev::T, ::Val{copy_if_inactive}=Val(false)) where {T, copy_if_inactive}
     make_zero(Core.Typeof(prev), IdDict(), prev, Val(copy_if_inactive))
@@ -383,10 +535,47 @@ if !isdefined(Base, :get_extension)
 end
 
 """
-   within_autodiff()
+    within_autodiff()
 
 Returns true if within autodiff, otherwise false.
 """
 function within_autodiff end
+
+"""
+    set_err_if_func_written(::Mode)
+
+Return a new mode which throws an error for any attempt to write into an unannotated function object.
+"""
+function set_err_if_func_written end
+
+"""
+    clear_err_if_func_written(::Mode)
+
+Return a new mode which doesn't throw an error for attempts to write into an unannotated function object.
+"""
+function clear_err_if_func_written end
+
+"""
+    set_runtime_activity(::Mode)
+    set_runtime_activity(::Mode, activitiy::Bool)
+    set_runtime_activity(::Mode, config::Union{FwdConfig,RevConfig})
+
+Return a new mode where runtime activity analysis is activated / set to the desired value.
+"""
+function set_runtime_activity end
+
+"""
+    clear_runtime_activity(::Mode)
+
+Return a new mode where runtime activity analysis is deactivated.
+"""
+function clear_runtime_activity end
+
+"""
+    set_abi(::Mode, ::Type{ABI})
+
+Return a new mode with its [`ABI`](@ref) set to the chosen type.
+"""
+function set_abi end
 
 end # module EnzymeCore
