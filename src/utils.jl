91a49eed7ba838606c821ca0307f0d9fb40c775b
William S. Moses <gh@wsmoses.com>
2024-10-21 04:03:19 -0400

    fix

--- src/utils.jl
+++ src/utils.jl
@@ -280,7 +280,7 @@ export my_methodinstance
 
 # returns the inner type of an sret/enzyme_sret/enzyme_sret_v
 function sret_ty(fn::LLVM.Function, idx::Int)
-    return eltype(value_type(LLVM.parameters(fn)[idx]))
+    return eltype(LLVM.value_type(LLVM.parameters(fn)[idx]))
 end
 
 export sret_ty
