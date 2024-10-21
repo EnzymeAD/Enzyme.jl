b0ea2a404f377430f124707adedcf575acb7cacd
William S. Moses <gh@wsmoses.com>
2024-10-21 18:30:17 -0400

    fix

--- src/utils.jl
+++ src/utils.jl
@@ -351,3 +351,11 @@ end
 end
 
 export typed_fieldtype
+
+# returns the inner type of an sret/enzyme_sret/enzyme_sret_v
+function sret_ty(fn::LLVM.Function, idx::Int)
+    return eltype(LLVM.value_type(LLVM.parameters(fn)[idx]))
+end
+
+export sret_ty
+
