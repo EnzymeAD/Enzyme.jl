647b13c77da370c93ad933c3f7edb695c49ca693
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-10-23 14:05:31 +0200

    Add CI testing for EnzymeCore (#2009)

--- lib/EnzymeCore/test/runtests.jl
+++ lib/EnzymeCore/test/runtests.jl
@@ -1,28 +1,8 @@
 using Test
 using EnzymeCore
 
-import EnzymeCore.EnzymeRules: forward, has_frule_from_sig
-
-g(x) = x ^ 2
-function forward(config, ::Const{typeof(g)}, ::Type{<:Const}, x::Const)
-    return Const(g(x.val))
+@testset verbose = true "EnzymeCore" begin
+    @testset "Miscellaneous" begin
+        include("misc.jl")
+    end
 end
-
-@test has_frule_from_sig(Base.signature_type(g, Tuple{Float64}))
-
-f(;kwargs) = 1.0
-
-function forward(config, ::Const{typeof(f)}, ::Type{<:Const}; kwargs...)
-    return Const(f(; kwargs...))
-end
-
-@test has_frule_from_sig(Base.signature_type(f, Tuple{}))
-
-data = [1.0, 2.0, 3.0, 4.0]
-
-d = @view data[2:end]
-y = @view data[3:end]
-@test_throws ErrorException Duplicated(d, y)
-
-@test_throws ErrorException Active(data)
-@test_throws ErrorException Active(d)
