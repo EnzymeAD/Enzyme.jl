ded54c770854704b5cc8a478b001847e52503da2
Markus Hauru <mhauru@turing.ac.uk>
2024-10-25 11:08:49 +0100

    Improve a docstring

--- test/integration/Distributions/runtests.jl
+++ test/integration/Distributions/runtests.jl
@@ -25,8 +25,8 @@ with both forward and reverse mode autodiff. `name` is for diagnostic printing.
 `runtime_activity` and `broken` are for specifying whether to use
 `Enzyme.set_runtime_activity` or not and whether the test is broken. Both of them taken
 values `Neither`, `Forward`, `Reverse` or `Both`, to specify which mode to apply the setting
-to. `splat` is for specifying whether to splat the value into the function or not. If yes,
-then value should be an iterable of arguments rather than a single argument.
+to. `splat` is for specifying whether to call the function as `func(value)` or as
+`func(value...)`.
 
 A constructor is also provided for giving a `Distribution` instead of a function, in which
 case the function is `x -> logpdf(distribution, x)`.
