9597c031c871b44a840587ffbd8be9d7ac38f9eb
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-10-30 19:39:35 +0100

    Move `WithPrimal`, `NoPrimal` tests to EnzymeCore

--- lib/EnzymeCore/test/runtests.jl
+++ lib/EnzymeCore/test/runtests.jl
@@ -2,6 +2,25 @@ using Test
 using EnzymeCore
 
 @testset verbose = true "EnzymeCore" begin
+    @testset "WithPrimal" begin
+        @test WithPrimal(Reverse) === ReverseWithPrimal
+        @test NoPrimal(Reverse) === Reverse
+        @test WithPrimal(ReverseWithPrimal) === ReverseWithPrimal
+        @test NoPrimal(ReverseWithPrimal) === Reverse
+    
+        @test WithPrimal(set_runtime_activity(Reverse)) === set_runtime_activity(ReverseWithPrimal)
+    
+        @test WithPrimal(Forward) === ForwardWithPrimal
+        @test NoPrimal(Forward) === Forward
+        @test WithPrimal(ForwardWithPrimal) === ForwardWithPrimal
+        @test NoPrimal(ForwardWithPrimal) === Forward
+    
+        @test WithPrimal(ReverseSplitNoPrimal) === ReverseSplitWithPrimal
+        @test NoPrimal(ReverseSplitNoPrimal) === ReverseSplitNoPrimal
+        @test WithPrimal(ReverseSplitWithPrimal) === ReverseSplitWithPrimal
+        @test NoPrimal(ReverseSplitWithPrimal) === ReverseSplitNoPrimal
+    end
+
     @testset "needs_primal" begin
         @test needs_primal(Reverse) === false
         @test needs_primal(ReverseWithPrimal) === true
