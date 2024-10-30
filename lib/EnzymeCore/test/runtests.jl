2eaaf0ee97f3171031104b6c423c304d1bf3e3a5
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-10-30 18:53:35 +0100

    Move `needs_primal` tests EnzymeCore

--- lib/EnzymeCore/test/runtests.jl
+++ lib/EnzymeCore/test/runtests.jl
@@ -2,6 +2,15 @@ using Test
 using EnzymeCore
 
 @testset verbose = true "EnzymeCore" begin
+    @testset "needs_primal" begin
+        @test needs_primal(Reverse) === false
+        @test needs_primal(ReverseWithPrimal) === true
+        @test needs_primal(Forward) === false
+        @test needs_primal(ForwardWithPrimal) === true
+        @test needs_primal(ReverseSplitNoPrimal) === false
+        @test needs_primal(ReverseSplitWithPrimal) === true
+    end
+
     @testset "Miscellaneous" begin
         include("misc.jl")
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3634,15 +3634,6 @@ end
     @test NoPrimal(ReverseSplitWithPrimal) === ReverseSplitNoPrimal
 end
 
-@testset "needs_primal" begin
-    @test needs_primal(Reverse) === false
-    @test needs_primal(ReverseWithPrimal) === true
-    @test needs_primal(Forward) === false
-    @test needs_primal(ForwardWithPrimal) === true
-    @test needs_primal(ReverseSplitNoPrimal) === false
-    @test needs_primal(ReverseSplitWithPrimal) === true
-end
-
 # TEST EXTENSIONS 
 using SpecialFunctions
 @testset "SpecialFunctions ext" begin
