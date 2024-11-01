6e6d6b8c0ad851937949ac883e72c2fa9b7674c3
William Moses <gh@wsmoses.com>
2024-11-01 01:32:35 -0500

    Fix enzymecore tests (#2038)
    * ix enzymecore tests
    
    * fix
    
    * Update runtests.jl

--- lib/EnzymeCore/test/runtests.jl
+++ lib/EnzymeCore/test/runtests.jl
@@ -3,31 +3,31 @@ using EnzymeCore
 
 @testset verbose = true "EnzymeCore" begin
     @testset "WithPrimal" begin
-        @test WithPrimal(Reverse) === ReverseWithPrimal
-        @test NoPrimal(Reverse) === Reverse
-        @test WithPrimal(ReverseWithPrimal) === ReverseWithPrimal
-        @test NoPrimal(ReverseWithPrimal) === Reverse
+        @test EnzymeCore.WithPrimal(Reverse) === ReverseWithPrimal
+        @test EnzymeCore.NoPrimal(Reverse) === Reverse
+        @test EnzymeCore.WithPrimal(ReverseWithPrimal) === ReverseWithPrimal
+        @test EnzymeCore.NoPrimal(ReverseWithPrimal) === Reverse
     
-        @test WithPrimal(set_runtime_activity(Reverse)) === set_runtime_activity(ReverseWithPrimal)
+        @test EnzymeCore.WithPrimal(EnzymeCore.set_runtime_activity(Reverse)) === EnzymeCore.set_runtime_activity(ReverseWithPrimal)
     
-        @test WithPrimal(Forward) === ForwardWithPrimal
-        @test NoPrimal(Forward) === Forward
-        @test WithPrimal(ForwardWithPrimal) === ForwardWithPrimal
-        @test NoPrimal(ForwardWithPrimal) === Forward
+        @test EnzymeCore.WithPrimal(Forward) === ForwardWithPrimal
+        @test EnzymeCore.NoPrimal(Forward) === Forward
+        @test EnzymeCore.WithPrimal(ForwardWithPrimal) === ForwardWithPrimal
+        @test EnzymeCore.NoPrimal(ForwardWithPrimal) === Forward
     
-        @test WithPrimal(ReverseSplitNoPrimal) === ReverseSplitWithPrimal
-        @test NoPrimal(ReverseSplitNoPrimal) === ReverseSplitNoPrimal
-        @test WithPrimal(ReverseSplitWithPrimal) === ReverseSplitWithPrimal
-        @test NoPrimal(ReverseSplitWithPrimal) === ReverseSplitNoPrimal
+        @test EnzymeCore.WithPrimal(ReverseSplitNoPrimal) === ReverseSplitWithPrimal
+        @test EnzymeCore.NoPrimal(ReverseSplitNoPrimal) === ReverseSplitNoPrimal
+        @test EnzymeCore.WithPrimal(ReverseSplitWithPrimal) === ReverseSplitWithPrimal
+        @test EnzymeCore.NoPrimal(ReverseSplitWithPrimal) === ReverseSplitNoPrimal
     end
 
     @testset "needs_primal" begin
-        @test needs_primal(Reverse) === false
-        @test needs_primal(ReverseWithPrimal) === true
-        @test needs_primal(Forward) === false
-        @test needs_primal(ForwardWithPrimal) === true
-        @test needs_primal(ReverseSplitNoPrimal) === false
-        @test needs_primal(ReverseSplitWithPrimal) === true
+        @test EnzymeCore.needs_primal(Reverse) === false
+        @test EnzymeCore.needs_primal(ReverseWithPrimal) === true
+        @test EnzymeCore.needs_primal(Forward) === false
+        @test EnzymeCore.needs_primal(ForwardWithPrimal) === true
+        @test EnzymeCore.needs_primal(ReverseSplitNoPrimal) === false
+        @test EnzymeCore.needs_primal(ReverseSplitWithPrimal) === true
     end
 
     @testset "Miscellaneous" begin
