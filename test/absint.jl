19e3ccb9c58eb12e5bc68478b1b22645d4bb8366
William Moses <gh@wsmoses.com>
2024-11-05 14:30:07 -0600

    Fix offset size for absint load (#2064)
    * Fix offset size for absint load
    
    * With test

--- test/absint.jl
+++ test/absint.jl
@@ -9,9 +9,15 @@ function (bc::BufferedMap!)()
 end
 
 
-@testset "Internal tests" begin
+@testset "Absint struct vector of vector" begin
     f = BufferedMap!([[2.7]])
     df = BufferedMap!([[3.1]])
 
     @test autodiff(Forward, Duplicated(f, df))[1] ≈ 3.1
+end
+
+@testset "Absint sum vector of vector" begin
+    a = [[2.7]]
+    da = [[3.1]]
+    @test autodiff(Forward, sum, Duplicated(a, da))[1] ≈ [3.1]
 end
