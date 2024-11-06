00de7631370fab1b7f2cfb8b5ecc2f0b44ac5934
William Moses <gh@wsmoses.com>
2024-11-05 21:33:17 -0600

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,6 @@
 using GPUCompiler
 using Enzyme
 using Test
-using FiniteDifferences
-using Aqua
 using Statistics
 using LinearAlgebra
 using InlineStrings
@@ -14,82 +12,7 @@ using InlineStrings
 using Enzyme_jll
 @info "Testing against" Enzyme_jll.libEnzyme
 
-function isapproxfn(fn, args...; kwargs...)
-    isapprox(args...; kwargs...)
-end
-# Test against FiniteDifferences
-function test_scalar(f, x; rtol=1e-9, atol=1e-9, fdm=central_fdm(5, 1), kwargs...)
-    ∂x, = autodiff(ReverseHolomorphic, f, Active, Active(x))[1]
-
-    finite_diff = if typeof(x) <: Complex
-      RT = typeof(x).parameters[1]
-      (fdm(dx -> f(x+dx), RT(0)) - im * fdm(dy -> f(x+im*dy), RT(0)))/2
-    else
-      fdm(f, x)
-    end
-
-    @test isapproxfn((Enzyme.Reverse, f), ∂x, finite_diff; rtol=rtol, atol=atol, kwargs...)
-
-    if typeof(x) <: Integer
-        x = Float64(x)
-    end
-
-    if typeof(x) <: Complex
-        ∂re, = autodiff(Forward, f, Duplicated(x, one(typeof(x))))
-        ∂im, = autodiff(Forward, f, Duplicated(x, im*one(typeof(x))))
-        ∂x = (∂re - im*∂im)/2
-    else
-        ∂x, = autodiff(Forward, f, Duplicated(x, one(typeof(x))))
-    end
-
-    @test isapproxfn((Enzyme.Reverse, f), ∂x, finite_diff; rtol=rtol, atol=atol, kwargs...)
-
-end
-
-
-genlatestsin(x)::Float64 = Base.invokelatest(sin, x)
-function genlatestsinx(xp)
-    x = @inbounds xp[1]
-    @inbounds xp[1] = 0.0
-    Base.invokelatest(sin, x)::Float64 + 1
-end
-
-function loadsin(xp)
-    x = @inbounds xp[1]
-    @inbounds xp[1] = 0.0
-    sin(x)
-end
-function invsin(xp)
-    xp = Base.invokelatest(convert, Vector{Float64}, xp)
-    loadsin(xp)
-end
-
 @testset "generic" begin
-    @test -0.4161468365471424 ≈ Enzyme.autodiff(Reverse, genlatestsin, Active, Active(2.0))[1][1]
-    @test -0.4161468365471424 ≈ Enzyme.autodiff(Forward, genlatestsin, Duplicated(2.0, 1.0))[1]
-
-    x = [2.0]
-    dx = [0.0]
-    Enzyme.autodiff(Reverse, genlatestsinx, Active, Duplicated(x, dx))
-    @test 0 ≈ x[1]
-    @test -0.4161468365471424 ≈ dx[1]
-
-    x = [2.0]
-    dx = [0.0]
-    Enzyme.autodiff(Reverse, invsin, Active, Duplicated(x, dx))
-    @test 0 ≈ x[1]
-    @test -0.4161468365471424 ≈ dx[1]
-
-	function inactive_gen(x)
-		n = 1
-		for k in 1:2
-			y = falses(n)
-		end
-		return x
-	end
-    @test 1.0 ≈ Enzyme.autodiff(Reverse, inactive_gen, Active, Active(1E4))[1][1]
-	@test 1.0 ≈ Enzyme.autodiff(Forward, inactive_gen, Duplicated(1E4, 1.0))[1]
-
     function whocallsmorethan30args(R)
         temp = diag(R)     
          R_inv = [temp[1] 0. 0. 0. 0. 0.; 
