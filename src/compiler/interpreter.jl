1c594dc39d7377779c945d917ea23b60aa8076ca
William Moses <gh@wsmoses.com>
2024-11-12 22:42:11 -0600

    Improve broadcast index analysis (#2079)
    * Improve broadcast index analysis
    
    * fixup
    
    * more
    
    * reduce ci err
    
    * fix
    
    * fix
    
    * fix
    
    * conditionally disable
    
    * with test

--- src/compiler/interpreter.jl
+++ src/compiler/interpreter.jl
@@ -43,6 +43,7 @@ struct EnzymeInterpreter{T} <: AbstractInterpreter
     forward_rules::Bool
     reverse_rules::Bool
     deferred_lower::Bool
+    broadcast_rewrite::Bool
     handler::T
 end
 
@@ -53,6 +54,7 @@ function EnzymeInterpreter(
     forward_rules::Bool,
     reverse_rules::Bool,
     deferred_lower::Bool = true,
+    broadcast_rewrite::Bool = true,
     handler = nothing
 )
     @assert world <= Base.get_world_counter()
@@ -79,6 +81,7 @@ function EnzymeInterpreter(
         forward_rules,
         reverse_rules,
         deferred_lower,
+        broadcast_rewrite,
         handler
     )
 end
@@ -89,8 +92,9 @@ EnzymeInterpreter(
     world::UInt,
     mode::API.CDerivativeMode,
     deferred_lower::Bool = true,
+    broadcast_rewrite::Bool = true,
     handler = nothing
-) = EnzymeInterpreter(cache_or_token, mt, world, mode == API.DEM_ForwardMode, mode == API.DEM_ReverseModeCombined || mode == API.DEM_ReverseModePrimal || mode == API.DEM_ReverseModeGradient, deferred_lower, handler)
+) = EnzymeInterpreter(cache_or_token, mt, world, mode == API.DEM_ForwardMode, mode == API.DEM_ReverseModeCombined || mode == API.DEM_ReverseModePrimal || mode == API.DEM_ReverseModeGradient, deferred_lower, broadcast_rewrite, handler)
 
 Core.Compiler.InferenceParams(interp::EnzymeInterpreter) = interp.inf_params
 Core.Compiler.OptimizationParams(interp::EnzymeInterpreter) = interp.opt_params
@@ -350,6 +354,404 @@ else
 end
 
 
+# julia> @btime Base.copyto!(dst, src);
+#   668.438 ns (0 allocations: 0 bytes)
+
+# inp = rand(2,3,4,5);
+# src = Base.Broadcast.preprocess(inp, convert(Base.Broadcast.Broadcasted{Nothing}, Base.Broadcast.instantiate(Base.broadcasted(Main.sin, inp))));
+# 
+# idx = Base.eachindex(src);
+# 
+# src2 = sin.(inp);
+# 
+# dst = zero(inp);
+# lindex_v1(idx, dst, src);
+# @assert dst == sin.(inp)
+# 
+# dst = zero(inp);
+# lindex_v1(idx, dst, src2);
+# @assert dst == sin.(inp)
+# 
+# @btime lindex_v1(idx, dst, src)
+# # 619.140 ns (0 allocations: 0 bytes)
+# 
+# @btime lindex_v1(idx, dst, src2)
+# # 153.258 ns (0 allocations: 0 bytes)
+
+@generated function lindex_v1(idx::BC2, dest, src) where BC2
+    if BC2 <: Base.CartesianIndices
+        nloops = BC2.parameters[1]
+        exprs = Expr[]
+        tot = :true
+        idxs = Symbol[]
+        lims = Symbol[]
+        for i in 1:nloops
+            sym = Symbol("lim_$i")
+            push!(lims, sym)
+            sidx = Symbol("idx_$i")
+            push!(idxs, sidx)
+            push!(exprs, quote
+                $sym = idx.indices[$i].stop
+            end)
+            if tot == :true
+                tot = quote $sym != 0 end
+            else
+                tot = quote $tot && ($sym != 0) end
+            end
+        end
+
+        loops = quote
+            @inbounds dest[$(idxs...)] = @inbounds Base.Broadcast._broadcast_getindex(src, Base.CartesianIndex($(idxs...)))
+        end
+
+        # for (sidx, lim) in zip(reverse(idxs), reverse(lims))
+        for (sidx, lim) in zip(idxs, lims)
+            loops = quote
+                let $sidx = 0
+                    @inbounds while true
+                        $sidx += 1
+                        $loops
+                        if $sidx == $lim
+                            break
+                        end
+                        $(Expr(:loopinfo, Symbol("julia.simdloop"), nothing))  # Mark loop as SIMD loop
+                    end
+                end
+            end
+        end
+
+        return quote
+            Base.@_inline_meta
+            $(exprs...)
+            if $tot
+                $loops
+            end
+        end
+    else
+        return quote
+            Base.@_inline_meta
+            @inbounds @simd for I in idx
+                dest[I] = src[I]
+            end
+        end
+    end
+end
+
+# inp = rand(2,3,4,5);
+# # inp = [2.0 3.0; 4.0 5.0; 7.0 9.0]
+# src = Base.Broadcast.preprocess(inp, convert(Base.Broadcast.Broadcasted{Nothing}, Base.Broadcast.instantiate(Base.broadcasted(Main.sin, inp))));
+# 
+# idx = Base.eachindex(src);
+# 
+# src2 = sin.(inp);
+# 
+# dst = zero(inp);
+# lindex_v2(idx, dst, src);
+# @assert dst == sin.(inp)
+# 
+# dst = zero(inp);
+# lindex_v2(idx, dst, src2);
+# @assert dst == sin.(inp)
+# 
+# @btime lindex_v2(idx, dst, src)
+# # 1.634 μs (0 allocations: 0 bytes)
+# 
+# @btime lindex_v2(idx, dst, src2)
+# # 1.617 μs (0 allocations: 0 bytes)
+@generated function lindex_v2(idx::BC2, dest, src, ::Val{Checked}=Val(true)) where {BC2, Checked}
+    if BC2 <: Base.CartesianIndices
+        nloops = BC2.parameters[1]
+        exprs = Union{Expr,Symbol}[]
+        tot = :true
+        idxs = Symbol[]
+        lims = Symbol[]
+
+        total = :1
+        for i in 1:nloops
+            sym = Symbol("lim_$i")
+            push!(lims, sym)
+            sidx = Symbol("idx_$i")
+            push!(idxs, sidx)
+            push!(exprs, quote
+                $sym = idx.indices[$i].stop
+            end)
+            if tot == :true
+                tot = quote $sym != 0 end
+                total = sym
+            else
+                tot = quote $tot && ($sym != 0) end
+                total = quote $total * $sym end
+            end
+        end
+
+        push!(exprs, quote total = $total end)
+
+        lexprs = Expr[]
+
+        if Checked
+            for (lidx, lim) in zip(idxs, lims)
+                push!(lexprs, quote
+                    $lidx = Base.urem_int(tmp, $lim) + 1
+                    tmp = Base.udiv_int(tmp, $lim)
+                end)
+            end
+        else
+            idxs = [quote I+1 end]
+        end
+
+        return quote
+            Base.@_inline_meta
+            $(exprs...)
+            if $tot
+                let I = 0
+                    @inbounds while true
+                        let tmp = I
+                            $(lexprs...)
+                            @inbounds dest[I+1] = @inbounds Base.Broadcast._broadcast_getindex(src, Base.CartesianIndex($(idxs...)))
+                        end
+                        I += 1
+                        if I == total
+                            break
+                        end
+                        $(Expr(:loopinfo, Symbol("julia.simdloop"), nothing))  # Mark loop as SIMD loop
+                    end
+                end
+            end
+        end
+    else
+        return quote
+            Base.@_inline_meta
+            @inbounds @simd for I in idx
+                dest[I] = src[I]
+            end
+        end
+    end
+end
+
+
+# inp = rand(2,3,4,5);
+# src = Base.Broadcast.preprocess(inp, convert(Base.Broadcast.Broadcasted{Nothing}, Base.Broadcast.instantiate(Base.broadcasted(Main.sin, inp))));
+# 
+# idx = Base.eachindex(src);
+# 
+# src2 = sin.(inp);
+# 
+# dst = zero(inp);
+# lindex_v3(idx, dst, src);
+# @assert dst == sin.(inp)
+# 
+# dst = zero(inp);
+# lindex_v3(idx, dst, src2);
+# @assert dst == sin.(inp)
+# 
+# @btime lindex_v3(idx, dst, src)
+# # 568.065 ns (0 allocations: 0 bytes)
+
+# @btime lindex_v3(idx, dst, src2)
+# # 23.906 ns (0 allocations: 0 bytes)
+@generated function lindex_v3(idx::BC2, dest, src) where BC2
+    if BC2 <: Base.CartesianIndices
+        nloops = BC2.parameters[1]
+        exprs = Union{Expr,Symbol}[]
+        tot = :true
+        idxs = Symbol[]
+        lims = Symbol[]
+
+        condition = :true
+        todo = Tuple{Type, Tuple}[(src, ())]
+
+        function index(x, ::Tuple{})
+            return x
+        end
+
+        function index(x, path)
+            if path[1] isa Symbol
+                return quote
+                    $(index(x, Base.tail(path))).$(path[1])
+                end
+            else
+                return quote getindex($(index(x, Base.tail(path))), $(path[1])) end
+            end
+        end
+
+        legal = true
+        while length(todo) != 0
+            cur, path = pop!(todo)
+            if cur <: AbstractArray
+                if condition == :true
+                    condition = quote idx.indices == axes($(index(:src, path))) end
+                else                
+                    condition = quote $condition && idx.indices == axes($(index(:src, path))) end
+                end
+                continue
+            end
+            if cur <: Base.Broadcast.Extruded
+                if condition == :true
+                    condition = quote all(($(index(:src, path))).keeps) end
+                else                
+                    condition = quote $condition && all(($(index(:src, path))).keeps) end
+                end
+                push!(todo, (cur.parameters[1], (:x, path...)))
+                continue
+            end
+            if cur == src && cur <: Base.Broadcast.Broadcasted
+                for (i, v) in enumerate(cur.parameters[4].parameters)
+                    push!(todo, (v, (i, :args, path...)))
+                end
+                continue
+            end
+            if cur <: AbstractFloat
+                continue
+            end
+            legal = false
+        end
+
+        if legal
+            return quote
+                Base.@_inline_meta
+                if $condition
+                    lindex_v2(idx, dest, src, Val(false))
+                else
+                    lindex_v1(idx, dest, src)
+                end
+            end
+        else
+            return quote
+                Base.@_inline_meta
+                lindex_v1(idx, dest, src)
+            end
+        end
+    else
+        return quote
+            Base.@_inline_meta
+            @inbounds @simd for I in idx
+                dest[I] = src[I]
+            end
+        end
+    end
+end
+
+# Override Base.copyto!(dest::AbstractArray, bc::Broadcasted{Nothing}) with
+#  a form which provides better analysis of loop indices
+@inline function override_bc_copyto!(dest::AbstractArray, bc::Base.Broadcast.Broadcasted{Nothing})
+	axdest = Base.axes(dest)
+	axbc = Base.axes(bc)
+    axdest == axbc || Base.Broadcast.throwdm(axdest, axbc)
+
+    if bc.args isa Tuple{AbstractArray}
+        A = bc.args[1]
+        if axdest == Base.axes(A)
+            if bc.f === Base.identity
+                Base.copyto!(dest, A)
+                return dest
+            end
+        end
+    end
+
+    # The existing code is rather slow for broadcast in practice: https://github.com/EnzymeAD/Enzyme.jl/issues/1434
+    src = Base.Broadcast.preprocess(dest, bc)
+    idx = Base.eachindex(src)
+    @inline Enzyme.Compiler.Interpreter.lindex_v3(idx, dest, src)
+    return dest
+end
+
+@generated function same_sized(x::Tuple)
+    result = :true
+    prev = nothing
+    for i in 1:length(x.parameters)
+        if x.parameters[i] <: Number
+            continue
+        end
+        if prev == nothing
+            prev = quote
+                sz = size(x[$i])
+            end
+            continue
+        end
+        if result == :true
+            result = quote
+                sz == size(x[$i])
+            end
+        else
+            result = quote
+                result && sz == size(x[$i])
+            end
+        end
+    end
+    return quote
+        Base.@_inline_meta
+        $prev
+        return $result
+    end
+end
+
+
+Base.@propagate_inbounds overload_broadcast_getindex(A::Union{Ref,AbstractArray{<:Any,0},Number}, I) = A[] # Scalar-likes can just ignore all indices
+Base.@propagate_inbounds overload_broadcast_getindex(::Ref{Type{T}}, I) where {T} = T
+# Tuples are statically known to be singleton or vector-like
+Base.@propagate_inbounds overload_broadcast_getindex(A::Tuple{Any}, I) = A[1]
+Base.@propagate_inbounds overload_broadcast_getindex(A::Tuple, I) = error("unhandled") # A[I[1]]
+Base.@propagate_inbounds overload_broadcast_getindex(A, I) = A[I]
+
+@inline function override_bc_materialize(bc)
+    if bc.args isa Tuple{AbstractArray} && bc.f === Base.identity
+        return copy(bc.args[1])
+    end
+    ElType = Base.Broadcast.combine_eltypes(bc.f, bc.args)
+    dest = similar(bc, ElType)
+    if all(isa_array_or_number, bc.args) && same_sized(bc.args)
+        @inbounds @simd for I in 1:length(bc)
+            val = Base.Broadcast._broadcast_getindex_evalf(bc.f, map(Base.Fix2(overload_broadcast_getindex, I), bc.args)...)
+            dest[I] = val
+        end
+    else
+        Base.copyto!(dest, bc)
+    end
+    return dest 
+end
+
+struct MultiOp{Position, NumUsed, F1, F2}
+    f1::F1
+    f2::F2
+end
+
+@generated function (m::MultiOp{Position, NumUsed})(args::Vararg{Any, N}) where {N, Position, NumUsed}
+    f2args = Union{Symbol, Expr}[]
+    for i in Position:(Position+NumUsed)
+        push!(f2args, :(args[$i]))
+    end
+    f1args = Union{Symbol, Expr}[]
+    for i in 1:Position
+        push!(f1args, :(args[$i]))
+    end
+    push!(f1args, quote
+        f2($(f2args...))
+    end)
+    for i in (Position+NumUsed):N
+        push!(f1args, :(args[$i]))
+    end
+    return quote
+        Base.@_inline_meta
+        f1($(f1args...))
+    end
+end
+
+@inline function array_or_number(@nospecialize(Ty))
+    return Ty <: AbstractArray || Ty <: Number
+end
+
+@inline function isa_array_or_number(@nospecialize(x))
+    return x isa AbstractArray || x isa Number
+end
+
+@inline function num_or_eltype(@nospecialize(Ty))
+    if Ty <: AbstractArray
+        eltype(Ty)
+    else
+        return Ty
+    end
+end
+
 function abstract_call_known(
     interp::EnzymeInterpreter,
     @nospecialize(f),
@@ -382,6 +784,55 @@ function abstract_call_known(
                 Core.Compiler.EFFECTS_TOTAL,
                 MethodResultPure(),
             )
+        end
+    end
+    
+    if interp.broadcast_rewrite
+        if f === Base.materialize && length(argtypes) == 2
+            bcty = widenconst(argtypes[2])
+            if Base.isconcretetype(bcty) && bcty <: Base.Broadcast.Broadcasted{<:Base.Broadcast.DefaultArrayStyle, Nothing} && all(array_or_number, bcty.parameters[4].parameters) && any(Base.Fix2(Base.:<:, AbstractArray), bcty.parameters[4].parameters)
+                fnty = bcty.parameters[3]
+                eltys = map(num_or_eltype, bcty.parameters[4].parameters)
+                retty = Core.Compiler._return_type(interp, Tuple{fnty, eltys...})
+                if Base.isconcretetype(retty)
+                    arginfo2 = ArgInfo(
+                        fargs isa Nothing ? nothing :
+                        [:(Enzyme.Compiler.Interpreter.override_bc_materialize), fargs[2:end]...],
+                        [Core.Const(Enzyme.Compiler.Interpreter.override_bc_materialize), argtypes[2:end]...],
+                    )
+                    return abstract_call_known(
+                        interp,
+                        Enzyme.Compiler.Interpreter.override_bc_materialize,
+                        arginfo2,
+                        si,
+                        sv,
+                        max_methods,
+                    )
+                end
+            end
+        end
+
+        if f === Base.copyto! && length(argtypes) == 3
+            # Ideally we just override uses of the AbstractArray base class, but
+            # I don't know how to override the method in base, without accidentally overridding
+            # it for say CuArray or other users. For safety, we only override for Array
+            if widenconst(argtypes[2]) <: Array &&
+               widenconst(argtypes[3]) <: Base.Broadcast.Broadcasted{Nothing}
+            
+                arginfo2 = ArgInfo(
+                    fargs isa Nothing ? nothing :
+                    [:(Enzyme.Compiler.Interpreter.override_bc_copyto!), fargs[2:end]...],
+                    [Core.Const(Enzyme.Compiler.Interpreter.override_bc_copyto!), argtypes[2:end]...],
+                )
+                return abstract_call_known(
+                    interp,
+                    Enzyme.Compiler.Interpreter.override_bc_copyto!,
+                    arginfo2,
+                    si,
+                    sv,
+                    max_methods,
+                )
+            end
         end
     end
 
