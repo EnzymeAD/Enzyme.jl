7626c52b718467aab6bd20bc2b1dda3099754d61
William Moses <gh@wsmoses.com>
2024-12-23 14:22:23 -0500

    Update EnzymeGPUArraysCoreExt.jl (#2224)

--- ext/EnzymeGPUArraysCoreExt.jl
+++ ext/EnzymeGPUArraysCoreExt.jl
@@ -4,11 +4,11 @@ using GPUArraysCore
 using Enzyme
 
 @inline function Enzyme.onehot(x::AbstractGPUArray)
-    onehot_internal(zerosetfn, x, 0, length(x))
+    Enzyme.onehot_internal(zerosetfn, x, 0, length(x))
 end
 
 @inline function Enzyme.onehot(x::AbstractGPUArray, start::Int, endl::Int)
-    onehot_internal(zerosetfn, x, start-1, endl-start+1)
+    Enzyme.onehot_internal(zerosetfn, x, start-1, endl-start+1)
 end
 
 function Enzyme.zerosetfn(x::AbstractGPUArray, i::Int)
@@ -18,9 +18,8 @@ function Enzyme.zerosetfn(x::AbstractGPUArray, i::Int)
 end
 
 function Enzyme.zerosetfn!(x::AbstractGPUArray, i::Int, val)
-    @allowscalar @inbounds x[i] = += val
+    @allowscalar @inbounds x[i] += val
     return
 end
-
 
 end # module
