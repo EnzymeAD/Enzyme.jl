38552f16068889940cc4011f1b31f4ec94535dfc
William Moses <gh@wsmoses.com>
2024-12-23 19:21:36 -0500

    Update EnzymeGPUArraysCoreExt.jl

--- ext/EnzymeGPUArraysCoreExt.jl
+++ ext/EnzymeGPUArraysCoreExt.jl
@@ -15,12 +15,24 @@ function Enzyme.zerosetfn!(x::AbstractGPUArray, i::Int, val)
 end
 
 @inline function Enzyme.onehot(x::AbstractGPUArray)
-    Enzyme.onehot_internal(Enzyme.zerosetfn, x, 0, length(x))
+    # Enzyme.onehot_internal(Enzyme.zerosetfn, x, 0, length(x))
+    N = length(x)
+    ntuple(Val(N)) do i
+        Base.@_inline_meta
+        res = zero(x)
+        @allowscalar @inbounds res[i] = 1
+        return res
+    end
 end
 
-@inline function Enzyme.onehot(x::AbstractGPUArray, start::Int, endl::Int)
-    Enzyme.onehot_internal(Enzyme.zerosetfn, x, start-1, endl-start+1)
+@inline function onehot(x::AbstractArray, start::Int, endl::Int)
+    # Enzyme.onehot_internal(Enzyme.zerosetfn, x, start-1, endl-start+1)
+    ntuple(Val(endl - start + 1)) do i
+        Base.@_inline_meta
+        res = zero(x)
+        @allowscalar @inbounds res[i + start - 1] = 1
+        return res
+    end
 end
-
 
 end # module
