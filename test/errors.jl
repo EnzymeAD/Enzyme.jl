61e16987b14c92dbdbb650dc622efbdd4e9f619c
William Moses <gh@wsmoses.com>
2025-05-21 11:06:44 -0500

    Update errors.jl

--- test/errors.jl
+++ test/errors.jl
@@ -3,5 +3,5 @@ using Enzyme, Test
 array_square(x) = 2 .* x
 
 @testset "Array of Pointer Copy" begin
-	@test_throws EnzymeNonScalarReturnException Enzyme.gradient(Reverse, array_square, [2.0])
+	@test_throws Enzyme.Compiler.EnzymeNonScalarReturnException Enzyme.gradient(Reverse, array_square, [2.0])
 end
