a3f25ac91a4bf317e90cebfa38c9383b3d722108
oscarddssmith <oscar.smith@juliacomputing.com>
2025-06-26 14:59:31 -0400

    start newpm migration

--- src/compiler.jl
+++ src/compiler.jl
@@ -2634,7 +2634,7 @@ function enzyme!(
     for f in collect(functions(mod))
         API.EnzymeFixupBatchedJuliaCallingConvention(f)
     end
-    ModulePassManager() do pm
+    NewPMModulePassManager() do pm
         dce!(pm)
         LLVM.run!(pm, mod)
     end
@@ -4175,7 +4175,7 @@ function lower_convention(
         throw(LLVM.LLVMException(msg))
     end
 
-    ModulePassManager() do pm
+    NewPMModulePassManager() do pm
         always_inliner!(pm)
         LLVM.run!(pm, mod)
     end
@@ -4259,7 +4259,7 @@ function lower_convention(
         LLVM.API.LLVMInstructionEraseFromParent(p)
     end
 
-    ModulePassManager() do pm
+    NewPMModulePassManager() do pm
         # Kill the temporary staging function
         global_dce!(pm)
         global_optimizer!(pm)
@@ -4373,7 +4373,7 @@ function GPUCompiler.compile_unhooked(output::Symbol, job::CompilerJob{<:EnzymeT
         permit_inlining!(f)
     end
 
-    LLVM.ModulePassManager() do pm
+    NewPMModulePassManager() do pm
         API.AddPreserveNVVMPass!(pm, true) #=Begin=#
         LLVM.run!(pm, mod)
     end
@@ -4425,7 +4425,7 @@ function GPUCompiler.compile_unhooked(output::Symbol, job::CompilerJob{<:EnzymeT
     found = String[]
     if bitcode_replacement() &&
        API.EnzymeBitcodeReplacement(mod, disableFallback, found) != 0
-        ModulePassManager() do pm
+        NewPMModulePassManager() do pm
             instruction_combining!(pm)
             LLVM.run!(pm, mod)
         end
@@ -4468,7 +4468,7 @@ function GPUCompiler.compile_unhooked(output::Symbol, job::CompilerJob{<:EnzymeT
             end
         end
 
-        ModulePassManager() do pm
+        NewPMModulePassManager() do pm
             always_inliner!(pm)
             LLVM.run!(pm, mod)
         end
@@ -4486,7 +4486,7 @@ function GPUCompiler.compile_unhooked(output::Symbol, job::CompilerJob{<:EnzymeT
             end
         end
         GPUCompiler.@safe_warn "Using fallback BLAS replacements for ($found), performance may be degraded"
-        ModulePassManager() do pm
+        NewPMModulePassManager() do pm
             global_optimizer!(pm)
             LLVM.run!(pm, mod)
         end
@@ -5034,7 +5034,7 @@ end
                 push!(toremove, name(f))
             end
         end
-        ModulePassManager() do pm
+        NewPMModulePassManager() do pm
             always_inliner!(pm)
             LLVM.run!(pm, mod)
         end
@@ -5056,7 +5056,7 @@ end
         augmented_primalf = nothing
     end
 
-    LLVM.ModulePassManager() do pm
+    NewPMModulePassManager() do pm
         API.AddPreserveNVVMPass!(pm, false) #=Begin=#
         LLVM.run!(pm, mod)
     end
@@ -5835,8 +5835,8 @@ function _thunk(job, postopt::Bool = true)::Tuple{LLVM.Module, Vector{Any}, Stri
         primal_name = nothing
     end
 
-    LLVM.ModulePassManager() do pm
-        add!(pm, FunctionPass("ReinsertGCMarker", reinsert_gcmarker_pass!))
+    NewPMModulePassManager() do pm
+        add!(pm, NewPMFunctionPass("ReinsertGCMarker", reinsert_gcmarker_pass!))
         LLVM.run!(pm, mod)
     end
 
