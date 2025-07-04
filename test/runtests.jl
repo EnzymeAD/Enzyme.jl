d3ddda5c3d646e0210985b8ac854c26f7a04bcb0
William S. Moses <gh@wsmoses.com>
2025-07-04 14:18:20 -0400

    restore

--- test/runtests.jl
+++ test/runtests.jl
@@ -68,6 +68,30 @@ function test_matrix_to_number(f, x; rtol=1e-9, atol=1e-9, fdm=central_fdm(5, 1)
     @test isapproxfn((Enzyme.Forward, f), dx_fwd, dx_fd; rtol=rtol, atol=atol, kwargs...)
 end
 
+# Aqua.test_all(Enzyme, unbound_args=false, piracies=false, deps_compat=false, stale_deps=(;:ignore=>[:EnzymeTestUtils]))
+# Aqua.test_all(Enzyme, unbound_args=false, piracies=false, deps_compat=false, stale_deps=(;:ignore=>[:EnzymeTestUtils]))
+
+include("abi.jl")
+include("typetree.jl")
+include("passes.jl")
+include("optimize.jl")
+include("make_zero.jl")
+include("runtime_calls.jl")
+
+include("rules.jl")
+include("rrules.jl")
+include("kwrules.jl")
+include("kwrrules.jl")
+include("internal_rules.jl")
+include("ruleinvalidation.jl")
+include("typeunstable.jl")
+include("absint.jl")
+include("array.jl")
+
+@static if !Sys.iswindows()
+    include("blas.jl")
+end
+
 f0(x) = 1.0 + x
 function vrec(start, x)
     if start > length(x)
@@ -85,6 +109,128 @@ end
 mutable struct MInts{A, B}
     v::B
     q::Int
+end
+
+@testset "Internal tests" begin
+    @static if VERSION < v"1.11-"
+    else
+    @assert Enzyme.Compiler.active_reg_inner(Memory{Float64}, (), nothing) == Enzyme.Compiler.DupState
+    end
+    @assert Enzyme.Compiler.active_reg_inner(Type{Array}, (), nothing) == Enzyme.Compiler.AnyState
+    @assert Enzyme.Compiler.active_reg_inner(Ints{<:Any, Integer}, (), nothing) == Enzyme.Compiler.AnyState
+    @assert Enzyme.Compiler.active_reg_inner(Ints{<:Any, Float64}, (), nothing) == Enzyme.Compiler.DupState
+    @assert Enzyme.Compiler.active_reg_inner(Ints{Integer, <:Any}, (), nothing) == Enzyme.Compiler.DupState
+    @assert Enzyme.Compiler.active_reg_inner(Ints{Integer, <:Integer}, (), nothing) == Enzyme.Compiler.AnyState
+    @assert Enzyme.Compiler.active_reg_inner(Ints{Integer, <:AbstractFloat}, (), nothing) == Enzyme.Compiler.DupState
+    @assert Enzyme.Compiler.active_reg_inner(Ints{Integer, Float64}, (), nothing) == Enzyme.Compiler.ActiveState
+    @assert Enzyme.Compiler.active_reg_inner(MInts{Integer, Float64}, (), nothing) == Enzyme.Compiler.DupState
+
+    @assert Enzyme.Compiler.active_reg(Tuple{Float32,Float32,Int})
+    @assert !Enzyme.Compiler.active_reg(Tuple{NamedTuple{(), Tuple{}}, NamedTuple{(), Tuple{}}})
+    @assert !Enzyme.Compiler.active_reg(Base.RefValue{Float32})
+    @assert Enzyme.Compiler.active_reg_inner(Ptr, (), nothing) == Enzyme.Compiler.DupState
+    @assert Enzyme.Compiler.active_reg_inner(Base.RefValue{Float32}, (), nothing) == Enzyme.Compiler.DupState
+    @assert Enzyme.Compiler.active_reg_inner(Colon, (), nothing) == Enzyme.Compiler.AnyState
+    @assert Enzyme.Compiler.active_reg_inner(Symbol, (), nothing) == Enzyme.Compiler.AnyState
+    @assert Enzyme.Compiler.active_reg_inner(String, (), nothing) == Enzyme.Compiler.AnyState
+    @assert Enzyme.Compiler.active_reg_inner(Tuple{Any,Int64}, (), nothing) == Enzyme.Compiler.DupState
+    @assert Enzyme.Compiler.active_reg_inner(Tuple{S,Int64} where S, (), Base.get_world_counter()) == Enzyme.Compiler.DupState
+    @assert Enzyme.Compiler.active_reg_inner(Union{Float64,Nothing}, (), nothing) == Enzyme.Compiler.DupState
+    @assert Enzyme.Compiler.active_reg_inner(Union{Float64,Nothing}, (), nothing, #=justActive=#Val(false), #=unionSret=#Val(true)) == Enzyme.Compiler.ActiveState
+    @test Enzyme.Compiler.active_reg_inner(Tuple, (), nothing) == Enzyme.Compiler.DupState
+    @test Enzyme.Compiler.active_reg_inner(Tuple, (), nothing, #=justactive=#Val(false), #=unionsret=#Val(false), #=abstractismixed=#Val(true)) == Enzyme.Compiler.MixedState
+    @test Enzyme.Compiler.active_reg_inner(Tuple{A,A} where A, (), nothing, #=justactive=#Val(false), #=unionsret=#Val(false), #=abstractismixed=#Val(true)) == Enzyme.Compiler.MixedState
+
+    # issue #1935
+    struct Incomplete
+        x::Float64
+        y
+        Incomplete(x) = new(x)
+        # incomplete constructor & non-bitstype field => !Base.allocatedinline(Incomplete)
+    end
+    @test Enzyme.Compiler.active_reg_inner(Tuple{Incomplete}, (), nothing, #=justActive=#Val(false)) == Enzyme.Compiler.MixedState
+    @test Enzyme.Compiler.active_reg_inner(Tuple{Incomplete}, (), nothing, #=justActive=#Val(true)) == Enzyme.Compiler.ActiveState
+
+    thunk_a = Enzyme.Compiler.thunk(Val(0), Const{typeof(f0)}, Active, Tuple{Active{Float64}}, Val(API.DEM_ReverseModeCombined), Val(1), Val((false, false)), Val(false), Val(false), DefaultABI, Val(false), Val(false), Val(false))
+    thunk_b = Enzyme.Compiler.thunk(Val(0), Const{typeof(f0)}, Const, Tuple{Const{Float64}}, Val(API.DEM_ReverseModeCombined), Val(1), Val((false, false)), Val(false), Val(false), DefaultABI, Val(false), Val(false), Val(false))
+    thunk_c = Enzyme.Compiler.thunk(Val(0), Const{typeof(f0)}, Active{Float64}, Tuple{Active{Float64}}, Val(API.DEM_ReverseModeCombined), Val(1), Val((false, false)), Val(false), Val(false), DefaultABI, Val(false), Val(false), Val(false))
+    thunk_d = Enzyme.Compiler.thunk(Val(0), Const{typeof(f0)}, Active{Float64}, Tuple{Active{Float64}}, Val(API.DEM_ReverseModeCombined), Val(1), Val((false, false)), Val(false), Val(false), DefaultABI, Val(false), Val(false), Val(false))
+    @test thunk_a.adjoint !== thunk_b.adjoint
+    @test thunk_c.adjoint === thunk_a.adjoint
+    @test thunk_c.adjoint === thunk_d.adjoint
+
+    @test thunk_a(Const(f0), Active(2.0), 1.0) == ((1.0,),)
+    @test thunk_a(Const(f0), Active(2.0), 2.0) == ((2.0,),)
+    @test thunk_b(Const(f0), Const(2.0)) === ((nothing,),)
+
+    forward, pullback = Enzyme.Compiler.thunk(Val(0), Const{typeof(f0)}, Active, Tuple{Active{Float64}}, Val(Enzyme.API.DEM_ReverseModeGradient), Val(1), Val((false, false)), Val(false), Val(false), DefaultABI, Val(false), Val(false), Val(false))
+
+    @test forward(Const(f0), Active(2.0)) == (nothing,nothing,nothing)
+    @test pullback(Const(f0), Active(2.0), 1.0, nothing) == ((1.0,),)
+
+    function mul2(x)
+        x[1] * x[2]
+    end
+    d = Duplicated([3.0, 5.0], [0.0, 0.0])
+
+    forward, pullback = Enzyme.Compiler.thunk(Val(0), Const{typeof(mul2)}, Active, Tuple{Duplicated{Vector{Float64}}}, Val(Enzyme.API.DEM_ReverseModeGradient), Val(1), Val((false, true)), Val(false), Val(false), DefaultABI, Val(false), Val(false), Val(false))
+    res = forward(Const(mul2), d)
+
+    @static if VERSION < v"1.11-"
+    @test typeof(res[1]) == Tuple{Float64, Float64}
+    else
+    @test typeof(res[1]) == NamedTuple{(Symbol("1"),Symbol("2"),Symbol("3")), Tuple{Any, Float64, Float64}}
+    end
+
+    pullback(Const(mul2), d, 1.0, res[1])
+    @test d.dval[1] ≈ 5.0
+    @test d.dval[2] ≈ 3.0
+
+    d = Duplicated([3.0, 5.0], [0.0, 0.0])
+    forward, pullback = Enzyme.Compiler.thunk(Val(0), Const{typeof(vrec)}, Active, Tuple{Const{Int}, Duplicated{Vector{Float64}}}, Val(Enzyme.API.DEM_ReverseModeGradient), Val(1), Val((false, false, true)), Val(false), Val(false), DefaultABI, Val(false), Val(false), Val(false))
+    res = forward(Const(vrec), Const(Int(1)), d)
+    pullback(Const(vrec), Const(1), d, 1.0, res[1])
+    @test d.dval[1] ≈ 5.0
+    @test d.dval[2] ≈ 3.0
+
+    # @test thunk_split.primal !== C_NULL
+    # @test thunk_split.primal !== thunk_split.adjoint
+    # @test thunk_a.adjoint !== thunk_split.adjoint
+    #
+    z = ([3.14, 21.5, 16.7], [0,1], [5.6, 8.9])
+    Enzyme.make_zero!(z)
+    @test z[1] ≈ [0.0, 0.0, 0.0]
+    @test z[2][1] == 0
+    @test z[2][2] == 1
+    @test z[3] ≈ [0.0, 0.0]
+    
+    z2 = ([3.14, 21.5, 16.7], [0,1], [5.6, 8.9])
+    Enzyme.make_zero!(z2)
+    @test z2[1] ≈ [0.0, 0.0, 0.0]
+    @test z2[2][1] == 0
+    @test z2[2][2] == 1
+    @test z2[3] ≈ [0.0, 0.0]
+    
+    z3 = [3.4, "foo"]
+    Enzyme.make_zero!(z3)
+    @test z3[1] ≈ 0.0
+    @test z3[2] == "foo"
+
+    z4 = sin
+    Enzyme.make_zero!(z4)
+    
+    struct Dense
+        n_inp::Int
+        b::Vector{Float64}
+    end
+
+    function Dense(n)
+        Dense(n, rand(n))
+    end
+
+    nn = Dense(4)
+    Enzyme.make_zero!(nn)
+    @test nn.b ≈ [0.0, 0.0, 0.0, 0.0]
 end
 
 @testset "Reflection" begin
@@ -516,7 +662,6 @@ end
     test_scalar(Base.inv, 3.0 + 4.0im)
 end
 
-@testset "Base functions" begin
     f1(x) = prod(ntuple(i -> i * x, 3))
     @test autodiff(Reverse, f1, Active, Active(2.0))[1][1] == 72
     @test autodiff(Forward, f1, Duplicated(2.0, 1.0))[1]   == 72
@@ -673,4 +818,3 @@ end
     f30(x) = reverse([x 2.0 3x])[1]
     @test autodiff(Reverse, f30, Active, Active(2.0))[1][1] == 3
     @test autodiff(Forward, f30, Duplicated(2.0, 1.0))[1]   == 3
-end
