14dd9acc78ba60951872e1875364fdf557ee1e05
William S. Moses <gh@wsmoses.com>
2025-07-11 11:24:28 -0500

    fix

--- src/typeutils/inference.jl
+++ src/typeutils/inference.jl
@@ -116,9 +116,18 @@ function primal_return_type_generator(world::UInt, source, self, @nospecialize(m
     )
     mi === nothing && return stub(world, source, method_error)
 
-    ci = Core.Compiler.retrieve_code_info(mi, world)
+    min_world2 = Ref{UInt}(typemin(UInt))
+    max_world2 = Ref{UInt}(typemax(UInt))
+   
+    mi2 = my_methodinstance(mode, typeof(Base.identity), Tuple{Nothing}, world, min_world2, max_world2)
+
+    ci = Core.Compiler.retrieve_code_info(mi2, world)
 
     # TODO @vchuravy, I have no idea why this fails here
+    if ci isa Nothing
+
+    end
+
     ci = ci::Core.Compiler.CodeInfo
 
     # prepare a new code info
