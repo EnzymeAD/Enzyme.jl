155db97cdea6427df0de39844ce4f8506bc79933
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-08-27 12:07:32 +0200

    Run Runic

--- test/integration/DifferentiationInterface/runtests.jl
+++ test/integration/DifferentiationInterface/runtests.jl
@@ -16,52 +16,52 @@ using StaticArrays: StaticArrays
 logging = get(ENV, "CI", "false") == "false"
 
 backends = [
-    AutoEnzyme(; function_annotation=Const),
-    AutoEnzyme(; mode=Forward),
-    AutoEnzyme(; mode=Reverse),
+    AutoEnzyme(; function_annotation = Const),
+    AutoEnzyme(; mode = Forward),
+    AutoEnzyme(; mode = Reverse),
 ]
 
 duplicated_backends = [
-    AutoEnzyme(; mode=Forward, function_annotation=Duplicated),
-    AutoEnzyme(; mode=Reverse, function_annotation=Duplicated),
+    AutoEnzyme(; mode = Forward, function_annotation = Duplicated),
+    AutoEnzyme(; mode = Reverse, function_annotation = Duplicated),
 ]
 
 @testset verbose = true "DifferentiationInterface integration" begin
     test_differentiation(
         backends,
-        default_scenarios(; include_constantified=true);
-        excluded=SECOND_ORDER,
+        default_scenarios(; include_constantified = true);
+        excluded = SECOND_ORDER,
         logging,
-        testset_name="Generic first order",
+        testset_name = "Generic first order",
     )
 
     test_differentiation(
         backends[1],
-        vcat(default_scenarios(; include_constantified=true), sparse_scenarios());
-        excluded=FIRST_ORDER,
+        vcat(default_scenarios(; include_constantified = true), sparse_scenarios());
+        excluded = FIRST_ORDER,
         logging,
-        testset_name="Generic second order",
+        testset_name = "Generic second order",
     )
 
     test_differentiation(
         backends[2],
         default_scenarios(;
-            include_normal=false,
-            include_cachified=true,
-            include_constantorcachified=true,
-            use_tuples=true,
+            include_normal = false,
+            include_cachified = true,
+            include_constantorcachified = true,
+            use_tuples = true,
         );
-        excluded=FIRST_ORDER,
+        excluded = FIRST_ORDER,
         logging,
-        testset_name="Caches",
+        testset_name = "Caches",
     )
 
     test_differentiation(
         duplicated_backends,
-        default_scenarios(; include_normal=false, include_closurified=true);
-        excluded=SECOND_ORDER,
+        default_scenarios(; include_normal = false, include_closurified = true);
+        excluded = SECOND_ORDER,
         logging,
-        testset_name="Closures",
+        testset_name = "Closures",
     )
 
     filtered_static_scenarios = filter(static_scenarios()) do s
@@ -71,8 +71,8 @@ duplicated_backends = [
     test_differentiation(
         backends[2:3],
         filtered_static_scenarios;
-        excluded=SECOND_ORDER,
+        excluded = SECOND_ORDER,
         logging,
-        testset_name="Static arrays",
+        testset_name = "Static arrays",
     )
 end
