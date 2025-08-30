b3b106a8f82cb7bceffffa0c34ded42a74b529c3
William Moses <gh@wsmoses.com>
2025-08-29 22:26:51 -0500

    Update threads.jl

--- test/threads.jl
+++ test/threads.jl
@@ -23,21 +23,4 @@ using Test
       end
     end
     
-    xs = Float64[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    dxs = ones(10)
-
-    Enzyme.autodiff(Reverse, psquare1, Duplicated(xs, dxs))
-    @test Float64[2, 4, 6, 8, 10, 12, 14, 16, 18, 20] ≈ dxs 
-
-    function psquare2(x, y)
-      Enzyme.@parallel x y for i = 1:10
-        @inbounds x[i] *= y[i]
-      end
-    end
-    
-    xs = Float64[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    dxs = ones(10)
-    
-    Enzyme.autodiff(Reverse, psquare2, Duplicated(xs, dxs), Duplicated(xs, dxs))
-    @test Float64[2, 4, 6, 8, 10, 12, 14, 16, 18, 20] ≈ dxs 
 end
