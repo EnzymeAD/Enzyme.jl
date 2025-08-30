d20560f1adf08fab520fbb374a76aaa1ef2f6eaf
William Moses <gh@wsmoses.com>
2025-08-30 11:29:14 -0500

    Remove test assertions and parallel function

--- test/threads.jl
+++ test/threads.jl
@@ -1,7 +1,5 @@
 using Enzyme
-using Test
 
-@testset "Closure-less threads $(Threads.nthreads())" begin
     function bf(i, x)
       x[i] *= x[i]
       nothing
@@ -15,12 +13,5 @@ using Test
     dxs = ones(10)
 
     Enzyme.autodiff(Reverse, psquare0, Duplicated(xs, dxs))
-    @test Float64[2, 4, 6, 8, 10, 12, 14, 16, 18, 20] ≈ dxs 
-
-    function psquare1(x)
-      Enzyme.@parallel x for i = 1:10
-        @inbounds x[i] *= x[i]
-      end
-    end
+    # @test Float64[2, 4, 6, 8, 10, 12, 14, 16, 18, 20] ≈ dxs 
     
-end
