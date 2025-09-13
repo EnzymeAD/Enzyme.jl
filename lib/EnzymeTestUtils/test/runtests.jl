f064347de43d178eba7b6ea455659ea9c78672da
William S. Moses <gh@wsmoses.com>
2025-09-13 13:20:22 -0500

    fix

--- lib/EnzymeTestUtils/test/runtests.jl
+++ lib/EnzymeTestUtils/test/runtests.jl
@@ -6,10 +6,10 @@ Random.seed!(0)
 
 @testset "EnzymeTestUtils.jl" begin
     include("helpers.jl")
-    # include("test_approx.jl")
-    # include("compatible_activities.jl")
-    # include("to_vec.jl")
-    # include("generate_tangent.jl")
+    include("test_approx.jl")
+    include("compatible_activities.jl")
+    include("to_vec.jl")
+    include("generate_tangent.jl")
     include("test_forward.jl")
-    # include("test_reverse.jl")
+    include("test_reverse.jl")
 end
