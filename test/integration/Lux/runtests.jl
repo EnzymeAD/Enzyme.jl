80200a5c03bb30d9bfc81dd84176a8759cac15e6
Avik Pal <avikpal@mit.edu>
2025-09-12 22:16:40 -0400

    chore: run formatter

--- test/integration/Lux/runtests.jl
+++ test/integration/Lux/runtests.jl
@@ -22,20 +22,20 @@ end
 function test_enzyme_gradients(model, x, ps, st)
     dx, dps = compute_enzyme_gradient(model, x, ps, st)
     dx_zygote, dps_zygote = compute_zygote_gradient(model, x, ps, st)
-    @test check_approx(dx, dx_zygote; atol=1.0f-3, rtol=1.0f-3)
-    @test check_approx(dps, dps_zygote; atol=1.0f-3, rtol=1.0f-3)
+    @test check_approx(dx, dx_zygote; atol = 1.0f-3, rtol = 1.0f-3)
+    return @test check_approx(dps, dps_zygote; atol = 1.0f-3, rtol = 1.0f-3)
 end
 
 const MODELS_LIST = Any[
     (Dense(2, 4), randn(StableRNG(0), Float32, 2, 3)),
     (Dense(2, 4, gelu), randn(StableRNG(0), Float32, 2, 3)),
-    (Dense(2, 4, gelu; use_bias=false), randn(StableRNG(0), Float32, 2, 3)),
+    (Dense(2, 4, gelu; use_bias = false), randn(StableRNG(0), Float32, 2, 3)),
     (Chain(Dense(2, 4, relu), Dense(4, 3)), randn(StableRNG(0), Float32, 2, 3)),
     (Scale(2), randn(StableRNG(0), Float32, 2, 3)),
     (Conv((3, 3), 2 => 3), randn(StableRNG(0), Float32, 3, 3, 2, 2)),
-    (Conv((3, 3), 2 => 3, gelu; pad=SamePad()), randn(StableRNG(0), Float32, 3, 3, 2, 2)),
+    (Conv((3, 3), 2 => 3, gelu; pad = SamePad()), randn(StableRNG(0), Float32, 3, 3, 2, 2)),
     (
-        Conv((3, 3), 2 => 3, relu; use_bias=false, pad=SamePad()),
+        Conv((3, 3), 2 => 3, relu; use_bias = false, pad = SamePad()),
         randn(StableRNG(0), Float32, 3, 3, 2, 2),
     ),
     (
@@ -43,21 +43,21 @@ const MODELS_LIST = Any[
         rand(StableRNG(0), Float32, 5, 5, 2, 2),
     ),
     (
-        Chain(Conv((4, 4), 2 => 2; pad=SamePad()), MeanPool((5, 5); pad=SamePad())),
+        Chain(Conv((4, 4), 2 => 2; pad = SamePad()), MeanPool((5, 5); pad = SamePad())),
         rand(StableRNG(0), Float32, 5, 5, 2, 2),
     ),
     (
-        Chain(Conv((3, 3), 2 => 3, relu; pad=SamePad()), MaxPool((2, 2))),
+        Chain(Conv((3, 3), 2 => 3, relu; pad = SamePad()), MaxPool((2, 2))),
         rand(StableRNG(0), Float32, 5, 5, 2, 2),
     ),
     (Maxout(() -> Dense(5 => 4, tanh), 3), randn(StableRNG(0), Float32, 5, 2)),
     (Bilinear((2, 2) => 3), randn(StableRNG(0), Float32, 2, 3)),
     (SkipConnection(Dense(2 => 2), vcat), randn(StableRNG(0), Float32, 2, 3)),
-    (ConvTranspose((3, 3), 3 => 2; stride=2), rand(StableRNG(0), Float32, 5, 5, 3, 1)),
+    (ConvTranspose((3, 3), 3 => 2; stride = 2), rand(StableRNG(0), Float32, 5, 5, 3, 1)),
     (StatefulRecurrentCell(RNNCell(3 => 5)), rand(StableRNG(0), Float32, 3, 2)),
     (StatefulRecurrentCell(RNNCell(3 => 5, gelu)), rand(StableRNG(0), Float32, 3, 2)),
     (
-        StatefulRecurrentCell(RNNCell(3 => 5, gelu; use_bias=false)),
+        StatefulRecurrentCell(RNNCell(3 => 5, gelu; use_bias = false)),
         rand(StableRNG(0), Float32, 3, 2),
     ),
     (
@@ -105,7 +105,7 @@ const MODELS_LIST = Any[
     (Chain(Dense(2, 4), BatchNorm(4)), randn(StableRNG(0), Float32, 2, 3)),
     (Chain(Dense(2, 4), BatchNorm(4, gelu)), randn(StableRNG(0), Float32, 2, 3)),
     (
-        Chain(Dense(2, 4), BatchNorm(4, gelu; track_stats=false)),
+        Chain(Dense(2, 4), BatchNorm(4, gelu; track_stats = false)),
         randn(StableRNG(0), Float32, 2, 3),
     ),
     (Chain(Conv((3, 3), 2 => 6), BatchNorm(6)), randn(StableRNG(0), Float32, 6, 6, 2, 2)),
