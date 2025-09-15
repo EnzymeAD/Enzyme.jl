1366bf3849dce246c13091173ac13eef39b32e45
William S. Moses <gh@wsmoses.com>
2025-09-15 11:44:25 -0500

    more cholmod special casing of pointers

--- src/absint.jl
+++ src/absint.jl
@@ -600,7 +600,7 @@ function abs_typeof(
             # add the extra poitner offset when loading here]. However for pointers constructed by ccall outside julia
             # to a julia object, which are not inline by type but appear so, like SparseArrays, this is a problem
             # and merits further investigation. x/ref https://github.com/EnzymeAD/Enzyme.jl/issues/2085
-            if !Base.allocatedinline(typ) && typ != SparseArrays.cholmod_dense_struct
+            if !Base.allocatedinline(typ) && typ != SparseArrays.cholmod_dense_struct && typ != SparseArrays.cholmod_sparse_struct
                 shouldLoad = false
                 offset %= sizeof(Int)
             else
