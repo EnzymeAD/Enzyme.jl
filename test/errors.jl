25985698a423e8ea6592d213bfdc5ded32b9f03e
William S. Moses <gh@wsmoses.com>
2025-09-16 12:28:24 -0500

    fix

--- test/errors.jl
+++ test/errors.jl
@@ -1,22 +1,7 @@
 using Enzyme, Test
 
-Enzyme.API.printall!(true)
-Enzyme.Compiler.DumpPreCheck[] = true
-Enzyme.Compiler.DumpPreEnzyme[] = true
-Enzyme.Compiler.DumpPostOpt[] = true
+array_square(x) = 2 .* x
 
-function array_square(x)
-	bc = Base.Broadcast.Broadcasted(*, (2, x))
-    dest = similar(x)
-    if undefined1(bc.args) && undefined2(bc.args)
-        for I in 1:length(x)
-        	fx = Base.Fix2(undefined3, I)
-            bc.f(fx(2), fx(x))
-        end
-    else
-        @inbounds Base.copyto!(dest, bc)
-    end
-    return dest 
+@testset "Array of Pointer Copy" begin
+	@test_throws Enzyme.Compiler.EnzymeNonScalarReturnException Enzyme.gradient(Reverse, array_square, [2.0])
 end
-
-Enzyme.gradient(Reverse, array_square, [2.0])
