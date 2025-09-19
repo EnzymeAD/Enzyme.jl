cffa09db6e815908ffe617db63339dbc2978f810
Valentin Churavy <v.churavy@gmail.com>
2025-09-19 18:40:20 -0400

    Revert "First activity cache (#2594)"
    This reverts commit 71a5afd59de7cc550ab8d6fe8a9cea962b5665a1.

--- src/compiler.jl
+++ src/compiler.jl
@@ -5871,9 +5871,7 @@ end
         StrongZero
     ) #=abiwrap=#
     tmp_job = if World isa Nothing
-        jb = Compiler.CompilerJob(mi, CompilerConfig(target, params; kernel = false))
-        check_activity_cache_invalidations(jb.world)
-        jb
+        Compiler.CompilerJob(mi, CompilerConfig(target, params; kernel = false))
     else
         Compiler.CompilerJob(mi, CompilerConfig(target, params; kernel = false), World)
     end
@@ -6067,8 +6065,6 @@ function thunk_generator(world::UInt, source::Union{Method, LineNumberNode}, @no
     
     mi === nothing && return stub(world, source, method_error)
  
-    check_activity_cache_invalidations(world)
-
     min_world2 = Ref{UInt}(typemin(UInt))
     max_world2 = Ref{UInt}(typemax(UInt))
    
