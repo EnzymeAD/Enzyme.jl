b937c2c3f34cab69e5c66b0d35138463bade46cc
Valentin Churavy <v.churavy@gmail.com>
2025-09-22 20:34:48 +0200

    add some benchmarks for remake_zero!
    (cherry picked from commit f19b0409352a2f3a9e646667709c98cc23ba59b1)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,6 +1,6 @@
 # To run:
 # using PkgBenchmark, Enzyme
-# result = benchmarkpkg(KernelAbstractions)
+# result = benchmarkpkg(Enzyme)
 # export_markdown("benchmark/perf.md", result)
 
 # Note: if you change this file you will need to delete an regenerate tune.json
@@ -14,3 +14,21 @@ const SUITE = BenchmarkGroup()
 SUITE["basics"] = BenchmarkGroup()
 
 SUITE["basics"]["overhead"] = @benchmarkable Enzyme.autodiff(Forward, identity, Const(1.0))
+
+SUITE["basics"]["make_zero"] = BenchmarkGroup()
+SUITE["basics"]["remake_zero!"] = BenchmarkGroup()
+
+p = (; x = 1.0, y = zeros(3))
+
+SUITE["basics"]["make_zero"]["namedtuple"] = @benchmarkable Enzyme.make_zero($p)
+SUITE["basics"]["remake_zero!"]["namedtuple"] = @benchmarkable Enzyme.remake_zero!(dp) setup = (dp = Enzyme.make_zero(p))
+
+struct MyStruct
+    x::Float64
+    y::Vector{Float64}
+end
+
+x = MyStruct(1.0, zeros(3))
+
+SUITE["basics"]["make_zero"]["struct"] = @benchmarkable Enzyme.make_zero($x)
+SUITE["basics"]["remake_zero!"]["struct"] = @benchmarkable Enzyme.remake_zero!(dx) setup = (dx = Enzyme.make_zero(x))
