e74c9162351ed368a5fd946a020a6937379b0a4d
Valentin Churavy <v.churavy@gmail.com>
2025-09-23 17:15:37 +0200

    wrap bfloat16 in testset

--- test/ext/bfloat16s.jl
+++ test/ext/bfloat16s.jl
@@ -2,6 +2,7 @@ using Enzyme
 using Test
 using BFloat16s
 
-@test_broken Enzyme.gradient(Reverse, sum, ones(BFloat16, 10))[1] ≈ ones(BFloat16, 10)
-
-@test_broken Enzyme.gradient(Forward, sum, ones(BFloat16, 10))[1] ≈ ones(BFloat16, 10)
+@testset "bfloat16s" begin
+    @test_broken Enzyme.gradient(Reverse, sum, ones(BFloat16, 10))[1] ≈ ones(BFloat16, 10)
+    @test_broken Enzyme.gradient(Forward, sum, ones(BFloat16, 10))[1] ≈ ones(BFloat16, 10)
+end
