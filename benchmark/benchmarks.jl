c539afa2e1d2cafcb5a49204b9dbbd956a829674
William Moses <gh@wsmoses.com>
2025-09-28 23:27:24 -0500

    Broadcast benchmark (#2629)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -32,3 +32,36 @@ x = MyStruct(1.0, zeros(3))
 
 SUITE["basics"]["make_zero"]["struct"] = @benchmarkable Enzyme.make_zero($x)
 SUITE["basics"]["remake_zero!"]["struct"] = @benchmarkable Enzyme.remake_zero!(dx) setup = (dx = Enzyme.make_zero(x))
+
+
+
+@noinline function sass(σ, x)
+    z = σ / x
+    return z
+end
+
+@noinline function multidim_sum_bcast0(dist, x, res)
+    broadcasted = Broadcast.broadcasted(sass, dist, x)
+    lp = sum(Broadcast.instantiate(broadcasted))
+    res[] = lp
+    nothing
+end
+
+function multidim_sum_bcast(dist, y)
+    tmp = Ref{Float64}(0.0)
+    multidim_sum_bcast0(dist, y, tmp)
+    return tmp[]::Float64
+end
+
+
+y1d = rand(10, 40);
+dist1d = fill(10.0, 40);
+
+SUITE["fold_broadcast"]["multidim_sum_bcast"]["1D"] = @benchmarkable multidim_sum_bcast($dist1d, $y1d)
+
+y2d = rand(10, 4);
+dist2d = fill(10.0, 10, 4);
+
+SUITE["fold_broadcast"]["multidim_sum_bcast"]["2D"] = @benchmarkable multidim_sum_bcast($dist2d, $y2d)
+
+
