108da13713e23c299798ad5cffc6f629e99151ba
Valentin Churavy <v.churavy@gmail.com>
2025-10-03 19:43:49 +0200

    add todo to julia_error

--- src/errors.jl
+++ src/errors.jl
@@ -965,6 +965,7 @@ end
             ) #=error=#
             world = enzyme_extract_world(f)
         end
+        # TODO(vchuravy)
         # what is data?
         # Can we get world here?
         # @assert world == enzyme_context(gutils).world
