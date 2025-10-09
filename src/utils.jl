cac08c0c11fba988efe4e6a6e3aa9f61cadfde67
Valentin Churavy <v.churavy@gmail.com>
2025-10-09 20:09:39 +0200

    fix unsafe_to_pointer definition for opaque pointers

--- src/utils.jl
+++ src/utils.jl
@@ -5,41 +5,52 @@
     Assumes that `val` is globally rooted and pointer to it can be leaked. Prefer `pointer_from_objref`.
     Only use inside Enzyme.jl should be for Types.
 """
-@inline unsafe_to_pointer(@nospecialize(val::Type)) = @static if sizeof(Int) == sizeof(Int64)
-    Base.llvmcall((
-"""
-declare nonnull {}* @julia.pointer_from_objref({} addrspace(11)*)
+function unsafe_to_pointer end
+export unsafe_to_pointer
 
-define i64 @f({} addrspace(10)* %obj) readnone alwaysinline {
-  %c = addrspacecast {} addrspace(10)* %obj to {} addrspace(11)*
-  %r = call {}* @julia.pointer_from_objref({} addrspace(11)* %c)
-  %e = ptrtoint {}* %r to i64
-  ret i64 %e
-}
-""", "f"),
-    Ptr{Cvoid},
-    Tuple{Any},
-    val,
-)
+if VERSION >= v"1.12-"
+    @inline function unsafe_to_pointer(@nospecialize(val::Type))
+        return Core.Intrinsics.llvmcall((
+            """
+            declare nonnull ptr @julia.pointer_from_objref(ptr addrspace(11))
+
+            define ptr @f(ptr addrspace(10) %obj) readnone alwaysinline {
+                %c = addrspacecast ptr addrspace(10) %obj to ptr addrspace(11)
+                %r = call ptr @julia.pointer_from_objref(ptr addrspace(11) %c)
+                ret ptr %r
+            }
+            """, "f"), Ptr{Cvoid}, Tuple{Any}, val)
+    end
+elseif Int == Int64
+    @inline function unsafe_to_pointer(@nospecialize(val::Type))
+        return Base.llvmcall((
+            """
+            declare nonnull {}* @julia.pointer_from_objref({} addrspace(11)*)
+
+            define i64 @f({} addrspace(10)* %obj) readnone alwaysinline {
+                %c = addrspacecast {} addrspace(10)* %obj to {} addrspace(11)*
+                %r = call {}* @julia.pointer_from_objref({} addrspace(11)* %c)
+                %e = ptrtoint {}* %r to i64
+                ret i64 %e
+            }
+            """, "f"), Ptr{Cvoid}, Tuple{Any}, val)
+    end
 else
-    Base.llvmcall((
-"""
-declare nonnull {}* @julia.pointer_from_objref({} addrspace(11)*)
+    @inline function unsafe_to_pointer(@nospecialize(val::Type))
+        return Base.llvmcall((
+            """
+            declare nonnull {}* @julia.pointer_from_objref({} addrspace(11)*)
 
-define i32 @f({} addrspace(10)* %obj) readnone alwaysinline {
-  %c = addrspacecast {} addrspace(10)* %obj to {} addrspace(11)*
-  %r = call {}* @julia.pointer_from_objref({} addrspace(11)* %c)
-  %e = ptrtoint {}* %r to i32
-  ret i32 %e
-}
-""", "f"),
-    Ptr{Cvoid},
-    Tuple{Any},
-    val,
-)
+            define i32 @f({} addrspace(10)* %obj) readnone alwaysinline {
+                %c = addrspacecast {} addrspace(10)* %obj to {} addrspace(11)*
+                %r = call {}* @julia.pointer_from_objref({} addrspace(11)* %c)
+                %e = ptrtoint {}* %r to i32
+                ret i32 %e
+            }
+            """, "f"), Ptr{Cvoid}, Tuple{Any}, val)
+    end
 end
 
-export unsafe_to_pointer
 
 @inline is_concrete_tuple(x::Type{T2}) where {T2} =
     (T2 <: Tuple) && !(T2 === Tuple) && !(T2 isa UnionAll)
