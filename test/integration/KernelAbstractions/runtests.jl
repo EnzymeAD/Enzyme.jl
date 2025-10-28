f0cd59abe601b6bcd00027b98c33b5146499f2f3
Valentin Churavy <v.churavy@gmail.com>
2025-10-28 17:25:09 +0100

    add KernelAbstractions integration test

---
+++ test/integration/KernelAbstractions/runtests.jl
@@ -0,0 +1,55 @@
+using Test
+using Enzyme
+using KernelAbstractions
+
+@kernel function square!(A)
+    I = @index(Global, Linear)
+    @inbounds A[I] *= A[I]
+end
+
+function square_caller(A)
+    backend = get_backend(A)
+    kernel = square!(backend)
+    kernel(A, ndrange = size(A))
+    KernelAbstractions.synchronize(backend)
+    return
+end
+
+
+@kernel function mul!(A, B)
+    I = @index(Global, Linear)
+    @inbounds A[I] *= B
+end
+
+function mul_caller(A, B)
+    backend = get_backend(A)
+    kernel = mul!(backend)
+    kernel(A, B, ndrange = size(A))
+    KernelAbstractions.synchronize(backend)
+    return
+end
+
+@testset "kernels" begin
+    A = Array{Float64}(undef, 64)
+    dA = Array{Float64}(undef, 64)
+
+    A .= (1:1:64)
+    dA .= 1
+
+    Enzyme.autodiff(Reverse, square_caller, Duplicated(A, dA))
+    @test all(dA .≈ (2:2:128))
+
+    A .= (1:1:64)
+    dA .= 1
+
+    _, dB, _ = Enzyme.autodiff(Reverse, mul_caller, Duplicated(A, dA), Active(1.2))[1]
+
+    @test all(dA .≈ 1.2)
+    @test dB ≈ sum(1:1:64)
+
+    A .= (1:1:64)
+    dA .= 1
+
+    Enzyme.autodiff(Forward, square_caller, Duplicated(A, dA))
+    @test all(dA .≈ 2:2:128)
+end
