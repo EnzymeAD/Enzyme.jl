afe0a2d76ef2de77f9ea584753f8ffcb95bccaa6
William Moses <gh@wsmoses.com>
2025-10-27 19:18:18 -0500

    Revise warning for Julia 1.12 support
    Updated warning message regarding Julia version support.

--- src/Enzyme.jl
+++ src/Enzyme.jl
@@ -1573,9 +1573,8 @@ function __init__()
     @static if VERSION ≥ v"1.12-"
         if ccall(:jl_generating_output, Cint, ()) == 1
             @warn """
-            Enzyme.jl currently doesn't support versions of Julia 1.12 or newer. We are
-            actively working on adding support for newer versions of Julia. For the time
-            being we recommend using 1.11 or LTS.
+            Enzyme.jl support for Julia 1.12 is presently in progress.
+			For the time being we recommend using 1.11 or LTS (1.10).
 
             For latest updates, check the status of support for Julia 1.12+ at
             https://github.com/EnzymeAD/Enzyme.jl/issues/2699.
