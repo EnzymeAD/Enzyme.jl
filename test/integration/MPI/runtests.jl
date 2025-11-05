9cd5aca879979ac7d0c42f65f0c7d5e7622c41ac
Valentin Churavy <v.churavy@gmail.com>
2025-11-05 17:58:28 +0100

    Test MPI.Comm_size/Comm_rank under AD

--- test/integration/MPI/runtests.jl
+++ test/integration/MPI/runtests.jl
@@ -1,3 +1,8 @@
 using MPI
 using Enzyme
 using Test
+
+# Query functions MPI_Comm_size/MPI_Comm_rank
+@testset "queries" for np in (1, 2, 4)
+    run(`$(mpiexec()) -n $np $(Base.julia_cmd()) --project=$(@__DIR__) $(joinpath(@__DIR__, "queries.jl"))`)
+end
