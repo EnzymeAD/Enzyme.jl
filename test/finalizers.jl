2b378c1e806dc4bb1f851e2b95f79c490e50137e
Valentin Churavy <v.churavy@gmail.com>
2025-11-06 20:03:02 +0100

    test batch forward

--- test/finalizers.jl
+++ test/finalizers.jl
@@ -29,20 +29,44 @@ GC.gc()
 @test length(FREE_LIST) == 1
 empty!(FREE_LIST)
 
-dx, x = autodiff(ForwardWithPrimal, compute, Duplicated(1.0, 2.0))
-@test dx == 4.0
-GC.gc()
-@test length(FREE_LIST) == 2
-empty!(FREE_LIST)
+@testset "forward" begin
+    dx, x = autodiff(ForwardWithPrimal, compute, Duplicated(1.0, 2.0))
+    @test dx == 4.0
+    GC.gc()
+    @test length(FREE_LIST) == 2
+    empty!(FREE_LIST)
 
-((dx,), x) = autodiff(ReverseWithPrimal, compute, Active(1.0))
-@test dx == 2.0
-GC.gc()
-@test length(FREE_LIST) == 2
-empty!(FREE_LIST)
+    dx, = autodiff(Forward, compute, Duplicated(1.0, 2.0))
+    @test dx == 4.0
+    GC.gc()
+    @test length(FREE_LIST) == 2
+    empty!(FREE_LIST)
+end
 
-dx, x = autodiff(ForwardWithPrimal, compute, DuplicatedBatched(1.0, (1.0, 2.0)))
-@test dx == 4.0
-GC.gc()
-@test length(FREE_LIST) == 3
-empty!(FREE_LIST)
+@testset "batched forward" begin
+    dx, x = autodiff(ForwardWithPrimal, compute, BatchDuplicated(1.0, (1.0, 2.0)))
+    @test dx == 4.0
+    GC.gc()
+    @test length(FREE_LIST) == 3
+    empty!(FREE_LIST)
+
+    dx, = autodiff(Forward, compute, BatchDuplicated(1.0, (1.0, 2.0)))
+    @test dx == 4.0
+    GC.gc()
+    @test length(FREE_LIST) == 3
+    empty!(FREE_LIST)
+end
+
+@testset "reverse" begin
+    ((dx,), x) = autodiff(ReverseWithPrimal, compute, Active(1.0))
+    @test dx == 2.0
+    GC.gc()
+    @test length(FREE_LIST) == 2
+    empty!(FREE_LIST)
+
+    ((dx,), x) = autodiff(Reverse, compute, Active(1.0))
+    @test dx == 2.0
+    GC.gc()
+    @test length(FREE_LIST) == 2
+    empty!(FREE_LIST)
+end
