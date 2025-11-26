c5c752a3a540918f691e66596f9d202e1849620a
William Moses <gh@wsmoses.com>
2025-11-25 22:06:57 -0600

    Fix pointer dereference for Core.Binding in orcv2.jl

--- src/compiler/orcv2.jl
+++ src/compiler/orcv2.jl
@@ -201,7 +201,7 @@ function prepare!(mod)
 	obj = Base.unsafe_pointer_to_objref(ptr)
 	# Let's try a de-bind for 1.10 lux
 	if isa(obj, Core.Binding)
-	   ptr = Compiler.unsafe_to_ptr(obj)
+	   ptr = Compiler.unsafe_to_ptr(obj.value)
 	end
 
         ptr = reinterpret(UInt, ptr)
