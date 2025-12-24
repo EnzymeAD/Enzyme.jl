44229e5dac77aa2c5eff981a4f690c1df8ae3d20
William S. Moses <gh@wsmoses.com>
2025-12-23 21:18:46 -0600

    don't peel

--- src/compiler/optimize.jl
+++ src/compiler/optimize.jl
@@ -114,7 +114,7 @@ function optimize!(mod::LLVM.Module, tm::LLVM.TargetMachine)
                     add!(lpm, IndVarSimplifyPass())
                     add!(lpm, LoopDeletionPass())
                 end
-                add!(fpm, LoopUnrollPass(opt_level=2, partial=false)) # what opt level?
+                add!(fpm, LoopUnrollPass(opt_level=2, partial=false, peeling=false)) # what opt level?
                 add!(fpm, AllocOptPass())
                 add!(fpm, SROAPass())
                 add!(fpm, GVNPass())
