236e9f3aea066547b8c29278b56eaa9c993ae4a3
Katharine Hyatt <katharine.s.hyatt@gmail.com>
2026-01-20 07:21:09 +0100

    Split rules tests up a bit too

--- test/rules/linear_algebra_rules.jl
+++ test/rules/linear_algebra_rules.jl
@@ -578,3 +578,25 @@ end
         end
     end
 end
+
+@testset "(matrix) det" begin
+    @testset "forward" begin
+        @testset for RT in (Const,DuplicatedNoNeed,Duplicated,),
+                     Tx in (Const,Duplicated,)
+            xr = [4.0 3.0; 2.0 1.0]
+            test_forward(LinearAlgebra.det, RT, (xr, Tx))
+
+            xc = [4.0+0.0im 3.0; 2.0-0.0im 1.0]
+            test_forward(LinearAlgebra.det, RT, (xc, Tx))
+        end
+    end
+    @testset "reverse" begin
+        @testset for RT in (Const, Active,), Tx in (Const, Duplicated,)
+            x = [4.0 3.0; 2.0 1.0]
+            test_reverse(LinearAlgebra.det, RT, (x, Tx))
+
+            x = [4.0+0.0im 3.0; 2.0-0.0im 1.0]
+            test_reverse(LinearAlgebra.det, RT, (x, Tx))
+        end
+    end
+end
