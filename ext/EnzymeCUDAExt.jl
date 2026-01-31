2a7b7f5f4db477e07395edbb3b70ac946a20661a
Billy Moses <wmoses@google.com>
2026-01-31 09:05:44 +0000

    also cuptr

--- ext/EnzymeCUDAExt.jl
+++ ext/EnzymeCUDAExt.jl
@@ -5,4 +5,6 @@ using CUDA, Enzyme
 @inline Enzyme.Compiler.is_arrayorvararg_ty(::Type{CUDA.CuRefValue{T}}) where T = true
 @inline Enzyme.Compiler.ptreltype(::Type{CUDA.CuRefValue{T}}) where T = T
 
+@inline Enzyme.Compiler.is_arrayorvararg_ty(::Type{CUDA.CuPtr{T}}) where T = true
+@inline Enzyme.Compiler.ptreltype(::Type{CUDA.CuPtr{T}}) where T = T
 end # module
