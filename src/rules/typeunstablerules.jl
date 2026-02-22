bb72e0f5b029b4a61ba398e37c4fb072b137d1b5
William Moses <gh@wsmoses.com>
2026-02-22 02:28:50 -0600

    Update typeunstablerules.jl

--- src/rules/typeunstablerules.jl
+++ src/rules/typeunstablerules.jl
@@ -444,7 +444,7 @@ function newstruct_common(fwd, run, offset, B, orig, gutils, normalR, shadowR)
 	pushfirst!(valTys, API.VT_Primal)
     end
     
-    shadowres = batch_call_same_with_inverted_arg_if_active!(B, gutils, orig, args, valTys, false)
+    shadowres = batch_call_same_with_inverted_arg_if_active!(B, gutils, orig, shadowsin, valTys, false)
     unsafe_store!(shadowR, shadowres.ref)
     return true
 end
