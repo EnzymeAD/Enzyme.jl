5845b9a73b598d877b5cd8f2c8e5418ecd615bde
abhro <5664668+abhro@users.noreply.github.com>
2026-02-23 07:01:20 -0500

    Run runic on docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,9 +4,9 @@ using EnzymeTestUtils
 using Documenter
 using Documenter.Remotes: GitHub
 
-DocMeta.setdocmeta!(Enzyme, :DocTestSetup, :(using Enzyme); recursive=true)
-DocMeta.setdocmeta!(EnzymeCore, :DocTestSetup, :(using EnzymeCore); recursive=true)
-DocMeta.setdocmeta!(EnzymeTestUtils, :DocTestSetup, :(using EnzymeTestUtils); recursive=true)
+DocMeta.setdocmeta!(Enzyme, :DocTestSetup, :(using Enzyme); recursive = true)
+DocMeta.setdocmeta!(EnzymeCore, :DocTestSetup, :(using EnzymeCore); recursive = true)
+DocMeta.setdocmeta!(EnzymeTestUtils, :DocTestSetup, :(using EnzymeTestUtils); recursive = true)
 @eval EnzymeCore begin
     const Enzyme = $(Enzyme)
 end
@@ -18,7 +18,7 @@ using Literate
 const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
 const OUTPUT_DIR = joinpath(@__DIR__, "src/generated")
 
-examples = Pair{String,String}[
+examples = Pair{String, String}[
     "Basics" => "autodiff"
     "Box model" => "box"
     "Custom rules" => "custom_rule"
@@ -57,19 +57,19 @@ end
 
 
 makedocs(;
-    modules=[Enzyme, EnzymeCore, EnzymeTestUtils],
-    authors="William Moses <wmoses@mit.edu>, Valentin Churavy <vchuravy@mit.edu>",
-    repo=GitHub("EnzymeAD/Enzyme.jl"),
-    sitename="Enzyme.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://enzyme.mit.edu/julia/",
+    modules = [Enzyme, EnzymeCore, EnzymeTestUtils],
+    authors = "William Moses <wmoses@mit.edu>, Valentin Churavy <vchuravy@mit.edu>",
+    repo = GitHub("EnzymeAD/Enzyme.jl"),
+    sitename = "Enzyme.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://enzyme.mit.edu/julia/",
         assets = [
             asset(
                 "https://plausible.io/js/plausible.js",
-                class=:js,
-                attributes=Dict(Symbol("data-domain") => "enzyme.mit.edu", :defer => "")
-            )
+                class = :js,
+                attributes = Dict(Symbol("data-domain") => "enzyme.mit.edu", :defer => "")
+            ),
         ],
         mathengine = MathJax3(),
         size_threshold_warn = 400 * 1024,   # 400 KiB
@@ -86,13 +86,13 @@ makedocs(;
         "Advanced" => [
             "For developers" => "dev_docs.md",
             "Internal API" => "internal_api.md",
-        ]
+        ],
     ],
     doctest = true,
 )
 
 deploydocs(;
-    repo="github.com/EnzymeAD/Enzyme.jl",
+    repo = "github.com/EnzymeAD/Enzyme.jl",
     devbranch = "main",
     push_preview = true,
 )
