fb29c02c78198ab154a3be0c5db8a6a06be8e742
Valentin Churavy <v.churavy@gmail.com>
2026-04-28 16:39:39 +0200

    Apply suggestions from code review
    Co-authored-by: Valentin Churavy <v.churavy@gmail.com>

--- src/preferences.jl
+++ src/preferences.jl
@@ -6,12 +6,13 @@ using Preferences
 Return whether the Enzyme attributor pass is enabled. This is a compile-time
 preference that requires restarting Julia to take effect after being changed.
 
-The attributor pass can improve differentiation quality but may cause issues on
-Julia 1.12 and later. Defaults to `false`.
+!!! note
+    The attributor pass can improve differentiation quality but may cause issues on
+    Julia 1.11 and later. Defaults to `true` on Julia 1.10 and `false` on 1.11 and later.
 
 See also [`set_run_attributor!`](@ref).
 """
-run_attributor() = @load_preference("run_attributor", false)
+run_attributor() = @load_preference("run_attributor", VERSION < v"1.11")
 
 """
     set_run_attributor!(val::Bool)
