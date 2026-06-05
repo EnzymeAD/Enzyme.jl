75d702e8c4ce784a3f297cb3254f256af70fcd54
Katharine Hyatt <kshyatt@users.noreply.github.com>
2026-06-05 18:24:39 +0200

    Fix jvp again and add more tests (#3147)

--- lib/EnzymeTestUtils/test/test_fd.jl
+++ lib/EnzymeTestUtils/test/test_fd.jl
@@ -7,4 +7,8 @@ using FiniteDifferences
     # we can make f_vec here identity since it cannot be
     # reached if x is itself empty
     @test isempty(j′vp(FiniteDifferences.central_fdm(5, 1), identity, Float32[], Float32[]))
+    # test also the real case
+    @test isempty(j′vp(FiniteDifferences.central_fdm(5, 1), identity, Float32[0.26], Float32[]))
+    # test also the complex case
+    @test isempty(j′vp(FiniteDifferences.central_fdm(5, 1), identity, Float32[0.26, 0.14], Float32[]))
 end
