0fa6084caa2213765e902ed641f34f611460513c
Billy Moses <wmoses@google.com>
2026-06-08 13:34:30 -0500

    Improve type lowering for ptr memcpy

--- test/absint.jl
+++ test/absint.jl
@@ -8,7 +8,6 @@ function (bc::BufferedMap!)()
     return @inbounds bc.x_buffer[1][1]
 end
 
-
 @testset "Absint struct vector of vector" begin
     f = BufferedMap!([[2.7]])
     df = BufferedMap!([[3.1]])
@@ -21,3 +20,34 @@ end
     da = [[3.1]]
     @test autodiff(Forward, sum, Duplicated(a, da))[1] ≈ [3.1]
 end
+
+struct MyStruct
+    a::Float64
+    b::Int
+    c::Float64
+    d::Int
+end
+
+function f_absint_memcpy!(dest, src)
+    if length(src) > 0
+        dest[1] = src[1]
+        for i in 2:length(src)
+            dest[i] = src[i]
+        end
+    end
+    nothing
+end
+
+@testset "Absint Ptr/GEP memcpy translation" begin
+    dest = [MyStruct(0.0, 0, 0.0, 0) for _ in 1:3]
+    ddest = [MyStruct(0.0, 0, 0.0, 0) for _ in 1:3]
+    src = [MyStruct(1.0, 2, 3.0, 4) for _ in 1:3]
+    dsrc = [MyStruct(0.0, 0, 0.0, 0) for _ in 1:3]
+
+    autodiff(Reverse, f_absint_memcpy!, Duplicated(dest, ddest), Duplicated(src, dsrc))
+    @test ddest[1].a == 0.0 # Just verifying it runs without EnzymeNoTypeError
+end
+
+
+Enzyme.Compiler.VERBOSE_ERRORS[] = true
+Enzyme.API.printall!(true)
