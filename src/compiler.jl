02f70fa51dcf2f729d064c06fb17a4719651a1f7
William Moses <gh@wsmoses.com>
2026-06-17 19:52:55 -0600

    Refactor return type handling in compiler.jl

--- src/compiler.jl
+++ src/compiler.jl
@@ -4894,20 +4894,13 @@ function lower_convention(
 		llactualRetType = get_return_info(actualRetType)[1]
 
 		ret_tt0 = typetree(actualRetType, ctx, dl, seen)
-		ret_tt = if llactualRetType == Ptr{actualRetType}
-		    typeTree = copy(ret_tt0)
-                    merge!(typeTree, TypeTree(API.DT_Pointer, ctx))
-                    only!(typeTree, -1)
-                    typeTree
-                else
-                    ret_tt0
-                end
+		
 
                 push!(
                     return_attributes(wrapper_f),
                     StringAttribute(
                         "enzyme_type",
-            			ret_tt
+            			string(ret_tt0)
                     ),
                 )
                 push!(
