469cd0383993c6d398af6a01ed6a177d55b75022
William Moses <gh@wsmoses.com>
2026-06-17 20:30:51 -0600

    Fix return type tree (#3193)
    * Fix return type tree
    
    * fix
    
    * Refactor return type handling in compiler.jl
    
    * Update compiler.jl

--- src/compiler.jl
+++ src/compiler.jl
@@ -4890,11 +4890,13 @@ function lower_convention(
                 position!(builder, def)
                 ret!(builder, extract_value!(builder, res, 0))
 
+				ret_tt0 = typetree(actualRetType, ctx, dl, seen)
+
                 push!(
                     return_attributes(wrapper_f),
                     StringAttribute(
                         "enzyme_type",
-                        string(typetree(actualRetType, ctx, dl, seen)),
+            			string(ret_tt0)
                     ),
                 )
                 push!(
@@ -5010,11 +5012,22 @@ function lower_convention(
                 emit_error(builder, nothing, "Expected return type of primal to be "*string(expected_RT)*" but did not find a value of that type")
                 unreachable!(builder)
             else
+                llactualRetType = get_return_info(actualRetType)[1]
+                ret_tt0 = typetree(actualRetType, ctx, dl, seen)
+                ret_tt = if llactualRetType == Ptr{actualRetType}
+                    typeTree = copy(ret_tt0)
+                    merge!(typeTree, TypeTree(API.DT_Pointer, ctx))
+                    only!(typeTree, -1)
+                    typeTree
+                else
+                    ret_tt0
+                end
+
                 push!(
                     return_attributes(wrapper_f),
                     StringAttribute(
                         "enzyme_type",
-                        string(typetree(actualRetType, ctx, dl, seen)),
+                        string(ret_tt),
                     ),
                 )
                 push!(
