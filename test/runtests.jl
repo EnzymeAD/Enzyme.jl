b4ad28dfbb20747033b1323f3e3eca3368bf78e1
William Moses <gh@wsmoses.com>
2026-06-24 13:50:18 -0500

    Remove redundant autodiff tests for BigFloat
    Removed several tests for arithmetic operations using autodiff.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Enzyme
 using Test
 
+Enzyme.API.printall!(true)
+Enzyme.Compiler.DumpPostOpt[] = true
+
 @testset "BigFloat arithmetic" begin
     a = BigFloat(1.234)
     da = BigFloat(-0.23)
@@ -13,10 +16,4 @@ using Test
 
     @test autodiff(Enzyme.Forward, +, Duplicated, Duplicated(a, da), Duplicated(b, db))[:1] ≈ da+db 
     @test autodiff(Enzyme.Forward, +, Duplicated, Duplicated(a, da), Duplicated(bf64, dbf64))[:1] ≈ da+dbf64 
-    @test autodiff(Enzyme.Forward, -, Duplicated, Duplicated(a, da), Duplicated(b, db))[:1] ≈ da-db 
-    @test autodiff(Enzyme.Forward, -, Duplicated, Duplicated(a, da), Duplicated(bf64, dbf64))[:1] ≈ da-dbf64 
-    @test autodiff(Enzyme.Forward, *, Duplicated, Duplicated(a, da), Duplicated(b, db))[:1] ≈ b*da + a*db 
-    @test autodiff(Enzyme.Forward, *, Duplicated, Duplicated(a, da), Duplicated(bf64, dbf64))[:1] ≈ bf64*da + a*dbf64
-    @test autodiff(Enzyme.Forward, /, Duplicated, Duplicated(a, da), Duplicated(b, db))[:1] ≈ da/b  - db * a/b^2
-    @test autodiff(Enzyme.Forward, /, Duplicated, Duplicated(a, da), Duplicated(bf64, dbf64))[:1] ≈ da/bf64 - dbf64 * a/bf64^2 
 end
