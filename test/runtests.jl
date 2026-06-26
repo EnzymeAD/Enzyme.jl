8bb1a1d16848e63409751fe74a6f058b37afec7e
Billy Moses <wmoses@google.com>
2026-06-26 12:33:29 -0500

    Revert GC rooting changes and add isolated test case

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,4 @@ using Test
 Enzyme.API.printall!(true)
 Enzyme.Compiler.DumpPostOpt[] = true
 
-@testset "BigFloat arithmetic" begin
-    a = BigFloat(1.234)
-    da = BigFloat(-0.23)
-    b = BigFloat(0.56)
-    db = BigFloat(0.27)
-    af64 = 1.234 # for testing mixed methods
-    daf64 = -0.23 # for testing mixed methods
-    bf64 = 0.56 # for testing mixed methods
-    dbf64 = 0.27 # for testing mixed methods
-
-    @test autodiff(Enzyme.Forward, +, Duplicated, Duplicated(a, da), Duplicated(b, db))[:1] ≈ da+db 
-end
+include("test_isolated.jl")
