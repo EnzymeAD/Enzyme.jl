fcd41803903ba8eb91d4a39084310d1059bae533
William Moses <gh@wsmoses.com>
2026-06-26 18:23:56 -0400

    Fix variable name in get_base_and_offset call

--- src/errors.jl
+++ src/errors.jl
@@ -1437,7 +1437,7 @@ function julia_error(
 @static if VERSION < v"1.11-"
 else   
             if isa(cur, LLVM.ConstantExpr)
-                larg, off = get_base_and_offset(operands(cur)[1]; inst=first(instructions(prevbb)))
+                larg, off = get_base_and_offset(operands(cur)[1]; inst=first(instructions(position(prevbb))))
                 legal2, obj = absint(larg)
                 obj = unbind(obj)
                 if legal2 && is_memory_instance(obj)
