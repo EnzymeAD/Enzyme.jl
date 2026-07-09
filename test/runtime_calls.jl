7056f2f3a3db0f7355db1a2eb50c512b4f008029
William Moses <gh@wsmoses.com>
2026-07-09 17:52:12 -0400

    Add logging pkg for tests (#3305)
    * Add logging pkg for tests
    
    * fix

--- test/runtime_calls.jl
+++ test/runtime_calls.jl
@@ -16,7 +16,7 @@ end
         end
         return sol
     end
-    g = Enzyme.gradient(Reverse, foo_logger, [2.0, 3.0])[1]
+    g = Enzyme.gradient(set_runtime_activity(Reverse), foo_logger, [2.0, 3.0])[1]
     @test g ≈ [3.0, 2.0]
 end
 
