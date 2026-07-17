4f47c146e1c3d387636a9f8529d42640746ba247
Billy Moses <wmoses@google.com>
2026-07-17 00:20:03 -0500

    fix

--- src/rules/customrules.jl
+++ src/rules/customrules.jl
@@ -1085,7 +1085,7 @@ end
     	metadata(sret)["enzymejl_allocart"] = MDNode(LLVM.Metadata[MDString(string(convert(UInt, unsafe_to_pointer(esret))))])
         pushfirst!(args, sret)
         if returnRoots !== nothing
-            returnRoots = create_rooted_array(alloctx, convert(LLVMType, eltype(returnRoots)))
+            returnRoots = alloca!(alloctx, convert(LLVMType, eltype(returnRoots)))
             insert!(args, 2, returnRoots)
         else
             returnRoots = nothing
