3a1c20ce40960b434774186eced95e9b9879f0d1
Billy Moses <wmoses@google.com>
2026-07-21 19:13:40 +0000

    1.13: vendor one-dim broadcast indexing

--- src/rules/customrules.jl
+++ src/rules/customrules.jl
@@ -2403,7 +2403,6 @@ function enzyme_custom_common_rev(
                     end
                     shadowV = C_NULL
                 else
-                    @assert value_type(shadowV) == shadowType
                     shadowV = shadowV.ref
                 end
                 idx += 1
