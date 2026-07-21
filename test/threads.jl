dcbf1b81a6320209b5615041818c13fe1486cdd9
William Moses <gh@wsmoses.com>
2026-07-21 12:48:47 -0500

    Update threads.jl

--- test/threads.jl
+++ test/threads.jl
@@ -4,27 +4,16 @@ using Test
 Enzyme.API.printall!(true)
 Enzyme.Compiler.DumpPostOpt[] = true
 
-@testset "Threads $(Threads.nthreads())" begin
-    function tasktest(M, x)
-        xr = Ref(x)
-        task = Threads.@spawn begin
-            @inbounds M[1] = xr[]
+@testset "Advanced, Active-var Threads $(Threads.nthreads())" begin
+    function f_multi(out, in)
+        Threads.@threads for idx in 1:length(out)
+            out[idx] = in
         end
-        @inbounds M[2] = x
-        wait(task)
-        nothing
+        return nothing
     end
 
-    R = Float64[0., 0.]
-    dR = Float64[2., 3.]
-
-    # We define the local variable `autodiff` as a `Ref` wrapper only to be able to capture
-    # and test the warning emitted by the `@generated` function `Enzyme.autodiff`.
-    autodiff = Ref{Any}(Enzyme.autodiff)
-    @test 5.0 ≈ @test_warn r"active variables passed by value to jl_new_task are not yet supported" autodiff[](Reverse, tasktest, Duplicated(R, dR), Active(2.0))[1][2]
-    @test Float64[2.0, 2.0] ≈ R
-    @test Float64[0.0, 0.0] ≈ dR
-    
-    # Enzyme.autodiff(Forward, tasktest, Duplicated(R, dR), Duplicated(2.0, 1.0))
-    # @test Float64[1.0, 1.0] ≈ dR
+    out = [1.0, 2.0]
+    dout = [1.0, 1.0]
+    res = autodiff(Reverse, f_multi, Const, Duplicated(out, dout), Active(2.0))
+    @test res[1][2] ≈ 2.0
 end
