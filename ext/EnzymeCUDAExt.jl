0098a61eeb2d8e2920dc0897eb012a5b33fe13ff
William S. Moses <gh@wsmoses.com>
2026-07-23 15:09:45 -0500

    array copy

--- ext/EnzymeCUDAExt.jl
+++ ext/EnzymeCUDAExt.jl
@@ -5,16 +5,19 @@ using Enzyme
 using Enzyme: EnzymeRules
 
 function _zero!(ptr::Ptr{T}, off::Integer, n::Integer) where {T <: AbstractFloat}
-    n == 0 && return nothing
     Base.Libc.memset(ptr + off * sizeof(T), 0, n * sizeof(T))
     return nothing
 end
 function _zero!(ptr::CuPtr{T}, off::Integer, n::Integer) where {T <: AbstractFloat}
-    n == 0 && return nothing
     bytes = reinterpret(CuPtr{UInt8}, ptr + off * sizeof(T))
     CUDA.memset(bytes, UInt8(0), n * sizeof(T))
     return nothing
 end
+
+const StridedSubArray{T,N,I<:Tuple{Vararg{Union{Base.RangeIndex, Base.ReshapedUnitRange,
+                                            Base.AbstractCartesianIndex}}}} =
+  SubArray{T,N,<:Array,I}
+const StridedArray{T,N} = Union{Array{T,N}, StridedSubArray{T,N}}
 
 #= Element-wise `dst .+= src`, moving across host/device as needed.
 
@@ -37,10 +40,10 @@ Float64   65536       8.256           14.423          0.57
 Float64   1048576     9.157           13.786          0.66
 Float64   16777216    256.104         257.62          0.99
 =#
-_accumulate!(dst::CuArray, src::CuArray) = (dst .+= src; nothing)
-_accumulate!(dst::Array, src::Array) = (dst .+= src; nothing)
-_accumulate!(dst::CuArray, src::Array) = _accumulate!(dst, CuArray(src))
-_accumulate!(dst::Array, src::CuArray) = _accumulate!(dst, Array(src))
+_accumulate!(dst::StridedCuArray, src::StridedCuArray) = (dst .+= src; nothing)
+_accumulate!(dst::StridedArray, src::StridedArray) = (dst .+= src; nothing)
+_accumulate!(dst::StridedCuArray, src::StridedArray) = _accumulate!(dst, CuArray(src))
+_accumulate!(dst::StridedArray, src::StridedCuArray) = _accumulate!(dst, Array(src))
 
 function _accumulate!(
         acc::Union{Ptr, CuPtr}, aoff::Integer,
@@ -56,8 +59,6 @@ end
 
 function _accumulate_and_zero!(src, soff, dest, doff, n::Integer)
     n == 0 && return nothing
-    _accumulate!(src, soff, dest, doff, n)
-    _zero!(dest, doff, n)
     return nothing
 end
 
@@ -65,52 +66,55 @@ end
     return EnzymeRules.width(config) == 1 ? x.dval : x.dval[batch]
 end
 
-function EnzymeRules.augmented_primal(
-        config::EnzymeRules.RevConfig,
-        func::Const{typeof(pointer)},
-        ::Type{RT},
-        array::Annotation{<:StridedCuArray},
-        index::Const;
-        kwargs...,
-    ) where {RT}
-    primal = if EnzymeRules.needs_primal(config)
-        func.val(array.val, index.val; kwargs...)
-    else
-        nothing
-    end
-    shadow = if EnzymeRules.needs_shadow(config) && !(array isa Const)
-        if EnzymeRules.width(config) == 1
-            func.val(array.dval, index.val; kwargs...)
-        else
-            ntuple(Val(EnzymeRules.width(config))) do batch
-                func.val(array.dval[batch], index.val; kwargs...)
-            end
-        end
-    else
-        nothing
-    end
-    return EnzymeRules.AugmentedReturn(primal, shadow, nothing)
-end
+# This rule should _NOT_ be needed. Without it there appears to be
+# a correctness bug, we should find out the actual root cause of the bug
+# rather than adding this here.
+# function EnzymeRules.augmented_primal(
+#         config::EnzymeRules.RevConfig,
+#         func::Const{typeof(pointer)},
+#         ::Type{RT},
+#         array::Annotation{<:StridedCuArray},
+#         index::Const;
+#         kwargs...,
+#     ) where {RT}
+#     primal = if EnzymeRules.needs_primal(config)
+#         func.val(array.val, index.val; kwargs...)
+#     else
+#         nothing
+#     end
+#     shadow = if EnzymeRules.needs_shadow(config) && !(array isa Const)
+#         if EnzymeRules.width(config) == 1
+#             func.val(array.dval, index.val; kwargs...)
+#         else
+#             ntuple(Val(EnzymeRules.width(config))) do batch
+#                 func.val(array.dval[batch], index.val; kwargs...)
+#             end
+#         end
+#     else
+#         nothing
+#     end
+#     return EnzymeRules.AugmentedReturn(primal, shadow, nothing)
+# end
+# 
+# function EnzymeRules.reverse(
+#         config::EnzymeRules.RevConfig,
+#         func::Const{typeof(pointer)},
+#         ::Type{RT},
+#         tape,
+#         array::Annotation{<:StridedCuArray},
+#         index::Const;
+#         kwargs...,
+#     ) where {RT}
+#     return (nothing, nothing)
+# end
 
-function EnzymeRules.reverse(
-        config::EnzymeRules.RevConfig,
-        func::Const{typeof(pointer)},
-        ::Type{RT},
-        tape,
-        array::Annotation{<:StridedCuArray},
-        index::Const;
-        kwargs...,
-    ) where {RT}
-    return (nothing, nothing)
-end
-
-const COPY_DIRECTIONS = (
+const PTR_COPY_DIRECTIONS = (
     (Ptr, CuPtr),
     (CuPtr, Ptr),
     (CuPtr, CuPtr),
 )
 
-for (DstPtr, SrcPtr) in COPY_DIRECTIONS
+for (DstPtr, SrcPtr) in PTR_COPY_DIRECTIONS
     @eval begin
         function EnzymeRules.augmented_primal(
                 config::EnzymeRules.RevConfig,
@@ -137,24 +141,75 @@ for (DstPtr, SrcPtr) in COPY_DIRECTIONS
                 func::Const{typeof(Base.unsafe_copyto!)},
                 ::Type{RT},
                 tape,
-                dest::Annotation{<:$DstPtr},
-                src::Annotation{<:$SrcPtr},
+		dest::Annotation{<:$DstPtr{T}},
+		src::Annotation{<:$SrcPtr{T}},
                 n::Const;
                 kwargs...,
-            ) where {RT}
+            ) where {RT, T <: AbstractFloat}
             if !(dest isa Const)
                 for batch in 1:EnzymeRules.width(config)
                     ddest = _shadow(dest, config, batch)
-                    if src isa Const
-                        _zero!(ddest, 0, n.val)
-                    else
-                        _accumulate_and_zero!(
-                            _shadow(src, config, batch), 0, ddest, 0, n.val
-                        )
+		    if !(src isa Const)
+			dsrc = _shadow(src, config, batch)
+    			_accumulate!(dsrc, 0, ddest, 0, n.val)
                     end
+                    _zero!(ddest, 0, n.val)
                 end
             end
             return (nothing, nothing, nothing)
+        end
+    end
+end
+
+const ARRAY_COPY_DIRECTIONS = (
+    (DenseCuArray, DenseCuArray),
+    (DenseCuArray, Array),
+    (Array, DenseCuArray),
+)
+
+for (DstArr, SrcArr) in ARRAY_COPY_DIRECTIONS
+    @eval begin
+        function EnzymeRules.augmented_primal(
+                config::EnzymeRules.RevConfig,
+                func::Const{typeof(Base.unsafe_copyto!)},
+                ::Type{RT},
+		dest::Annotation{<:$DstArr{T}},
+		doffs::Const,
+		src::Annotation{<:$SrcArr{T}},
+		soffs::Const,
+                n::Const) where {RT, T<: AbstractFloat}
+            func.val(dest.val, doffs.val, src.val, soffs.val, n.val)
+            primal = EnzymeRules.needs_primal(config) ? dest.val : nothing
+            shadow = if !(RT <: Const) && EnzymeRules.needs_shadow(config) &&
+                    !(dest isa Const)
+                dest.dval
+            else
+                nothing
+            end
+            return EnzymeRules.AugmentedReturn(primal, shadow, nothing)
+        end
+
+        function EnzymeRules.reverse(
+                config::EnzymeRules.RevConfig,
+                func::Const{typeof(Base.unsafe_copyto!)},
+                ::Type{RT},
+                tape,
+		dest::Annotation{<:$DstArr{T}},
+		doffs::Const,
+		src::Annotation{<:$SrcArr{T}},
+		soffs::Const,
+                n::Const) where {RT, T<: AbstractFloat}
+            if !(dest isa Const)
+                for batch in 1:EnzymeRules.width(config)
+                    ddest = _shadow(dest, config, batch)
+		    if !(src isa Const)
+			dsrc = _shadow(src, config, batch)
+			_accumulate!(@view(dsrc[soffs.val:soffs.val+n.val-1]), @view(ddest[doffs.val:doffs.val+n.val-1]))
+                    end
+		    _zero!(pointer(ddest, doffs.val), 0, n.val)
+                end
+            end
+            return (nothing, nothing, nothing, nothing, nothing)
         end
     end
 end
